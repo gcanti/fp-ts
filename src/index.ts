88735c4208fe3325cf83c7977a272352e757c0e8
Giulio Canti <giulio.canti@gmail.com>
2017-04-09 17:06:52 +0200

    add all exports for rollup (#29)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,84 @@
-export { StaticAlt, FantasyAlt } from './Alt'
-export { StaticAlternative, FantasyAlternative } from './Alternative'
-export { StaticApplicative, FantasyApplicative } from './Applicative'
-export { StaticApply, FantasyApply } from './Apply'
-export { StaticBifunctor, FantasyBifunctor } from './Bifunctor'
-export { StaticChain, FantasyChain } from './Chain'
-export { StaticChainRec, FantasyChainRec } from './ChainRec'
-export { StaticComonad, FantasyComonad } from './Comonad'
-export { StaticContravariant, FantasyContravariant } from './Contravariant'
-export { StaticCopointed, FantasyCopointed } from './Copointed'
-export { StaticExtend, FantasyExtend } from './Extend'
-export { StaticFoldable, FantasyFoldable } from './Foldable'
-export { StaticFunctor, FantasyFunctor } from './Functor'
-export { HKT, HKTS, HKT2, HKT2S } from './HKT'
-export { StaticMonad, FantasyMonad } from './Monad'
-export { StaticMonoid } from './Monoid'
-export { StaticOrd } from './Ord'
-export { Ordering } from './Ordering'
-export { StaticPlus, FantasyPlus } from './Plus'
-export { StaticPointed, FantasyPointed } from './Pointed'
-export { StaticSemigroup } from './Semigroup'
-export { StaticSetoid } from './Setoid'
-export { StaticTraversable, FantasyTraversable } from './Traversable'
-export { StaticUnfoldable } from './Unfoldable'
+import * as alt from './Alt'
+export { alt }
+import * as alternative from './Alternative'
+export { alternative }
+import * as applicative from './Applicative'
+export { applicative }
+import * as apply from './Apply'
+export { apply }
+import * as array from './Array'
+export { array }
+import * as bifunctor from './Bifunctor'
+export { bifunctor }
+import * as chain from './Chain'
+export { chain }
+import * as chainRec from './ChainRec'
+export { chainRec }
+import * as comonad from './Comonad'
+export { comonad }
+import * as const_ from './Const'
+export { const_ as const }
+import * as contravariant from './Contravariant'
+export { contravariant }
+import * as copointed from './Copointed'
+export { copointed }
+import * as dictionary from './Dictionary'
+export { dictionary }
+import * as either from './Either'
+export { either }
+import * as extend from './Extend'
+export { extend }
+import * as foldable from './Foldable'
+export { foldable }
+import * as function_ from './function'
+export { function_ as function }
+import * as functor from './Functor'
+export { functor }
+import * as hkt from './HKT'
+export { hkt }
+import * as identity from './Identity'
+export { identity }
+import * as io from './IO'
+export { io }
+import * as monad from './Monad'
+export { monad }
+import * as monoid from './Monoid'
+export { monoid }
+import * as nonEmptyArray from './NonEmptyArray'
+export { nonEmptyArray }
+import * as option from './Option'
+export { option }
+import * as ord from './Ord'
+export { ord }
+import * as ordering from './Ordering'
+export { ordering }
+import * as plus from './Plus'
+export { plus }
+import * as pointed from './Pointed'
+export { pointed }
+import * as reader from './Reader'
+export { reader }
+import * as rxjs from './Rxjs'
+export { rxjs }
+import * as semigroup from './Semigroup'
+export { semigroup }
+import * as semigroupoid from './Semigroupoid'
+export { semigroupoid }
+import * as setoid from './Setoid'
+export { setoid }
+import * as state from './State'
+export { state }
+import * as task from './Task'
+export { task }
+import * as traced from './Traced'
+export { traced }
+import * as traversable from './Traversable'
+export { traversable }
+import * as tuple from './Tuple'
+export { tuple }
+import * as unfoldable from './Unfoldable'
+export { unfoldable }
+import * as validation from './Validation'
+export { validation }
+import * as writer from './Writer'
+export { writer }
