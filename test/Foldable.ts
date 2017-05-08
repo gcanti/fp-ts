953a94fa6e18eaf952455da333256504ad49d685
gcanti <giulio.canti@gmail.com>
2017-05-08 15:43:57 +0200

    Foldable: add intercalate function. closes #65

--- test/Foldable.ts
+++ test/Foldable.ts
@@ -1,10 +1,12 @@
 import * as assert from 'assert'
 import {
   toArray,
-  getFoldableComposition
+  getFoldableComposition,
+  intercalate
 } from '../src/Foldable'
 import * as array from '../src/Array'
 import * as option from '../src/Option'
+import { monoidString } from '../src/Monoid'
 
 export const ArrayOptionURI = 'ArrayOption'
 
@@ -26,6 +28,17 @@ describe('Foldable', () => {
     const arrayOptionFoldable = getFoldableComposition(ArrayOptionURI)(array, option)
     assert.strictEqual(arrayOptionFoldable.reduce((b, a) => b + a, 0, [option.some(1), option.some(2)]), 3)
     assert.strictEqual(arrayOptionFoldable.reduce((b, a) => b + a, 0, [option.none, option.some(2)]), 2)
+  })
+
+  it('intercalate', () => {
+    const join = intercalate(getFoldableComposition('ArrayOption')(array, option), monoidString)
+    assert.strictEqual(join(' ', []), '')
+    assert.strictEqual(join(' ', [option.some('a')]), 'a')
+    assert.strictEqual(join(' ', [
+      option.some('a'),
+      option.none,
+      option.some('b')
+    ]), 'a b')
   })
 
 })
