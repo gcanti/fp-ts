f9c15937e559a0401f9384e38870f95558b66f03
gcanti <giulio.canti@gmail.com>
2017-05-13 16:51:55 +0200

    add getFunctionStaticMonoid, closes #70

--- test/Monoid.ts
+++ test/Monoid.ts
@@ -1,13 +1,28 @@
 import * as assert from 'assert'
 import {
   fold,
-  monoidSum
+  monoidSum,
+  getFunctionStaticMonoid,
+  monoidAll,
+  monoidAny
 } from '../src/Monoid'
+import { filter } from '../src/Array'
 
 describe('Monoid', () => {
 
   it('fold', () => {
     assert.strictEqual(fold(monoidSum, [1, 2, 3]), 6)
+  })
+
+  it('getFunctionStaticMonoid', () => {
+    const getPredicateStaticMonoidAll = getFunctionStaticMonoid(monoidAll)
+    const getPredicateStaticMonoidAny = getFunctionStaticMonoid(monoidAny)
+
+    const isLessThan10 = (n: number) => n <= 10
+    const isEven = (n: number) => n % 2 === 0
+
+    assert.deepEqual(filter(fold(getPredicateStaticMonoidAll<number>(), [isLessThan10, isEven]), [1, 2, 3, 40]), [2])
+    assert.deepEqual(filter(fold(getPredicateStaticMonoidAny<number>(), [isLessThan10, isEven]), [1, 2, 3, 40, 41]), [1, 2, 3, 40])
   })
 
 })
