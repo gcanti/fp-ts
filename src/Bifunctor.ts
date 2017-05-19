45d09d9039418ab4ddd59b0b15e3949b8080f606
gcanti <giulio.canti@gmail.com>
2017-05-19 07:32:50 +0200

    Bifunctor: use HKT2S

--- src/Bifunctor.ts
+++ src/Bifunctor.ts
@@ -1,10 +1,10 @@
-import { HKT2 } from './HKT'
+import { HKT2, HKT2S } from './HKT'
 
-export interface StaticBifunctor<F extends keyof HKT2<any, any>> {
+export interface StaticBifunctor<F extends HKT2S> {
   readonly URI: F
   bimap<A, B, C, D>(f: (a: A) => B, g: (c: C) => D, fac: HKT2<A, C>[F]): HKT2<B, D>[F]
 }
 
-export interface FantasyBifunctor<F extends keyof HKT2<any, any>, A, C> {
+export interface FantasyBifunctor<F extends HKT2S, A, C> {
   bimap<B, D>(f: (a: A) => B, g: (c: C) => D): HKT2<B, D>[F]
 }
