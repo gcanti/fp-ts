6cb58f232ab690e3ca5239eec028c7c1376d5f75
gcanti <giulio.canti@gmail.com>
2017-07-07 17:23:40 +0200

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+import * as alt from './Alt'
+export { alt }
+import * as alternative from './Alternative'
+export { alternative }
+import * as applicative from './Applicative'
+export { applicative }
+import * as apply from './Apply'
+export { apply }
+import * as array from './Array'
+export { array }
+import * as bifunctor from './Bifunctor'
+export { bifunctor }
+import * as chain from './Chain'
+export { chain }
+import * as chainRec from './ChainRec'
+export { chainRec }
+import * as comonad from './Comonad'
+export { comonad }
+import * as const_ from './Const'
+export { const_ as const }
+import * as contravariant from './Contravariant'
+export { contravariant }
+import * as either from './Either'
+export { either }
+import * as eitherT from './EitherT'
+export { eitherT }
+import * as extend from './Extend'
+export { extend }
+import * as field from './Field'
+export { field }
+import * as filterable from './Filterable'
+export { filterable }
+import * as foldable from './Foldable'
+export { foldable }
+import * as free from './Free'
+export { free }
+import * as function_ from './function'
+export { function_ as function }
+import * as functor from './Functor'
+export { functor }
+import * as hkt from './HKT'
+export { hkt }
+import * as identity from './Identity'
+export { identity }
+import * as io from './IO'
+export { io }
+import * as monad from './Monad'
+export { monad }
+import * as monoid from './Monoid'
+export { monoid }
+import * as naturalTransformation from './NaturalTransformation'
+export { naturalTransformation }
+import * as nonEmptyArray from './NonEmptyArray'
+export { nonEmptyArray }
+import * as option from './Option'
+export { option }
+import * as optionT from './OptionT'
+export { optionT }
+import * as ord from './Ord'
+export { ord }
+import * as ordering from './Ordering'
+export { ordering }
+import * as plus from './Plus'
+export { plus }
+import * as profunctor from './Profunctor'
+export { profunctor }
+import * as reader from './Reader'
+export { reader }
+import * as readerT from './ReaderT'
+export { readerT }
+import * as ring from './Ring'
+export { ring }
+import * as semigroup from './Semigroup'
+export { semigroup }
+import * as semigroupoid from './Semigroupoid'
+export { semigroupoid }
+import * as semiring from './Semiring'
+export { semiring }
+import * as setoid from './Setoid'
+export { setoid }
+import * as state from './State'
+export { state }
+import * as stateT from './StateT'
+export { stateT }
+import * as store from './Store'
+export { store }
+import * as strmap from './StrMap'
+export { strmap }
+import * as task from './Task'
+export { task }
+import * as these from './These'
+export { these }
+import * as traversable from './Traversable'
+export { traversable }
+import * as tuple from './Tuple'
+export { tuple }
+import * as unfoldable from './Unfoldable'
+export { unfoldable }
+import * as validation from './Validation'
+export { validation }
+import * as witherable from './Witherable'
+export { witherable }
+import * as writer from './Writer'
+export { writer }
