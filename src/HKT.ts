a9e39ee919087083a322e9b178c40c2158794bf3
Simon Meskens <meskens.simon@gmail.com>
2017-07-15 18:25:33 +0200

    Add typechecks to the type-level HKT dictionary

--- src/HKT.ts
+++ src/HKT.ts
@@ -8,11 +8,19 @@ export interface HKT2<URI, L, A> extends HKT<URI, A> {
 }
 
 // type-level dictionary for HKTs with one type parameter
-export interface URI2HKT<A> {}
+export type HKTMap<T extends string> = {
+    [K in T]: HKT<K, any>
+}
+
+export interface URI2HKT<A> extends HKTMap<HKTS> {}
 
 export type HKTS = keyof URI2HKT<any>
 
 // type-level dictionary for HKTs with two type parameters
-export interface URI2HKT2<L, A> {}
+export type HKTMap2<T extends string> = {
+    [K in T]: HKT2<K, any, any>
+}
+
+export interface URI2HKT2<L, A> extends HKTMap2<HKT2S> {}
 
 export type HKT2S = keyof URI2HKT2<any, any>
