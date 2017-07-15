002098397025bc5a4ef022dc19cb709537912740
gcanti <giulio.canti@gmail.com>
2017-07-15 19:09:24 +0200

    fix wrong HKT2 phantom types

--- src/HKT.ts
+++ src/HKT.ts
@@ -8,18 +8,14 @@ export interface HKT2<URI, L, A> extends HKT<URI, A> {
 }
 
 // type-level dictionary for HKTs with one type parameter
-export type HKTMap<T extends string> = {
-    [K in T]: HKT<K, any>
-}
+export type HKTMap<T extends string> = { [K in T]: HKT<K, any> }
 
 export interface URI2HKT<A> extends HKTMap<HKTS> {}
 
 export type HKTS = keyof URI2HKT<any>
 
 // type-level dictionary for HKTs with two type parameters
-export type HKTMap2<T extends string> = {
-    [K in T]: HKT2<K, any, any>
-}
+export type HKTMap2<T extends string> = { [K in T]: HKT2<K, any, any> }
 
 export interface URI2HKT2<L, A> extends HKTMap2<HKT2S> {}
 
