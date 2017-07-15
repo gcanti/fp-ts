e3988b3d46c8dba8a5cd0014e295426585ea40ce
Simon Meskens <meskens.simon@gmail.com>
2017-07-15 22:13:58 +0200

    Linting
    My bad, didn't realize linting was on, I'll check in the future

--- src/HKT.ts
+++ src/HKT.ts
@@ -8,19 +8,19 @@ export interface HKT2<URI, L, A> extends HKT<URI, A> {
 }
 
 // type-level dictionary for HKTs with one type parameter
-//export type HKTMap<T extends string> = { [K in T]: HKT<K, any> }
+// export type HKTMap<T extends string> = { [K in T]: HKT<K, any> }
 
 export interface URI2HKT<A> {}
 
 export type HKTS = keyof URI2HKT<any>
 
-(null! as URI2HKT<any>) as { [k in keyof URI2HKT<any>]: HKT<k, any> };
+(null! as URI2HKT<any>) as { [k in keyof URI2HKT<any>]: HKT<k, any> }
 
 // type-level dictionary for HKTs with two type parameters
-//export type HKTMap2<T extends string> = { [K in T]: HKT2<K, any, any> }
+// export type HKTMap2<T extends string> = { [K in T]: HKT2<K, any, any> }
 
 export interface URI2HKT2<L, A> {}
 
 export type HKT2S = keyof URI2HKT2<any, any>
 
-(null! as URI2HKT2<any, any>) as { [k in keyof URI2HKT2<any, any>]: HKT<k, any> };
+(null! as URI2HKT2<any, any>) as { [k in keyof URI2HKT2<any, any>]: HKT<k, any> }
