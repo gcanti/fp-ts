295b68a5e742dba071cd5400f64b51a480e8844d
Simon Meskens <meskens.simon@gmail.com>
2017-07-15 22:05:29 +0200

    Different map check method
    This one doesn't seem to give me the same issues, but I'm worried any attempt to typecheck the map will run into file ordering issues, which I assume is what's causing these heisenbugs

--- src/HKT.ts
+++ src/HKT.ts
@@ -8,15 +8,19 @@ export interface HKT2<URI, L, A> extends HKT<URI, A> {
 }
 
 // type-level dictionary for HKTs with one type parameter
-export type HKTMap<T extends string> = { [K in T]: HKT<K, any> }
+//export type HKTMap<T extends string> = { [K in T]: HKT<K, any> }
 
-export interface URI2HKT<A> extends HKTMap<HKTS> {}
+export interface URI2HKT<A> {}
 
 export type HKTS = keyof URI2HKT<any>
 
-// type-level dictionary for HKTs with two type parameters
-export type HKTMap2<T extends string> = { [K in T]: HKT2<K, any, any> }
+(null! as URI2HKT<any>) as { [k in keyof URI2HKT<any>]: HKT<k, any> };
 
-export interface URI2HKT2<L, A> extends HKTMap2<HKT2S> {}
+// type-level dictionary for HKTs with two type parameters
+//export type HKTMap2<T extends string> = { [K in T]: HKT2<K, any, any> }
+
+export interface URI2HKT2<L, A> {}
 
 export type HKT2S = keyof URI2HKT2<any, any>
+
+(null! as URI2HKT2<any, any>) as { [k in keyof URI2HKT2<any, any>]: HKT<k, any> };
