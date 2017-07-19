856664bbc09907ab4ce953051b6b6bacb403f7ae
Simon Meskens <meskens.simon@gmail.com>
2017-07-19 17:04:00 +0200

    Re-added integrity checks

--- src/HKT.ts
+++ src/HKT.ts
@@ -12,7 +12,7 @@ export interface HKT2<URI, L, A> extends HKT<URI, A> {
 export interface URI2HKT<A> {}
 export interface URI2HKT2<L, A> {}
 
-// URI constrains with dictionary integrity check
+// URI constraints with dictionary integrity constraint
 
 export type HKTS = URI2HKT<any>[keyof URI2HKT<any>]['_URI']
 
@@ -23,3 +23,10 @@ export type HKT2S = URI2HKT2<any, any>[keyof URI2HKT2<any, any>]['_URI']
 export type HKTAs<URI extends HKTS, A> = URI2HKT<A>[URI]
 
 export type HKT2As<URI extends HKT2S, L, A> = URI2HKT2<L, A>[URI]
+
+// Type-level integrity check
+
+/* tslint:disable */
+(null! as URI2HKT<any>) as { [k in keyof URI2HKT<any>]: HKT<k, any> }
+(null! as URI2HKT2<any, any>) as { [k in keyof URI2HKT2<any, any>]: HKT2<k, any, any> }
+/* tslint:enable */
