6cc66203d66578e8e7eefc0196d9a19a21628515
gcanti <giulio.canti@gmail.com>
2017-07-28 11:56:58 +0200

    add IxMonad module

--- src/HKT.ts
+++ src/HKT.ts
@@ -7,26 +7,34 @@ export interface HKT2<URI, L, A> extends HKT<URI, A> {
   readonly _L: L
 }
 
+export interface HKT3<URI, U, L, A> extends HKT2<URI, L, A> {
+  readonly _U: U
+}
+
 // type-level dictionaries for HKTs
 
 export interface URI2HKT<A> {}
 export interface URI2HKT2<L, A> {}
+export interface URI2HKT3<U, L, A> {}
 
 // URI constraints with dictionary integrity constraint
 
 export type HKTS = (URI2HKT<any> & { never: HKT<never, never> })[keyof URI2HKT<any> | 'never']['_URI']
-
 export type HKT2S = (URI2HKT2<any, any> & { never: HKT<never, never> })[keyof URI2HKT2<any, any> | 'never']['_URI']
+export type HKT3S = (URI2HKT3<any, any, any> & { never: HKT<never, never> })[
+  | keyof URI2HKT3<any, any, any>
+  | 'never']['_URI']
 
 // HKTAs<U, A> is the same as URI2HKT<A>[U], but checks for URI constraints
 
 export type HKTAs<URI extends HKTS, A> = URI2HKT<A>[URI]
-
 export type HKT2As<URI extends HKT2S, L, A> = URI2HKT2<L, A>[URI]
+export type HKT3As<URI extends HKT3S, U, L, A> = URI2HKT3<U, L, A>[URI]
 
 // Type-level integrity check
 
 /* tslint:disable */
 (null! as URI2HKT<any>) as { [k in keyof URI2HKT<any>]: HKT<k, any> }
 (null! as URI2HKT2<any, any>) as { [k in keyof URI2HKT2<any, any>]: HKT2<k, any, any> }
+(null! as URI2HKT3<any, any, any>) as { [k in keyof URI2HKT3<any, any, any>]: HKT3<k, any, any, any> }
 /* tslint:enable */
---
+++ src/IxMonad.ts
@@ -0,0 +1,39 @@
+import { HKT3, HKT3S, HKT3As } from './HKT'
+import { constant } from './function'
+
+// Adapted from https://github.com/garyb/purescript-indexed-monad
+
+export interface IxMonad<F> {
+  readonly URI: F
+  iof<I, A>(a: A): HKT3<F, I, I, A>
+  ichain<I, O, Z, A, B>(f: (a: A) => HKT3<F, O, Z, B>, fa: HKT3<F, I, O, A>): HKT3<F, I, Z, B>
+}
+
+export interface FantasyIxMonad<F, A, O, I> {
+  iof<I, B>(b: B): HKT3<F, I, I, B>
+  ichain<Z, B>(f: (a: A) => HKT3<F, O, Z, B>): HKT3<F, I, Z, B>
+}
+
+export class Ops {
+  iapplyFirst<F extends HKT3S>(
+    ixmonad: IxMonad<F>
+  ): <I, O, Z, A, B>(fa: HKT3As<F, I, O, A>, fb: HKT3As<F, O, Z, B>) => HKT3As<F, I, Z, A>
+  iapplyFirst<F>(ixmonad: IxMonad<F>): <I, O, Z, A, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, A>
+  iapplyFirst<F>(ixmonad: IxMonad<F>): <I, O, Z, A, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, A> {
+    return (fa, fb) => ixmonad.ichain(a => ixmonad.ichain(() => ixmonad.iof(a), fb), fa)
+  }
+
+  iapplySecond<F extends HKT3S>(
+    ixmonad: IxMonad<F>
+  ): <I, O, Z, A, B>(fa: HKT3As<F, I, O, A>, fb: HKT3As<F, O, Z, B>) => HKT3As<F, I, Z, B>
+  iapplySecond<F>(ixmonad: IxMonad<F>): <I, O, Z, A, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, B>
+  iapplySecond<F>(
+    ixmonad: IxMonad<F>
+  ): <I, O, Z, A, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, B> {
+    return (fa, fb) => ixmonad.ichain(constant(fb), fa)
+  }
+}
+
+const ops = new Ops()
+export const iapplyFirst: Ops['iapplyFirst'] = ops.iapplyFirst
+export const iapplySecond: Ops['iapplySecond'] = ops.iapplySecond
