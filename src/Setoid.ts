c6dd45a45e3f6c3b4e406c488316cb3adfb04548
gcanti <giulio.canti@gmail.com>
2017-08-12 15:54:57 +0200

    Setoid: add getArraySetoid

--- src/Setoid.ts
+++ src/Setoid.ts
@@ -11,3 +11,11 @@ export const setoidString: Setoid<string> = { equals: strictEqual }
 export const setoidNumber: Setoid<number> = { equals: strictEqual }
 
 export const setoidBoolean: Setoid<boolean> = { equals: strictEqual }
+
+export function getArraySetoid<A>(S: Setoid<A>): Setoid<Array<A>> {
+  return {
+    equals(xs, ys) {
+      return xs.length === ys.length && xs.every((x, i) => S.equals(x, ys[i]))
+    }
+  }
+}
