7bb45945a9a8c4309c6fba6e9cf99974ad4c622b
Kirill Agalakov <agalakov@devexperts.com>
2017-08-16 17:08:20 +0300

    FR: migrate to curried // Ring

--- src/Field.ts
+++ src/Field.ts
@@ -14,7 +14,7 @@ export const fieldNumber: Field<number> = {
   zero: () => 0,
   mul: x => y => x * y,
   one: () => 1,
-  sub: (x, y) => x - y,
+  sub: x => y => x - y,
   degree: () => 1,
   div: (x, y) => x / y,
   mod: (x, y) => x % y
