b35ddfbc19c11b1d492b52c7d1fd6aa1a34533d9
gcanti <giulio.canti@gmail.com>
2017-08-18 10:45:24 +0200

    FR: migrate to curried // Profunctor

--- src/Profunctor.ts
+++ src/Profunctor.ts
@@ -9,10 +9,10 @@ export interface FantasyProfunctor<F, B, C> extends FantasyFunctor<F, C> {
   promap<A, D>(f: (a: A) => B, g: (c: C) => D): HKT2<F, A, D>
 }
 
-export function lmap<F>(profunctor: Profunctor<F>): <A, B, C>(f: (a: A) => B, fbc: HKT2<F, B, C>) => HKT2<F, A, C> {
-  return (f, fbc) => profunctor.promap(f, c => c, fbc)
-}
+export const lmap = <F>(profunctor: Profunctor<F>) => <A, B>(f: (a: A) => B) => <C>(
+  fbc: HKT2<F, B, C>
+): HKT2<F, A, C> => profunctor.promap(f, c => c, fbc)
 
-export function rmap<F>(profunctor: Profunctor<F>): <B, C, D>(g: (c: C) => D, fbc: HKT2<F, B, C>) => HKT2<F, B, D> {
-  return <B, C, D>(g: (c: C) => D, fbc: HKT2<F, B, C>) => profunctor.promap((b: B) => b, g, fbc)
-}
+export const rmap = <F>(profunctor: Profunctor<F>) => <C, D>(g: (c: C) => D) => <B>(
+  fbc: HKT2<F, B, C>
+): HKT2<F, B, D> => profunctor.promap(b => b, g, fbc)
