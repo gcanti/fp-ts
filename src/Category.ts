bdb0d9a3bfcaf5ec16ec1f065f0d543eb6e0115b
gcanti <giulio.canti@gmail.com>
2017-08-21 16:49:17 +0200

    fix Category definition

--- src/Category.ts
+++ src/Category.ts
@@ -2,5 +2,5 @@ import { HKT2 } from './HKT'
 import { Semigroupoid } from './Semigroupoid'
 
 export interface Category<F> extends Semigroupoid<F> {
-  id<A>(): HKT2<F, A, A>
+  id: <A>() => HKT2<F, A, A>
 }
