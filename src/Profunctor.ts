5c61136e2a8d5149fda371a6615d2af6d350eaad
gcanti <giulio.canti@gmail.com>
2017-08-24 08:32:24 +0200

    curry Profunctor.promap

--- src/Profunctor.ts
+++ src/Profunctor.ts
@@ -2,7 +2,7 @@ import { HKT2 } from './HKT'
 import { Functor, FantasyFunctor } from './Functor'
 
 export interface Profunctor<F> extends Functor<F> {
-  promap: <A, B, C, D>(f: (a: A) => B, g: (c: C) => D, fbc: HKT2<F, B, C>) => HKT2<F, A, D>
+  promap: <A, B, C, D>(f: (a: A) => B, g: (c: C) => D) => (fbc: HKT2<F, B, C>) => HKT2<F, A, D>
 }
 
 export interface FantasyProfunctor<F, B, C> extends FantasyFunctor<F, C> {
@@ -11,8 +11,8 @@ export interface FantasyProfunctor<F, B, C> extends FantasyFunctor<F, C> {
 
 export const lmap = <F>(profunctor: Profunctor<F>) => <A, B>(f: (a: A) => B) => <C>(
   fbc: HKT2<F, B, C>
-): HKT2<F, A, C> => profunctor.promap(f, c => c, fbc)
+): HKT2<F, A, C> => profunctor.promap(f, (c: C) => c)(fbc)
 
 export const rmap = <F>(profunctor: Profunctor<F>) => <C, D>(g: (c: C) => D) => <B>(
   fbc: HKT2<F, B, C>
-): HKT2<F, B, D> => profunctor.promap(b => b, g, fbc)
+): HKT2<F, B, D> => profunctor.promap((b: B) => b, g)(fbc)
