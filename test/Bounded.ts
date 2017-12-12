9328abcbf0740b4f0281342ab9094e117357b7b6
gcanti <giulio.canti@gmail.com>
2017-12-12 18:49:42 +0100

    add Bounded typeclass

---
+++ test/Bounded.ts
@@ -0,0 +1,14 @@
+import * as assert from 'assert'
+import { boundedNumber, Bounded } from '../src/Bounded'
+import { fold, getMeetSemigroup, getJoinSemigroup } from '../src/Semigroup'
+
+describe('Bounded', () => {
+  it('boundedNumber', () => {
+    const min_ = <A>(B: Bounded<A>): ((as: Array<A>) => A) => fold(getMeetSemigroup(B))(B.top())
+    const max_ = <A>(B: Bounded<A>): ((as: Array<A>) => A) => fold(getJoinSemigroup(B))(B.bottom())
+    const min = min_(boundedNumber)
+    const max = max_(boundedNumber)
+    assert.strictEqual(min([9, 2, 6, 1]), 1)
+    assert.strictEqual(max([9, 2, 6, 1]), 9)
+  })
+})
