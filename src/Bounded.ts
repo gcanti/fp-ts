62109e0458fea237dff10a34f6bc04e440d3a263
gcanti <giulio.canti@gmail.com>
2017-12-12 18:49:42 +0100

    add Bounded typeclass

---
+++ src/Bounded.ts
@@ -0,0 +1,22 @@
+import { Ord, ordNumber } from './Ord'
+
+/**
+ * The `Bounded` type class represents totally ordered types that have an
+ * upper and lower boundary.
+ *
+ * Instances should satisfy the following law in addition to the `Ord` laws:
+ *
+ * - Bounded: `bottom <= a <= top`
+ * @typeclass
+ */
+export interface Bounded<A> extends Ord<A> {
+  top: () => A
+  bottom: () => A
+}
+
+/** @instance */
+export const boundedNumber: Bounded<number> = {
+  ...ordNumber,
+  top: () => Infinity,
+  bottom: () => -Infinity
+}
