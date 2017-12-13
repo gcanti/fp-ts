2e738440cdafc7921185a1e046b9718c615c9956
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2017-12-13 23:55:17 +0100

    optimize span

---
+++ perf/span.js
@@ -0,0 +1,55 @@
+var Benchmark = require('benchmark')
+
+const suite = new Benchmark.Suite()
+
+const arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+
+const span = (predicate, as) => {
+  const init = []
+  let i = 0
+  for (; i < as.length; i++) {
+    if (predicate(as[i])) {
+      init.push(as[i])
+    } else {
+      break
+    }
+  }
+  return { init, rest: as.slice(i) }
+}
+
+const span2 = (predicate, as) => {
+  const l = as.length
+  let i = 0
+  for (; i < l; i++) {
+    if (predicate(as[i])) {
+    } else {
+      break
+    }
+  }
+
+  const init = Array(i)
+
+  for (let j = 0; j < i; j++) {
+    init[j] = as[j]
+  }
+  const rest = Array(l - i)
+  for (let j = i; j < l; j++) {
+    rest[j - i] = as[j]
+  }
+  return { init, rest }
+}
+
+suite
+  .add('span1', function() {
+    span(x => x < 3, arr)
+  })
+  .add('span2', function() {
+    span2(x => x < 3, arr)
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  .run({ async: true })
