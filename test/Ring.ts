4ff7319654595d2d60d906c29533fce8f26a3868
gcanti <giulio.canti@gmail.com>
2018-01-28 18:46:31 +0100

    uncurry Ring

--- test/Ring.ts
+++ test/Ring.ts
@@ -8,7 +8,7 @@ describe('Ring', () => {
     assert.deepEqual(ring.add([1, 2], [3, 4]), [4, 6])
     assert.deepEqual(ring.mul([1, 2], [3, 4]), [3, 8])
     assert.deepEqual(ring.one, [1, 1])
-    assert.deepEqual(ring.sub([1, 2])([3, 4]), [-2, -2])
+    assert.deepEqual(ring.sub([1, 2], [3, 4]), [-2, -2])
     assert.deepEqual(ring.zero, [0, 0])
   })
 })
