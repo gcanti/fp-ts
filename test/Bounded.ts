558d807711580a6c5bd8030c8e77a515889ec613
gcanti <giulio.canti@gmail.com>
2018-01-28 19:34:03 +0100

    remove thunks from Bounded

--- test/Bounded.ts
+++ test/Bounded.ts
@@ -4,8 +4,8 @@ import { fold, getMeetSemigroup, getJoinSemigroup } from '../src/Semigroup'
 
 describe('Bounded', () => {
   it('boundedNumber', () => {
-    const min_ = <A>(B: Bounded<A>): ((as: Array<A>) => A) => fold(getMeetSemigroup(B))(B.top())
-    const max_ = <A>(B: Bounded<A>): ((as: Array<A>) => A) => fold(getJoinSemigroup(B))(B.bottom())
+    const min_ = <A>(B: Bounded<A>): ((as: Array<A>) => A) => fold(getMeetSemigroup(B))(B.top)
+    const max_ = <A>(B: Bounded<A>): ((as: Array<A>) => A) => fold(getJoinSemigroup(B))(B.bottom)
     const min = min_(boundedNumber)
     const max = max_(boundedNumber)
     assert.strictEqual(min([9, 2, 6, 1]), 1)
