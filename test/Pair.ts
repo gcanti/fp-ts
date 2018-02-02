02be0ca5c4a3eef4e9a65a9600e6ec26ee4eb7d9
gcanti <giulio.canti@gmail.com>
2018-02-02 12:17:25 +0100

    refactor Pair

--- test/Pair.ts
+++ test/Pair.ts
@@ -5,7 +5,7 @@ import { none, some, option } from '../src/Option'
 
 describe('Pair', () => {
   it('traverse', () => {
-    assert.deepEqual(traverse(option, pair)(new Pair([0, 1]), n => (n >= 0 ? some(n) : none)), some(new Pair([0, 1])))
-    assert.deepEqual(traverse(option, pair)(new Pair([0, 1]), n => (n >= 2 ? some(n) : none)), none)
+    assert.deepEqual(traverse(option, pair)(new Pair(0, 1), n => (n >= 0 ? some(n) : none)), some(new Pair(0, 1)))
+    assert.deepEqual(traverse(option, pair)(new Pair(0, 1), n => (n >= 2 ? some(n) : none)), none)
   })
 })
