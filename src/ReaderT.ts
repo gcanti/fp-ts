d000b3bad7ef874e33d55f94372aea8b31f02c4e
gcanti <giulio.canti@gmail.com>
2018-02-25 11:20:28 +0100

    Internal: add URIS3 overloadings to ReaderT

--- src/ReaderT.ts
+++ src/ReaderT.ts
@@ -1,8 +1,8 @@
-import { HKT, URIS, URIS2, Type, Type2 } from './HKT'
-import { Functor, Functor2, Functor1 } from './Functor'
-import { Applicative, Applicative1, Applicative2 } from './Applicative'
-import { Chain, Chain1, Chain2 } from './Chain'
-import { Monad, Monad1, Monad2 } from './Monad'
+import { HKT, URIS, URIS2, URIS3, Type, Type2, Type3 } from './HKT'
+import { Functor, Functor1, Functor2, Functor3 } from './Functor'
+import { Applicative, Applicative1, Applicative2, Applicative3 } from './Applicative'
+import { Chain, Chain1, Chain2, Chain3 } from './Chain'
+import { Monad, Monad1, Monad2, Monad3 } from './Monad'
 
 export interface ReaderT<M> {
   map: <E, A, B>(f: (a: A) => B, fa: (e: E) => HKT<M, A>) => (e: E) => HKT<M, B>
@@ -25,6 +25,22 @@ export interface ReaderT2<M extends URIS2> {
   chain: <L, E, A, B>(f: (a: A) => (e: E) => Type2<M, L, B>, fa: (e: E) => Type2<M, L, A>) => (e: E) => Type2<M, L, B>
 }
 
+export interface ReaderT3<M extends URIS3> {
+  map: <U, L, E, A, B>(f: (a: A) => B, fa: (e: E) => Type3<M, U, L, A>) => (e: E) => Type3<M, U, L, B>
+  of: <U, L, E, A>(a: A) => (e: E) => Type3<M, U, L, A>
+  ap: <U, L, E, A, B>(
+    fab: (e: E) => Type3<M, U, L, (a: A) => B>,
+    fa: (e: E) => Type3<M, U, L, A>
+  ) => (e: E) => Type3<M, U, L, B>
+  chain: <U, L, E, A, B>(
+    f: (a: A) => (e: E) => Type3<M, U, L, B>,
+    fa: (e: E) => Type3<M, U, L, A>
+  ) => (e: E) => Type3<M, U, L, B>
+}
+
+export function map<F extends URIS3>(
+  F: Functor3<F>
+): <U, L, E, A, B>(f: (a: A) => B, fa: (e: E) => Type3<F, U, L, A>) => (e: E) => Type3<F, U, L, B>
 export function map<F extends URIS2>(
   F: Functor2<F>
 ): <L, E, A, B>(f: (a: A) => B, fa: (e: E) => Type2<F, L, A>) => (e: E) => Type2<F, L, B>
@@ -37,6 +53,7 @@ export function map<F>(F: Functor<F>): <E, A, B>(f: (a: A) => B, fa: (e: E) => H
   return (f, fa) => e => F.map(fa(e), f)
 }
 
+export function of<F extends URIS3>(F: Applicative3<F>): <U, L, E, A>(a: A) => (e: E) => Type3<F, U, L, A>
 export function of<F extends URIS2>(F: Applicative2<F>): <L, E, A>(a: A) => (e: E) => Type2<F, L, A>
 export function of<F extends URIS>(F: Applicative1<F>): <E, A>(a: A) => (e: E) => Type<F, A>
 export function of<F>(F: Applicative<F>): <E, A>(a: A) => (e: E) => HKT<F, A>
@@ -45,6 +62,12 @@ export function of<F>(F: Applicative<F>): <E, A>(a: A) => (e: E) => HKT<F, A> {
   return <A>(a: A) => <E>(e: E) => F.of(a)
 }
 
+export function ap<F extends URIS3>(
+  F: Applicative3<F>
+): <U, L, E, A, B>(
+  fab: (e: E) => Type3<F, U, L, (a: A) => B>,
+  fa: (e: E) => Type3<F, U, L, A>
+) => (e: E) => Type3<F, U, L, B>
 export function ap<F extends URIS2>(
   F: Applicative2<F>
 ): <L, E, A, B>(fab: (e: E) => Type2<F, L, (a: A) => B>, fa: (e: E) => Type2<F, L, A>) => (e: E) => Type2<F, L, B>
@@ -61,6 +84,12 @@ export function ap<F>(
   return (fab, fa) => e => F.ap(fab(e), fa(e))
 }
 
+export function chain<F extends URIS3>(
+  F: Chain3<F>
+): <U, L, E, A, B>(
+  f: (a: A) => (e: E) => Type3<F, U, L, B>,
+  fa: (e: E) => Type3<F, U, L, A>
+) => (e: E) => Type3<F, U, L, B>
 export function chain<F extends URIS2>(
   F: Chain2<F>
 ): <L, E, A, B>(f: (a: A) => (e: E) => Type2<F, L, B>, fa: (e: E) => Type2<F, L, A>) => (e: E) => Type2<F, L, B>
@@ -77,6 +106,7 @@ export function chain<F>(
   return (f, fa) => e => F.chain(fa(e), a => f(a)(e))
 }
 
+export function ask<F extends URIS3>(F: Applicative3<F>): <U, L, E>() => (e: E) => Type3<F, U, L, E>
 export function ask<F extends URIS2>(F: Applicative2<F>): <L, E>() => (e: E) => Type2<F, L, E>
 export function ask<F extends URIS>(F: Applicative1<F>): <E>() => (e: E) => Type<F, E>
 export function ask<F>(F: Applicative<F>): <E>() => (e: E) => HKT<F, E>
@@ -85,6 +115,7 @@ export function ask<F>(F: Applicative<F>): <E>() => (e: E) => HKT<F, E> {
   return () => F.of
 }
 
+export function asks<F extends URIS3>(F: Applicative3<F>): <U, L, E, A>(f: (e: E) => A) => (e: E) => Type3<F, U, L, A>
 export function asks<F extends URIS2>(F: Applicative2<F>): <L, E, A>(f: (e: E) => A) => (e: E) => Type2<F, L, A>
 export function asks<F extends URIS>(F: Applicative1<F>): <E, A>(f: (e: E) => A) => (e: E) => Type<F, A>
 export function asks<F>(F: Applicative<F>): <E, A>(f: (e: E) => A) => (e: E) => HKT<F, A>
@@ -93,6 +124,7 @@ export function asks<F>(F: Applicative<F>): <E, A>(f: (e: E) => A) => (e: E) => 
   return f => e => F.of(f(e))
 }
 
+export function getReaderT<M extends URIS3>(M: Monad3<M>): ReaderT3<M>
 export function getReaderT<M extends URIS2>(M: Monad2<M>): ReaderT2<M>
 export function getReaderT<M extends URIS>(M: Monad1<M>): ReaderT1<M>
 export function getReaderT<M>(M: Monad<M>): ReaderT<M>
