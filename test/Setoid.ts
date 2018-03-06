1f554f7c4930ec75d39bbbf14bae219073686be3
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2018-03-06 22:13:16 +0100

    add contramap to Setoid

--- test/Setoid.ts
+++ test/Setoid.ts
@@ -1,12 +1,13 @@
 import * as assert from 'assert'
-import { getRecordSetoid, setoidString, setoidNumber, getProductSetoid } from '../src/Setoid'
+import { getRecordSetoid, setoidString, setoidNumber, getProductSetoid, fromEquals, contramap } from '../src/Setoid'
 
 describe('Setoid', () => {
+  interface Person {
+    name: string
+    age: number
+  }
+
   it('getRecordSetoid', () => {
-    interface Person {
-      name: string
-      age: number
-    }
     const S = getRecordSetoid<Person>({
       name: setoidString,
       age: setoidNumber
@@ -21,5 +22,19 @@ describe('Setoid', () => {
     assert.strictEqual(S.equals(['a', 1], ['a', 1]), true)
     assert.strictEqual(S.equals(['a', 1], ['b', 1]), false)
     assert.strictEqual(S.equals(['a', 1], ['a', 2]), false)
+  })
+
+  it('fromEquals', () => {
+    const S = fromEquals((a: number, b: number) => a === b)
+    assert.strictEqual(S.equals(1, 1), true)
+    assert.strictEqual(S.equals(1, 0), false)
+  })
+
+  it('contramap', () => {
+    const S = contramap((p: Person) => p.name, setoidString)
+    assert.strictEqual(S.equals({ name: 'a', age: 1 }, { name: 'a', age: 2 }), true)
+    assert.strictEqual(S.equals({ name: 'a', age: 1 }, { name: 'a', age: 1 }), true)
+    assert.strictEqual(S.equals({ name: 'a', age: 1 }, { name: 'b', age: 1 }), false)
+    assert.strictEqual(S.equals({ name: 'a', age: 1 }, { name: 'b', age: 2 }), false)
   })
 })
