1f7d65378ee2e35ed4157f81828fc3b299cf5ff9
gcanti <giulio.canti@gmail.com>
2018-04-10 12:40:27 +0200

    New Feature: add listen, pass, listens, censor to Writer

--- src/Writer.ts
+++ src/Writer.ts
@@ -2,7 +2,7 @@ import { Monoid } from './Monoid'
 import { Functor2 } from './Functor'
 import { Monad2C } from './Monad'
 import { Semigroup } from './Semigroup'
-import { phantom } from './function'
+import { phantom, tuple } from './function'
 
 declare module './HKT' {
   interface URI2HKT2<L, A> {
@@ -61,11 +61,60 @@ const chain = <W>(S: Semigroup<W>) => <A, B>(fa: Writer<W, A>, f: (a: A) => Writ
 }
 
 /**
+ * Appends a value to the accumulator
  * @function
  * @since 1.0.0
  */
 export const tell = <W>(w: W): Writer<W, void> => {
   return new Writer(() => [undefined, w])
+}
+
+/**
+ * Modifies the result to include the changes to the accumulator
+ * @function
+ * @since 1.3.0
+ */
+export const listen = <W, A>(fa: Writer<W, A>): Writer<W, [A, W]> => {
+  return new Writer(() => {
+    const [a, w] = fa.run()
+    return [tuple(a, w), w]
+  })
+}
+
+/**
+ * Applies the returned function to the accumulator
+ * @function
+ * @since 1.3.0
+ */
+export const pass = <W, A>(fa: Writer<W, [A, (w: W) => W]>): Writer<W, A> => {
+  return new Writer(() => {
+    const [[a, f], w] = fa.run()
+    return [a, f(w)]
+  })
+}
+
+/**
+ * Projects a value from modifications made to the accumulator during an action
+ * @function
+ * @since 1.3.0
+ */
+export const listens = <W, A, B>(fa: Writer<W, A>, f: (w: W) => B): Writer<W, [A, B]> => {
+  return new Writer(() => {
+    const [a, w] = fa.run()
+    return [tuple(a, f(w)), w]
+  })
+}
+
+/**
+ * Modify the final accumulator value by applying a function
+ * @function
+ * @since 1.3.0
+ */
+export const censor = <W, A>(fa: Writer<W, A>, f: (w: W) => W): Writer<W, A> => {
+  return new Writer(() => {
+    const [a, w] = fa.run()
+    return [a, f(w)]
+  })
 }
 
 /**
