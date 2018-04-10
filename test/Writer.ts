e1f91de43c95e5a27f177a95c89294a3cf685ca6
gcanti <giulio.canti@gmail.com>
2018-04-10 12:40:27 +0200

    New Feature: add listen, pass, listens, censor to Writer

---
+++ test/Writer.ts
@@ -0,0 +1,29 @@
+import * as assert from 'assert'
+import { Writer, tell, listen, pass, listens, censor } from '../src/Writer'
+import { tuple } from '../src/function'
+
+describe('Writer', () => {
+  it('tell', () => {
+    assert.deepEqual(tell(1).run(), [undefined, 1])
+  })
+
+  it('listen', () => {
+    const fa = new Writer(() => [1, 'a'])
+    assert.deepEqual(listen(fa).run(), [[1, 'a'], 'a'])
+  })
+
+  it('pass', () => {
+    const fa = new Writer(() => [tuple(1, (w: string) => w + 'b'), 'a'])
+    assert.deepEqual(pass(fa).run(), [1, 'ab'])
+  })
+
+  it('listens', () => {
+    const fa = new Writer(() => [1, 'a'])
+    assert.deepEqual(listens(fa, w => w.length).run(), [[1, 1], 'a'])
+  })
+
+  it('censor', () => {
+    const fa = new Writer(() => [1, ['a', 'b']])
+    assert.deepEqual(censor(fa, w => w.filter(a => a !== 'a')).run(), [1, ['b']])
+  })
+})
