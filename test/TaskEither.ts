d1b241e48513a6f8e5e2bb9c367795e6028cf843
gcanti <giulio.canti@gmail.com>
2018-04-12 18:20:14 +0200

    New Feature: add `TaskEither.fromLeft`

--- test/TaskEither.ts
+++ test/TaskEither.ts
@@ -1,16 +1,16 @@
 import * as assert from 'assert'
 import { right, left } from '../src/Either'
-import { fromEither, taskEither } from '../src/TaskEither'
+import { taskEither, fromLeft } from '../src/TaskEither'
 
 describe('TaskEither', () => {
   it('chain', () => {
     const te1 = taskEither.chain(
-      fromEither<string, string>(right('foo')),
-      a => (a.length > 2 ? fromEither<string, number>(right(a.length)) : fromEither<string, number>(left('foo')))
+      taskEither.of<string, string>('foo'),
+      a => (a.length > 2 ? taskEither.of<string, number>(a.length) : fromLeft<string, number>('foo'))
     )
     const te2 = taskEither.chain(
-      fromEither<string, string>(right('a')),
-      a => (a.length > 2 ? fromEither<string, number>(right(a.length)) : fromEither<string, number>(left('foo')))
+      taskEither.of<string, string>('a'),
+      a => (a.length > 2 ? taskEither.of<string, number>(a.length) : fromLeft<string, number>('foo'))
     )
     return Promise.all([te1.run(), te2.run()]).then(([e1, e2]) => {
       assert.deepEqual(e1, right(3))
@@ -21,8 +21,8 @@ describe('TaskEither', () => {
   it('fold', () => {
     const f = (s: string): boolean => s.length > 2
     const g = (n: number): boolean => n > 2
-    const te1 = fromEither<string, number>(right(1)).fold(f, g)
-    const te2 = fromEither<string, number>(left('foo')).fold(f, g)
+    const te1 = taskEither.of<string, number>(1).fold(f, g)
+    const te2 = fromLeft<string, number>('foo').fold(f, g)
     return Promise.all([te1.run(), te2.run()]).then(([b1, b2]) => {
       assert.strictEqual(b1, false)
       assert.strictEqual(b2, true)
@@ -32,8 +32,8 @@ describe('TaskEither', () => {
   it('bimap', () => {
     const f = (s: string): number => s.length
     const g = (n: number): boolean => n > 2
-    const teRight = fromEither(right<string, number>(1)).bimap(f, g)
-    const teLeft = fromEither(left<string, number>('foo')).bimap(f, g)
+    const teRight = taskEither.of<string, number>(1).bimap(f, g)
+    const teLeft = fromLeft<string, number>('foo').bimap(f, g)
     return Promise.all([teRight.run(), teLeft.run()]).then(([e1, e2]) => {
       assert.deepEqual(e1, right(false))
       assert.deepEqual(e2, left(3))
@@ -41,10 +41,10 @@ describe('TaskEither', () => {
   })
 
   it('orElse', () => {
-    const l = fromEither<string, number>(left('foo'))
-    const r = fromEither<string, number>(right(1))
-    const tl = l.orElse(l => fromEither<number, number>(right(l.length)))
-    const tr = r.orElse(() => fromEither<number, number>(right(2)))
+    const l = fromLeft<string, number>('foo')
+    const r = taskEither.of<string, number>(1)
+    const tl = l.orElse(l => taskEither.of<number, number>(l.length))
+    const tr = r.orElse(() => taskEither.of<number, number>(2))
     return Promise.all([tl.run(), tr.run()]).then(([el, er]) => {
       assert.deepEqual(el, right(3))
       assert.deepEqual(er, right(1))
