ce150a7faeb2d98a8682cba6b97f8d07b9b9e133
gcanti <giulio.canti@gmail.com>
2018-05-13 08:57:07 +0200

    Tests: 100% coverage Exception

--- test/Exception.ts
+++ test/Exception.ts
@@ -1,36 +1,67 @@
 import * as assert from 'assert'
-import * as exception from '../src/Exception'
+import { catchError, error, message, stack, throwError, tryCatch } from '../src/Exception'
 import { IO } from '../src/IO'
 
 describe('Exception', () => {
   it('error', () => {
     const message = 'bum!'
-    const e = exception.error(message)
+    const e = error(message)
     assert.strictEqual(e instanceof Error, true)
     assert.strictEqual(e.message, message)
   })
 
   it('message', () => {
-    const message = 'bum!'
-    const e = new Error(message)
-    assert.strictEqual(exception.message(e), message)
+    const m = 'bum!'
+    const e = new Error(m)
+    assert.strictEqual(message(e), m)
+  })
+
+  it('stack', () => {
+    try {
+      throw new Error('bum!')
+    } catch (e) {
+      assert.strictEqual(
+        stack(e)
+          .filter(s => s.startsWith('Error: bum!'))
+          .isSome(),
+        true
+      )
+    }
+    const e = new Error('bum!')
+    delete e.stack
+    assert.strictEqual(stack(e).isNone(), true)
   })
 
   it('throwError', () => {
-    const eio = exception.throwError<number>(new Error('bum!'))
+    const eio = throwError<number>(new Error('bum!'))
     assert.throws(() => eio.run())
   })
 
   it('catchError', () => {
-    const eio = exception.throwError<number>(new Error('bum!'))
-    const io = exception.catchError(eio, e => new IO(() => 1))
-    assert.strictEqual(io.run(), 1)
+    assert.strictEqual(
+      catchError(
+        new IO(() => {
+          throw new Error('bum!')
+        }),
+        e => new IO(() => 1)
+      ).run(),
+      1
+    )
+    assert.strictEqual(
+      catchError(
+        new IO(() => {
+          throw 'bum!'
+        }),
+        e => new IO(() => 1)
+      ).run(),
+      1
+    )
   })
 
   it('tryCatch', () => {
-    const eiol = exception.tryCatch(exception.throwError<number>(new Error('bum!')))
+    const eiol = tryCatch(throwError<number>(new Error('bum!')))
     assert.strictEqual(eiol.run().isLeft(), true)
-    const eior = exception.tryCatch(new IO(() => 1))
+    const eior = tryCatch(new IO(() => 1))
     assert.strictEqual(eior.run().isRight(), true)
   })
 })
