8dd0e873f949cc07c4736bc5559465ee1786a8e0
gcanti <giulio.canti@gmail.com>
2018-05-13 14:47:00 +0200

    Tests: 100% coverage State

--- test/State.ts
+++ test/State.ts
@@ -1,7 +1,15 @@
 import * as assert from 'assert'
-import { State, get, gets, modify, put } from '../src/State'
+import { State, get, gets, modify, put, state } from '../src/State'
 
 describe('State', () => {
+  it('eval', () => {
+    assert.deepEqual(state.of(1).eval(0), 1)
+  })
+
+  it('exec', () => {
+    assert.deepEqual(state.of(1).exec(0), 0)
+  })
+
   it('put', () => {
     assert.deepEqual(put(2).run(1), [undefined, 2])
   })
@@ -22,13 +30,24 @@ describe('State', () => {
 
   it('map', () => {
     const double = (n: number) => n * 2
-    const state = new State<number, number>(s => [s - 1, s + 1])
-    assert.deepEqual(state.map(double).run(0), [-2, 1])
+    const x = new State<number, number>(s => [s - 1, s + 1])
+    assert.deepEqual(x.map(double).run(0), [-2, 1])
+    assert.deepEqual(state.map(x, double).run(0), [-2, 1])
+  })
+
+  it('ap', () => {
+    const double = (n: number) => n * 2
+    const fab = state.of(double)
+    const fa = state.of(1)
+    assert.deepEqual(fa.ap(fab).run(0), [2, 0])
+    assert.deepEqual(state.ap(fab, fa).run(0), [2, 0])
+    assert.deepEqual(fab.ap_(fa).run(0), [2, 0])
   })
 
   it('chain', () => {
     const f = (n: number) => new State<number, number>(s => [s - 1, s + 1])
-    const state = new State<number, number>(s => [s - 1, s + 1])
-    assert.deepEqual(state.chain(f).run(0), [0, 2])
+    const x = new State<number, number>(s => [s - 1, s + 1])
+    assert.deepEqual(x.chain(f).run(0), [0, 2])
+    assert.deepEqual(state.chain(x, f).run(0), [0, 2])
   })
 })
