1a07da75fc272223280fc2d2109563e494c0a46f
gcanti <giulio.canti@gmail.com>
2018-05-13 11:47:33 +0200

    Tests: 100% coverage Validation

--- test/Validation.ts
+++ test/Validation.ts
@@ -1,42 +1,40 @@
 import * as assert from 'assert'
 import { array } from '../src/Array'
 import * as either from '../src/Either'
-import { monoidString } from '../src/Monoid'
+import { monoidString, monoidSum } from '../src/Monoid'
+import { none, option, some } from '../src/Option'
 import { getArraySemigroup, semigroupString } from '../src/Semigroup'
 import { setoidNumber, setoidString } from '../src/Setoid'
-import { sequence } from '../src/Traversable'
+import { sequence, traverse } from '../src/Traversable'
 import {
   failure,
   fromEither,
+  fromPredicate,
   getAlt,
   getApplicative,
   getMonad,
   getSemigroup,
   getSetoid,
-  success
+  isFailure,
+  isSuccess,
+  success,
+  validation,
+  getMonoid
 } from '../src/Validation'
 
 describe('Validation', () => {
-  it('chain', () => {
+  it('getMonad', () => {
     const M = getMonad(monoidString)
     const f = (s: string) => success<string, number>(s.length)
     assert.deepEqual(M.chain(success<string, string>('abc'), f), success(3))
     assert.deepEqual(M.chain(failure<string, string>('a'), f), failure('a'))
     assert.deepEqual(M.chain(failure<string, string>('a'), () => failure('b')), failure('a'))
-  })
-
-  it('of', () => {
-    const { of } = getApplicative(monoidString)
-    assert.deepEqual(of(1), success(1))
-  })
-
-  it('ap', () => {
-    const { ap } = getApplicative(monoidString)
+    assert.deepEqual(M.of(1), success(1))
     const double = (n: number) => n * 2
-    assert.deepEqual(ap(success(double), success(1)), success(2))
-    assert.deepEqual(ap(success(double), failure('foo')), failure('foo'))
-    assert.deepEqual(ap(failure<string, (n: number) => number>('foo'), success(1)), failure('foo'))
-    assert.deepEqual(ap(failure<string, (n: number) => number>('foo'), failure('bar')), failure('foobar'))
+    assert.deepEqual(M.ap(success(double), success(1)), success(2))
+    assert.deepEqual(M.ap(success(double), failure('foo')), failure('foo'))
+    assert.deepEqual(M.ap(failure<string, (n: number) => number>('foo'), success(1)), failure('foo'))
+    assert.deepEqual(M.ap(failure<string, (n: number) => number>('foo'), failure('bar')), failure('foobar'))
   })
 
   it('traverse', () => {
@@ -100,5 +98,80 @@ describe('Validation', () => {
     assert.deepEqual(alt.alt(failure([1]), success('a')), success('a'))
     assert.deepEqual(alt.alt(success('a'), failure([1])), success('a'))
     assert.deepEqual(alt.alt(failure([1]), failure([2])), failure([1, 2]))
+  })
+
+  it('reduce', () => {
+    assert.deepEqual(success('bar').reduce('foo', (b, a) => b + a), 'foobar')
+    assert.deepEqual(failure('bar').reduce('foo', (b, a) => b + a), 'foo')
+    assert.deepEqual(validation.reduce(success('bar'), 'foo', (b, a) => b + a), 'foobar')
+  })
+
+  it('mapFailure', () => {
+    const double = (n: number): number => n * 2
+    assert.deepEqual(success<number, string>('bar').mapFailure(double), success('bar'))
+    assert.deepEqual(failure<number, string>(2).mapFailure(double), failure(4))
+  })
+
+  it('toString', () => {
+    assert.strictEqual(success('bar').toString(), 'success("bar")')
+    assert.strictEqual(success('bar').inspect(), 'success("bar")')
+    assert.strictEqual(failure('bar').toString(), 'failure("bar")')
+    assert.strictEqual(failure('bar').inspect(), 'failure("bar")')
+  })
+
+  it('swap', () => {
+    assert.deepEqual(success('bar').swap(), failure('bar'))
+    assert.deepEqual(failure('bar').swap(), success('bar'))
+  })
+
+  it('isFailure', () => {
+    assert.strictEqual(success(1).isFailure(), false)
+    assert.strictEqual(failure(1).isFailure(), true)
+    assert.strictEqual(isFailure(success(1)), false)
+    assert.strictEqual(isFailure(failure(1)), true)
+  })
+
+  it('isSuccess', () => {
+    assert.strictEqual(success(1).isSuccess(), true)
+    assert.strictEqual(failure(1).isSuccess(), false)
+    assert.strictEqual(isSuccess(success(1)), true)
+    assert.strictEqual(isSuccess(failure(1)), false)
+  })
+
+  it('fold', () => {
+    const f = (s: string) => `failure${s.length}`
+    const g = (s: string) => `success${s.length}`
+    assert.strictEqual(failure<string, string>('abc').fold(f, g), 'failure3')
+    assert.strictEqual(success<string, string>('abc').fold(f, g), 'success3')
+  })
+
+  it('bimap', () => {
+    const f = (s: string): number => s.length
+    const g = (n: number): boolean => n > 2
+    assert.deepEqual(success<string, number>(1).bimap(f, g), success(false))
+    assert.deepEqual(failure<string, number>('foo').bimap(f, g), failure(3))
+    assert.deepEqual(validation.bimap(success<string, number>(1), f, g), success(false))
+  })
+
+  it('fromPredicate', () => {
+    const predicate = (n: number) => n >= 2
+    const handleError = (n: number) => `Invalid number ${n}`
+    const gt2 = fromPredicate(predicate, handleError)
+    assert.deepEqual(gt2(3), success(3))
+    assert.deepEqual(gt2(1), failure('Invalid number 1'))
+  })
+
+  it('traverse', () => {
+    assert.deepEqual(traverse(option, validation)(failure('foo'), a => (a >= 2 ? some(a) : none)), some(failure('foo')))
+    assert.deepEqual(traverse(option, validation)(success(1), a => (a >= 2 ? some(a) : none)), none)
+    assert.deepEqual(traverse(option, validation)(success(3), a => (a >= 2 ? some(a) : none)), some(success(3)))
+  })
+
+  it('getMonoid', () => {
+    const M = getMonoid(monoidString, monoidSum)
+    assert.deepEqual(M.concat(success(1), success(2)), success(3))
+    assert.deepEqual(M.concat(success(1), failure('foo')), failure('foo'))
+    assert.deepEqual(M.concat(failure('foo'), success(1)), failure('foo'))
+    assert.deepEqual(M.concat(failure('foo'), failure('bar')), failure('foobar'))
   })
 })
