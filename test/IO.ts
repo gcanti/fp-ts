709d2ce677f927ad1f0d462cc69ab2527b514978
gcanti <giulio.canti@gmail.com>
2018-05-13 14:13:12 +0200

    Tests: 100% coverage IO

--- test/IO.ts
+++ test/IO.ts
@@ -1,16 +1,43 @@
 import * as assert from 'assert'
-import { IO, getSemigroup } from '../src/IO'
-import { semigroupVoid } from '../src/Semigroup'
+import { IO, getSemigroup, io, getMonoid } from '../src/IO'
+import { semigroupSum } from '../src/Semigroup'
+import { monoidSum } from '../src/Monoid'
 
 describe('IO', () => {
+  it('ap', () => {
+    const double = (n: number): number => n * 2
+    const fab = io.of(double)
+    const fa = io.of(1)
+    assert.strictEqual(fa.ap(fab).run(), 2)
+    assert.strictEqual(fab.ap_(fa).run(), 2)
+  })
+
+  it('chain', () => {
+    const f = (n: number) => io.of(n * 2)
+    assert.strictEqual(
+      io
+        .of(1)
+        .chain(f)
+        .run(),
+      2
+    )
+    assert.strictEqual(io.chain(io.of(1), f).run(), 2)
+  })
+
   it('getSemigroup', () => {
-    const S = getSemigroup(semigroupVoid)
+    const S = getSemigroup(semigroupSum)
     const log: Array<string> = []
-    const append = (message: string) =>
-      new IO(() => {
-        log.push(message)
-      })
-    S.concat(append('a'), append('b')).run()
+    const append = (message: string): IO<number> => new IO(() => log.push(message))
+    assert.strictEqual(S.concat(append('a'), append('b')).run(), 3)
+    assert.deepEqual(log, ['a', 'b'])
+  })
+
+  it('getMonoid', () => {
+    const M = getMonoid(monoidSum)
+    const log: Array<string> = []
+    const append = (message: string): IO<number> => new IO(() => log.push(message))
+    assert.strictEqual(M.concat(append('a'), M.empty).run(), 1)
+    assert.strictEqual(M.concat(M.empty, append('b')).run(), 2)
     assert.deepEqual(log, ['a', 'b'])
   })
 
@@ -24,5 +51,10 @@ describe('IO', () => {
       2
     )
     assert.deepEqual(log, ['a', 'b'])
+  })
+
+  it('toString', () => {
+    assert.strictEqual(io.of(1).toString(), 'new IO(<function0>)')
+    assert.strictEqual(io.of(1).inspect(), 'new IO(<function0>)')
   })
 })
