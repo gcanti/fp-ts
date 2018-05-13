6c42476ce5dcdb2cce42bde951368fd6e67b3990
gcanti <giulio.canti@gmail.com>
2018-05-13 08:46:54 +0200

    Tests: 100% coverage Writer

--- test/Writer.ts
+++ test/Writer.ts
@@ -1,8 +1,32 @@
 import * as assert from 'assert'
-import { Writer, censor, listen, listens, pass, tell } from '../src/Writer'
+import { Writer, censor, listen, listens, pass, tell, getMonad, writer } from '../src/Writer'
 import { tuple } from '../src/function'
+import { monoidString } from '../src/Monoid'
 
 describe('Writer', () => {
+  it('eval', () => {
+    const fa = new Writer(() => [1, 'a'])
+    assert.strictEqual(fa.eval(), 1)
+  })
+
+  it('exec', () => {
+    const fa = new Writer(() => [1, 'a'])
+    assert.strictEqual(fa.exec(), 'a')
+  })
+
+  it('map', () => {
+    const fa = new Writer(() => [1, 'a'])
+    const double = (n: number): number => n * 2
+    assert.deepEqual(fa.map(double).run(), [2, 'a'])
+    assert.deepEqual(writer.map(fa, double).run(), [2, 'a'])
+  })
+
+  it('map', () => {
+    const fa = new Writer(() => [1, 'a'])
+    const double = (n: number): number => n * 2
+    assert.deepEqual(fa.map(double).run(), [2, 'a'])
+  })
+
   it('tell', () => {
     assert.deepEqual(tell(1).run(), [undefined, 1])
   })
@@ -25,5 +49,14 @@ describe('Writer', () => {
   it('censor', () => {
     const fa = new Writer(() => [1, ['a', 'b']])
     assert.deepEqual(censor(fa, w => w.filter(a => a !== 'a')).run(), [1, ['b']])
+  })
+
+  it('getMonad', () => {
+    const M = getMonad(monoidString)
+    assert.deepEqual(M.of(1).run(), [1, ''])
+    const double = (n: number): number => n * 2
+    assert.deepEqual(M.ap(M.of(double), M.of(1)).run(), [2, ''])
+    const f = (n: number) => M.of(n * 2)
+    assert.deepEqual(M.chain(M.of(1), f).run(), [2, ''])
   })
 })
