2cb3f9f9330857d7ecadfeb64f749a46041f61e4
gcanti <giulio.canti@gmail.com>
2018-05-13 14:40:59 +0200

    Tests: 100% coverage StateT

--- test/StateT.ts
+++ test/StateT.ts
@@ -30,6 +30,18 @@ describe('StateT', () => {
     assert.deepEqual(stateTOption.map(double, state)(0), option.some([-2, 1]))
   })
 
+  it('of', () => {
+    const x = stateTOption.of(1)
+    assert.deepEqual(x(0), option.some([1, 0]))
+  })
+
+  it('ap', () => {
+    const double = (n: number) => n * 2
+    const fab = stateTOption.of(double)
+    const fa = stateTOption.of(1)
+    assert.deepEqual(stateTOption.ap(fab, fa)(0), option.some([2, 0]))
+  })
+
   it('chain', () => {
     const f = (n: number) => (s: number) => option.some<[number, number]>([s - 1, s + 1])
     const state = (s: number) => option.some<[number, number]>([s - 1, s + 1])
