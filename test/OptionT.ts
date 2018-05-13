9d16f456968226cc07d7bf8b76ab02e4f14dc01a
gcanti <giulio.canti@gmail.com>
2018-05-13 15:39:17 +0200

    Tests: 100% coverage OptionT

--- test/OptionT.ts
+++ test/OptionT.ts
@@ -1,10 +1,10 @@
 import * as assert from 'assert'
 import * as option from '../src/Option'
 import * as optionT from '../src/OptionT'
-import * as task from '../src/Task'
+import { task } from '../src/Task'
 
-const taskOption = optionT.getOptionT(task.task)
-const none = optionT.none(task.task)()
+const taskOption = optionT.getOptionT(task)
+const none = optionT.none(task)()
 
 describe('OptionT', () => {
   it('map', () => {
@@ -28,13 +28,13 @@ describe('OptionT', () => {
     const f = 'none'
     const g = (s: string) => `some${s.length}`
     const p1 = optionT
-      .fold(task.task)(f, g, none)
+      .fold(task)(f, g, none)
       .run()
       .then(s => {
         assert.strictEqual(s, 'none')
       })
     const p2 = optionT
-      .fold(task.task)(f, g, taskOption.of('s'))
+      .fold(task)(f, g, taskOption.of('s'))
       .run()
       .then(s => {
         assert.strictEqual(s, 'some1')
@@ -44,7 +44,7 @@ describe('OptionT', () => {
 
   it('getOrElse', () => {
     const greetingT = taskOption.of('welcome')
-    const getOrElse = optionT.getOrElse(task.task)('hello, there!')
+    const getOrElse = optionT.getOrElse(task)('hello, there!')
     const p1 = getOrElse(greetingT)
       .run()
       .then(s => {
@@ -56,5 +56,31 @@ describe('OptionT', () => {
         assert.strictEqual(s, 'hello, there!')
       })
     return Promise.all([p1, p2])
+  })
+
+  it('some', () => {
+    const some = optionT.some(task)
+    return some(1)
+      .run()
+      .then(o => {
+        assert.deepEqual(o, option.some(1))
+      })
+  })
+
+  it('fromOption', () => {
+    const fromOption = optionT.fromOption(task)
+    return Promise.all([fromOption(option.some(1)).run(), fromOption(option.none).run()]).then(([o1, o2]) => {
+      assert.deepEqual(o1, option.some(1))
+      assert.deepEqual(o2, option.none)
+    })
+  })
+
+  it('liftF', () => {
+    const liftF = optionT.liftF(task)
+    return liftF(task.of(1))
+      .run()
+      .then(o => {
+        assert.deepEqual(o, option.some(1))
+      })
   })
 })
