0d0c487a57c41b863aa828216471ce3ed7cff55a
gcanti <giulio.canti@gmail.com>
2018-05-13 09:13:41 +0200

    Tests: 100% coverage Ring

--- test/Ring.ts
+++ test/Ring.ts
@@ -1,14 +1,26 @@
 import * as assert from 'assert'
 import { fieldNumber } from '../src/Field'
-import { getProductRing } from '../src/Ring'
+import { getProductRing, negate, getFunctionRing } from '../src/Ring'
 
 describe('Ring', () => {
   it('getProductRing', () => {
-    const ring = getProductRing(fieldNumber, fieldNumber)
-    assert.deepEqual(ring.add([1, 2], [3, 4]), [4, 6])
-    assert.deepEqual(ring.mul([1, 2], [3, 4]), [3, 8])
-    assert.deepEqual(ring.one, [1, 1])
-    assert.deepEqual(ring.sub([1, 2], [3, 4]), [-2, -2])
-    assert.deepEqual(ring.zero, [0, 0])
+    const R = getProductRing(fieldNumber, fieldNumber)
+    assert.deepEqual(R.add([1, 2], [3, 4]), [4, 6])
+    assert.deepEqual(R.mul([1, 2], [3, 4]), [3, 8])
+    assert.deepEqual(R.one, [1, 1])
+    assert.deepEqual(R.sub([1, 2], [3, 4]), [-2, -2])
+    assert.deepEqual(R.zero, [0, 0])
+  })
+
+  it('negate', () => {
+    assert.strictEqual(negate(fieldNumber)(1), -1)
+  })
+
+  it('getFunctionRing', () => {
+    const R = getFunctionRing<string, number>(fieldNumber)
+    const f1 = (s: string): number => s.length
+    const f2 = (s: string): number => s.indexOf('a')
+    assert.strictEqual(R.sub(f1, f2)('foo'), 4)
+    assert.strictEqual(R.sub(f1, f2)('fooa'), 1)
   })
 })
