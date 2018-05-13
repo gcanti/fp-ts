ea198a51b890b10d751813fc5ebd08d319768f3f
gcanti <giulio.canti@gmail.com>
2018-05-13 16:01:34 +0200

    Tests: 100% coverage Monoidal

--- test/Monoidal.ts
+++ test/Monoidal.ts
@@ -5,19 +5,21 @@ import { none, option, some } from '../src/Option'
 
 describe('Monoidal', () => {
   it('fromApplicative', () => {
-    const monoidalOption = fromApplicative(option)
-    assert.deepEqual(monoidalOption.mult(some(1), some('a')), some([1, 'a']))
-    assert.deepEqual(monoidalOption.mult(some(1), none), none)
+    const M = fromApplicative(option)
+    assert.deepEqual(M.mult(some(1), some('a')), some([1, 'a']))
+    assert.deepEqual(M.mult(some(1), none), none)
+    assert.deepEqual(M.unit(), some(undefined))
     const monoidalEither = fromApplicative(either)
     assert.deepEqual(monoidalEither.mult(right(1), right('a')), right([1, 'a']))
     assert.deepEqual(monoidalEither.mult(right(1), left('error')), left('error'))
   })
 
   it('toApplicative', () => {
-    const monoidalOption = fromApplicative(option)
-    const applicative = toApplicative(monoidalOption)
+    const M = fromApplicative(option)
+    const F = toApplicative(M)
     const double = (n: number) => n * 2
-    assert.deepEqual(applicative.ap(option.of(double), some(1)), some(2))
-    assert.deepEqual(applicative.ap(option.of(double), none), none)
+    assert.deepEqual(F.ap(option.of(double), some(1)), some(2))
+    assert.deepEqual(F.ap(option.of(double), none), none)
+    assert.deepEqual(F.of(1), some(1))
   })
 })
