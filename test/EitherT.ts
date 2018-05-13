08bea1be697af356f4ef6051bb6f39a1e62f8b8e
gcanti <giulio.canti@gmail.com>
2018-05-13 15:58:29 +0200

    Tests: 100% coverage EitherT

---
+++ test/EitherT.ts
@@ -0,0 +1,18 @@
+import * as assert from 'assert'
+import * as eitherT from '../src/EitherT'
+import * as either from '../src/Either'
+import { task } from '../src/Task'
+
+describe('EitherT', () => {
+  it('chain', () => {
+    const chain = eitherT.chain(task)
+    const of = eitherT.getEitherT(task).of
+    const f = (n: number) => of(n * 2)
+    const x = of(1)
+    const y = eitherT.fromEither(task)<string, number>(either.left('foo'))
+    return Promise.all([chain(f, x).run(), chain(f, y).run()]).then(([e1, e2]) => {
+      assert.deepEqual(e1, either.right(2))
+      assert.deepEqual(e2, either.left('foo'))
+    })
+  })
+})
