fc2167b780a39308f332d108189435d3bfc03004
gcanti <giulio.canti@gmail.com>
2018-05-18 09:08:19 +0200

    Tests: add test checking exported modules in index.ts

---
+++ test/index.ts
@@ -0,0 +1,35 @@
+import * as assert from 'assert'
+import { getModuleNames } from '../docs/api/fs'
+
+const getExportName = (name: string): string => {
+  if (name === 'HKT' || name === 'IO') {
+    return name.toLowerCase()
+  }
+  if (name === 'IOEither') {
+    return 'ioEither'
+  }
+  if (name === 'TaskEither') {
+    return 'taskEither'
+  }
+  if (name === 'StrMap') {
+    return 'strmap'
+  }
+  return name.substring(0, 1).toLowerCase() + name.substring(1)
+}
+
+describe('index', () => {
+  it('check exported modules', () => {
+    const moduleNames = getModuleNames()
+    const fp = require('../src')
+    moduleNames.forEach(name => {
+      if (name !== 'index') {
+        const exportName = getExportName(name)
+        assert.strictEqual(
+          fp[exportName] !== undefined,
+          true,
+          `The "${name}" module is not exported in src/index.ts as ${exportName}`
+        )
+      }
+    })
+  })
+})
