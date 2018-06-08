bfaa8aceac118d9116250d90ea64e94590708cbe
gcanti <giulio.canti@gmail.com>
2018-06-08 16:54:53 +0200

    Tests: 100% coverage Random module

---
+++ test/Random.ts
@@ -0,0 +1,29 @@
+import * as assert from 'assert'
+import { random, randomInt, randomBool, randomRange } from '../src/Random'
+
+describe('Random', () => {
+  it('random', () => {
+    const n = random.run()
+    assert.strictEqual(typeof n, 'number')
+  })
+
+  it('randomInt', () => {
+    const n = randomInt(0, 10).run()
+    assert.strictEqual(typeof n, 'number')
+    assert.strictEqual(n % 1 === 0, true)
+    assert.strictEqual(n >= 0, true)
+    assert.strictEqual(n <= 10, true)
+  })
+
+  it('randomRange', () => {
+    const n = randomRange(0, 10).run()
+    assert.strictEqual(typeof n, 'number')
+    assert.strictEqual(n >= 0, true)
+    assert.strictEqual(n <= 10, true)
+  })
+
+  it('randomBool', () => {
+    const b = randomBool.run()
+    assert.strictEqual(typeof b, 'boolean')
+  })
+})
