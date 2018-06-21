7c79020dc452574401a7d746b3db4123100ac127
Kirill Agalakov <agalakov@devexperts.com>
2018-06-21 19:21:41 +0300

    feature: Witherable // relax Compactable requirements

--- test/Witherable.ts
+++ test/Witherable.ts
@@ -9,14 +9,34 @@ describe('Witherable', () => {
   it('wiltDefault', () => {
     const f = (x: number) => (x > 2 ? new Identity(right(x * 10)) : new Identity(left(x)))
     const list = [1, 2, 3]
-    const wilt = wiltDefault(array, I)
+    const { URI, map, reduce, traverse, separate } = array
+    const wilt = wiltDefault(
+      {
+        URI,
+        map,
+        reduce,
+        traverse,
+        separate
+      },
+      I
+    )
     assert.deepEqual(wilt(list, f), new Identity({ left: [1, 2], right: [30] }))
   })
 
   it('witherDefault', () => {
     const f = (x: number) => (x > 2 ? new Identity(some(x * 10)) : new Identity(none))
     const list = [1, 2, 3]
-    const wither = witherDefault(array, I)
+    const { URI, map, reduce, traverse, compact } = array
+    const wither = witherDefault(
+      {
+        URI,
+        map,
+        reduce,
+        traverse,
+        compact
+      },
+      I
+    )
     const result = wither(list, f)
     const expected = new Identity([30])
     assert.deepEqual(result, expected)
