75598e23db6af1049e1a9e9fbfe4ab8737ad8da1
gcanti <giulio.canti@gmail.com>
2018-07-15 16:22:16 +0200

    Polish: remove old comment

--- src/HKT.ts
+++ src/HKT.ts
@@ -25,8 +25,6 @@ export type URIS3 = (URI2HKT3<any, any, any> & { never: HKT<never, never> })[
   | keyof URI2HKT3<any, any, any>
   | 'never']['_URI']
 
-// HKTAs<U, A> is the same as URI2HKT<A>[U], but checks for URI constraints
-
 export type Type<URI extends URIS, A> = URI2HKT<A>[URI]
 export type Type2<URI extends URIS2, L, A> = URI2HKT2<L, A>[URI]
 export type Type3<URI extends URIS3, U, L, A> = URI2HKT3<U, L, A>[URI]
