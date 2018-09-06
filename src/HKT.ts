60142486518c951d6e64c80a0bd1d0f19a3a5731
Babak <badaei@yahoo.com>
2018-09-06 03:06:00 -0700

    Update HKT.ts

--- src/HKT.ts
+++ src/HKT.ts
@@ -25,15 +25,15 @@ export type URIS2 = (URI2HKT2<any, any> & { never: HKT<never, never> })[keyof UR
 export type URIS3 = (URI2HKT3<any, any, any> & { never: HKT<never, never> })[
   | keyof URI2HKT3<any, any, any>
   | 'never']['_URI']
-export type URIS4 = (URI2HKT3<any, any, any, any> & { never: HKT<never, never> })[
-  | keyof URI2HKT3<any, any, any, any>
+export type URIS4 = (URI2HKT4<any, any, any, any> & { never: HKT<never, never> })[
+  | keyof URI2HKT4<any, any, any, any>
   | 'never']['_URI']
 
 
 export type Type<URI extends URIS, A> = {} & URI2HKT<A>[URI]
 export type Type2<URI extends URIS2, L, A> = {} & URI2HKT2<L, A>[URI]
 export type Type3<URI extends URIS3, U, L, A> = {} & URI2HKT3<U, L, A>[URI]
-export type Type4<URI extends URIS4, V, U, L, A> = {} & URI2HKT3<V, U, L, A>[URI]
+export type Type4<URI extends URIS4, V, U, L, A> = {} & URI2HKT4<V, U, L, A>[URI]
 
 // Type-level integrity check
 
