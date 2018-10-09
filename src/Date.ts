f1de1e65090417db054df15f3791581ba9a277a7
gcanti <giulio.canti@gmail.com>
2018-10-09 08:34:46 +0200

    rename now to create + docs

--- src/Date.ts
+++ src/Date.ts
@@ -1,3 +1,15 @@
 import { IO } from './IO'
 
-export const now: IO<Date> = new IO(() => new Date())
+/**
+ * Returns the current `Date`
+ * @constant
+ * @since 1.10.0
+ */
+export const create: IO<Date> = new IO(() => new Date())
+
+/**
+ * Returns the number of milliseconds elapsed since January 1, 1970, 00:00:00 UTC
+ * @constant
+ * @since 1.10.0
+ */
+export const now: IO<number> = new IO(() => new Date().getTime())
