b881bc535c707bcba8596039a8534d6b48f62b4a
gcanti <giulio.canti@gmail.com>
2018-10-23 09:54:39 +0200

    add TraverseComposition1, SequenceComposition1 and an example of getTraversableComposition to Traversable2v

--- test/Traversable2v.ts
+++ test/Traversable2v.ts
@@ -1,5 +1,4 @@
 import * as assert from 'assert'
-import { Applicative } from '../src/Applicative'
 import { array } from '../src/Array'
 import { none, option, some } from '../src/Option'
 import { getTraversableComposition } from '../src/Traversable2v'
@@ -10,18 +9,14 @@ export type ArrayOptionURI = typeof ArrayOptionURI
 
 describe('Traversable2v', () => {
   it('getTraversableComposition', () => {
-    const o: Applicative<'Option'> = option as any
-    const arrayOptionTraversable = getTraversableComposition(array, option)
+    const T = getTraversableComposition(array, option)
     assert.deepEqual(
-      arrayOptionTraversable.traverse(o)([some(1), some(2)], (n: number) => (n <= 2 ? some(n * 2) : none)),
+      T.traverse(option)([some(1), some(2)], (n: number) => (n <= 2 ? some(n * 2) : none)),
       some([some(2), some(4)])
     )
-    assert.deepEqual(
-      arrayOptionTraversable.traverse(o)([some(1), some(3)], (n: number) => (n <= 2 ? some(n * 2) : none)),
-      none
-    )
-    assert.deepEqual(arrayOptionTraversable.sequence(o)([some(some(1)), some(some(2))]), some([some(1), some(2)]))
-    assert.deepEqual(arrayOptionTraversable.sequence(o)([some(some(1)), none]), some([some(1), none]))
-    assert.deepEqual(arrayOptionTraversable.sequence(o)([some(some(1)), some(none)]), none)
+    assert.deepEqual(T.traverse(option)([some(1), some(3)], (n: number) => (n <= 2 ? some(n * 2) : none)), none)
+    assert.deepEqual(T.sequence(option)([some(some(1)), some(some(2))]), some([some(1), some(2)]))
+    assert.deepEqual(T.sequence(option)([some(some(1)), none]), some([some(1), none]))
+    assert.deepEqual(T.sequence(option)([some(some(1)), some(none)]), none)
   })
 })
