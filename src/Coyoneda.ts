eb5e2b52026118e12626f78f2bd78b869fc43435
Alcala, Jose Luis <JoseLuis.Alcala@adidas-group.com>
2018-11-02 20:27:34 +0100

    WIP:Simplified

--- src/Coyoneda.ts
+++ src/Coyoneda.ts
@@ -1,35 +1,21 @@
-import { Functor2 } from './Functor'
-import { HKT } from './HKT'
+import { compose,identity } from './function'
 
-declare module './HKT' {
-  interface URI2HKT2<L, A> {
-    Coyoneda: Coyoneda<L, A,any>
+export class Coyoneda {
+
+  constructor(readonly x: any, readonly f: any) {}
+
+  map<A,B>(f: (a: A) => B): Coyoneda {
+    return new Coyoneda(this.x, compose(this.f,f))
+  }
+
+  lower() {
+    return this.x.map(this.f)
   }
 }
 
-export const URI = 'Coyoneda'
-export type URI = typeof URI
-
-export class Coyoneda<F, A, X> {
-  readonly _A!: A
-  readonly _L!: F
-  readonly _URI!: URI
-  constructor(readonly fx: HKT<F, X>, readonly f: (x: X) => Coyoneda<F, A, X>) {}
-
-  map<B>(f: (a: A) => B): Coyoneda<F, B, X> {
-    return new Coyoneda(this.fx, x => this.f(x).map(f))
-  }
-
-}
-
-// export const lift = <A>(hack: A) => new Coyoneda(hack)
-const map = <F,A, B>(fa: Coyoneda<F, A,any>, f: (a: A) => B): Coyoneda<F, B,any> => {
-  return fa.map(f)
-}
-
-export const lift = <F>(fa: HKT<F, any>): Coyoneda<F,any,any> => {
-  const s = new Coyoneda(fa, a => a)
+export const lift = <A>(val: A): Coyoneda => {
+  const s = new Coyoneda(val, identity)
   return s
 }
 
-export const coyoneda: Functor2<URI> = { URI, map }
+// export const coyoneda: Functor2<URI> = { URI, map }
--- test/Coyoneda.ts
+++ test/Coyoneda.ts
@@ -1,7 +1,20 @@
 import { lift } from '../src/Coyoneda'
 
 describe('Coyoneda',() => {
-  it('CoyonedaIt',() => {
-    const coyo = lift({}).map
+  it('Executes only one loop',() => {
+    const coyo = lift([2,2]).map(x => {
+      console.log(`Entra con ${x}`)
+      return x + 1
+    }).map(x => {
+      console.log(`Entra con ${x}`)
+      return x + 2
+    })
+    const res = coyo.x.map(coyo.f)
+    expect(res).toEqual([5,5])
+  })
+  it('Project something into a Functor',() => {
+    const coyo = lift({ a: 1 }).map(x => x.a + 2)
+    const res = coyo.f(coyo.x)
+    expect(res).toEqual(3)
   })
 })
