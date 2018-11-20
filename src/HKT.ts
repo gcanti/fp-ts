8fa7b8b7eea4a0876d2d58b44d1b5925259a9978
gcanti <giulio.canti@gmail.com>
2018-11-20 14:46:40 +0100

    Polish: rename V to X (HKT4)

--- src/HKT.ts
+++ src/HKT.ts
@@ -11,8 +11,8 @@ export interface HKT3<URI, U, L, A> extends HKT2<URI, L, A> {
   readonly _U: U
 }
 
-export interface HKT4<URI, V, U, L, A> extends HKT3<URI, U, L, A> {
-  readonly _V: V
+export interface HKT4<URI, X, U, L, A> extends HKT3<URI, U, L, A> {
+  readonly _X: X
 }
 
 // type-level dictionaries for HKTs
@@ -20,7 +20,7 @@ export interface HKT4<URI, V, U, L, A> extends HKT3<URI, U, L, A> {
 export interface URI2HKT<A> {}
 export interface URI2HKT2<L, A> {}
 export interface URI2HKT3<U, L, A> {}
-export interface URI2HKT4<V, U, L, A> {}
+export interface URI2HKT4<X, U, L, A> {}
 
 // URI constraints with dictionary integrity constraint
 
@@ -36,7 +36,7 @@ export type URIS4 = (URI2HKT4<any, any, any, any> & { never: HKT<never, never> }
 export type Type<URI extends URIS, A> = {} & URI2HKT<A>[URI]
 export type Type2<URI extends URIS2, L, A> = {} & URI2HKT2<L, A>[URI]
 export type Type3<URI extends URIS3, U, L, A> = {} & URI2HKT3<U, L, A>[URI]
-export type Type4<URI extends URIS4, V, U, L, A> = {} & URI2HKT4<V, U, L, A>[URI]
+export type Type4<URI extends URIS4, X, U, L, A> = {} & URI2HKT4<X, U, L, A>[URI]
 
 // Type-level integrity check
 
