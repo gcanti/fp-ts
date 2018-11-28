371e7baa02bb698c8bbf049ea779aca33521449c
gcanti <giulio.canti@gmail.com>
2018-11-28 11:03:04 +0100

    Polish: fix getFoldableComposition overloadings

--- src/Foldable2v.ts
+++ src/Foldable2v.ts
@@ -20,7 +20,11 @@ import {
   FoldableComposition12,
   FoldableComposition21,
   FoldableComposition22,
-  foldMap
+  foldMap,
+  FoldableComposition12C,
+  FoldableComposition2C1,
+  FoldableComposition22C,
+  FoldableComposition3C1
 } from './Foldable'
 import { applyFirst } from './Apply'
 
@@ -70,17 +74,41 @@ export interface Foldable2vComposition11<F extends URIS, G extends URIS> extends
 
 export interface Foldable2vComposition12<F extends URIS, G extends URIS2> extends FoldableComposition12<F, G> {
   readonly foldMap: <M>(M: Monoid<M>) => <L, A>(fa: Type<F, Type2<G, L, A>>, f: (a: A) => M) => M
-  readonly foldr: <L, A, B>(fa: Type<F, Type2<G, L, A>>, b: B, f: (a: A, b: B) => B) => B
+  readonly foldr: <LG, A, B>(fa: Type<F, Type2<G, LG, A>>, b: B, f: (a: A, b: B) => B) => B
+}
+
+export interface Foldable2vComposition12C<F extends URIS, G extends URIS2, LG>
+  extends FoldableComposition12C<F, G, LG> {
+  readonly foldMap: <M>(M: Monoid<M>) => <A>(fa: Type<F, Type2<G, LG, A>>, f: (a: A) => M) => M
+  readonly foldr: <A, B>(fa: Type<F, Type2<G, LG, A>>, b: B, f: (a: A, b: B) => B) => B
 }
 
 export interface Foldable2vComposition21<F extends URIS2, G extends URIS> extends FoldableComposition21<F, G> {
   readonly foldMap: <M>(M: Monoid<M>) => <L, A>(fa: Type2<F, L, Type<G, A>>, f: (a: A) => M) => M
-  readonly foldr: <L, A, B>(fa: Type2<F, L, Type<G, A>>, b: B, f: (a: A, b: B) => B) => B
+  readonly foldr: <LF, A, B>(fa: Type2<F, LF, Type<G, A>>, b: B, f: (a: A, b: B) => B) => B
+}
+
+export interface Foldable2vComposition2C1<F extends URIS2, G extends URIS, LF>
+  extends FoldableComposition2C1<F, G, LF> {
+  readonly foldMap: <M>(M: Monoid<M>) => <A>(fa: Type2<F, LF, Type<G, A>>, f: (a: A) => M) => M
+  readonly foldr: <A, B>(fa: Type2<F, LF, Type<G, A>>, b: B, f: (a: A, b: B) => B) => B
 }
 
 export interface Foldable2vComposition22<F extends URIS2, G extends URIS2> extends FoldableComposition22<F, G> {
   readonly foldMap: <M>(M: Monoid<M>) => <LF, LG, A>(fa: Type2<F, LF, Type2<G, LG, A>>, f: (a: A) => M) => M
   readonly foldr: <LF, LG, A, B>(fa: Type2<F, LF, Type2<G, LG, A>>, b: B, f: (a: A, b: B) => B) => B
+}
+
+export interface Foldable2vComposition22C<F extends URIS2, G extends URIS2, LG>
+  extends FoldableComposition22C<F, G, LG> {
+  readonly foldMap: <M>(M: Monoid<M>) => <LF, A>(fa: Type2<F, LF, Type2<G, LG, A>>, f: (a: A) => M) => M
+  readonly foldr: <LF, A, B>(fa: Type2<F, LF, Type2<G, LG, A>>, b: B, f: (a: A, b: B) => B) => B
+}
+
+export interface Foldable2vComposition3C1<F extends URIS3, G extends URIS, UF, LF>
+  extends FoldableComposition3C1<F, G, UF, LF> {
+  readonly foldMap: <M>(M: Monoid<M>) => <A>(fa: Type3<F, UF, LF, Type<G, A>>, f: (a: A) => M) => M
+  readonly foldr: <LF, A, B>(fa: Type3<F, UF, LF, Type<G, A>>, b: B, f: (a: A, b: B) => B) => B
 }
 
 /**
@@ -99,14 +127,30 @@ export interface Foldable2vComposition22<F extends URIS2, G extends URIS2> exten
  * @function
  * @since 1.10.0
  */
+export function getFoldableComposition<F extends URIS3, G extends URIS, UF, LF>(
+  F: Foldable2v3C<F, UF, LF>,
+  G: Foldable2v1<G>
+): Foldable2vComposition3C1<F, G, UF, LF>
+export function getFoldableComposition<F extends URIS2, G extends URIS2, LG>(
+  F: Foldable2v2<F>,
+  G: Foldable2v2C<G, LG>
+): Foldable2vComposition22C<F, G, LG>
 export function getFoldableComposition<F extends URIS2, G extends URIS2>(
   F: Foldable2v2<F>,
   G: Foldable2v2<G>
 ): Foldable2vComposition22<F, G>
+export function getFoldableComposition<F extends URIS2, G extends URIS, LF>(
+  F: Foldable2v2C<F, LF>,
+  G: Foldable2v1<G>
+): Foldable2vComposition2C1<F, G, LF>
 export function getFoldableComposition<F extends URIS2, G extends URIS>(
   F: Foldable2v2<F>,
   G: Foldable2v1<G>
 ): Foldable2vComposition21<F, G>
+export function getFoldableComposition<F extends URIS, G extends URIS2, LG>(
+  F: Foldable2v1<F>,
+  G: Foldable2v2C<G, LG>
+): Foldable2vComposition12C<F, G, LG>
 export function getFoldableComposition<F extends URIS, G extends URIS2>(
   F: Foldable2v1<F>,
   G: Foldable2v2<G>
