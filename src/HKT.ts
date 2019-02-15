9735d77a29f3e2fabcee96694e907bc697008f70
gcanti <giulio.canti@gmail.com>
2019-02-15 16:33:09 +0100

    drop Type-level integrity check

--- src/HKT.ts
+++ src/HKT.ts
@@ -1,47 +1,63 @@
+//
+// Defunctionalization
+//
+
+// * -> * constructors
 export interface HKT<URI, A> {
   readonly _URI: URI
   readonly _A: A
 }
 
+// * -> * -> * constructors
 export interface HKT2<URI, L, A> extends HKT<URI, A> {
   readonly _L: L
 }
 
+// * -> * -> * -> * constructors
 export interface HKT3<URI, U, L, A> extends HKT2<URI, L, A> {
   readonly _U: U
 }
 
+// * -> * -> * -> * -> * constructors
 export interface HKT4<URI, X, U, L, A> extends HKT3<URI, U, L, A> {
   readonly _X: X
 }
 
-// type-level dictionaries for HKTs
+//
+// inj: type-level dictionaries for HKTs: URI -> concrete type
+//
 
+// * -> * constructors
 export interface URI2HKT<A> {}
+// * -> * -> * constructors
 export interface URI2HKT2<L, A> {}
+// * -> * -> * -> * constructors
 export interface URI2HKT3<U, L, A> {}
+// * -> * -> * -> * -> * constructors
 export interface URI2HKT4<X, U, L, A> {}
 
-// URI constraints with dictionary integrity constraint
+//
+// unions of URIs
+//
 
-export type URIS = (URI2HKT<any> & { never: HKT<never, never> })[keyof URI2HKT<any> | 'never']['_URI']
-export type URIS2 = (URI2HKT2<any, any> & { never: HKT<never, never> })[keyof URI2HKT2<any, any> | 'never']['_URI']
-export type URIS3 = (URI2HKT3<any, any, any> & { never: HKT<never, never> })[
-  | keyof URI2HKT3<any, any, any>
-  | 'never']['_URI']
-export type URIS4 = (URI2HKT4<any, any, any, any> & { never: HKT<never, never> })[
-  | keyof URI2HKT4<any, any, any, any>
-  | 'never']['_URI']
+// * -> * constructors
+export type URIS = keyof URI2HKT<any>
+// * -> * -> * constructors
+export type URIS2 = keyof URI2HKT2<any, any>
+// * -> * -> * -> * constructors
+export type URIS3 = keyof URI2HKT3<any, any, any>
+// * -> * -> * -> * -> * constructors
+export type URIS4 = keyof URI2HKT4<any, any, any, any>
 
+//
+// prj
+//
+
+// * -> * constructors
 export type Type<URI extends URIS, A> = {} & URI2HKT<A>[URI]
+// * -> * -> * constructors
 export type Type2<URI extends URIS2, L, A> = {} & URI2HKT2<L, A>[URI]
+// * -> * -> * -> * constructors
 export type Type3<URI extends URIS3, U, L, A> = {} & URI2HKT3<U, L, A>[URI]
+// * -> * -> * -> * -> * constructors
 export type Type4<URI extends URIS4, X, U, L, A> = {} & URI2HKT4<X, U, L, A>[URI]
-
-// Type-level integrity check
-
-// tslint:disable:no-unused-expression
-(null! as URI2HKT<any>) as { [k in keyof URI2HKT<any>]: HKT<k, any> }
-(null! as URI2HKT2<any, any>) as { [k in keyof URI2HKT2<any, any>]: HKT2<k, any, any> }
-(null! as URI2HKT3<any, any, any>) as { [k in keyof URI2HKT3<any, any, any>]: HKT3<k, any, any, any> }
-(null! as URI2HKT4<any, any, any, any>) as { [k in keyof URI2HKT4<any, any, any, any>]: HKT4<k, any, any, any, any> }
