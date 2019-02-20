13be198944c5ce12df1e75925957fd686230ae7c
gcanti <giulio.canti@gmail.com>
2019-02-20 09:56:10 +0100

    move jest config to its own file

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [151001]
+      }
+    }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts}', '!src/Free.ts', '!src/Store.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: 'test',
+  moduleFileExtensions: ['ts', 'js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0
+    }
+  }
+}
