6c20f657dfc96f3af55ddb58f470d8d5785fed7b
gcanti <giulio.canti@gmail.com>
2019-03-29 19:28:20 +0100

    New Feature: add Traced comonad, closes #798

---
+++ docs/modules/Traced.ts.md
@@ -0,0 +1,127 @@
+---
+title: Traced.ts
+nav_order: 88
+parent: Modules
+---
+
+---
+
+<h2 class="text-delta">Table of contents</h2>
+
+- [URI (type alias)](#uri-type-alias)
+- [Traced (class)](#traced-class)
+  - [map (method)](#map-method)
+- [URI (constant)](#uri-constant)
+- [traced (constant)](#traced-constant)
+- [censor (function)](#censor-function)
+- [getComonad (function)](#getcomonad-function)
+- [listen (function)](#listen-function)
+- [listens (function)](#listens-function)
+- [tracks (function)](#tracks-function)
+
+---
+
+# URI (type alias)
+
+**Signature**
+
+```ts
+export type URI = typeof URI
+```
+
+# Traced (class)
+
+**Signature**
+
+```ts
+export class Traced<P, A> {
+  constructor(readonly run: (p: P) => A) { ... }
+  ...
+}
+```
+
+Added in v1.16.0
+
+## map (method)
+
+**Signature**
+
+```ts
+map<B>(f: (a: A) => B): Traced<P, B> { ... }
+```
+
+# URI (constant)
+
+**Signature**
+
+```ts
+export const URI = ...
+```
+
+# traced (constant)
+
+**Signature**
+
+```ts
+export const traced: Functor2<URI> = ...
+```
+
+Added in v1.16.0
+
+# censor (function)
+
+Apply a function to the current position
+
+**Signature**
+
+```ts
+export const censor = <P, A>(wa: Traced<P, A>, f: (p: P) => P): Traced<P, A> => ...
+```
+
+Added in v1.16.0
+
+# getComonad (function)
+
+**Signature**
+
+```ts
+export function getComonad<P>(monoid: Monoid<P>): Comonad2C<URI, P> { ... }
+```
+
+Added in v1.16.0
+
+# listen (function)
+
+Get the current position
+
+**Signature**
+
+```ts
+export const listen = <P, A>(wa: Traced<P, A>): Traced<P, [A, P]> => ...
+```
+
+Added in v1.16.0
+
+# listens (function)
+
+Get a value which depends on the current position
+
+**Signature**
+
+```ts
+export const listens = <P, A, B>(wa: Traced<P, A>, f: (p: P) => B): Traced<P, [A, B]> => ...
+```
+
+Added in v1.16.0
+
+# tracks (function)
+
+Extracts a value at a relative position which depends on the current value.
+
+**Signature**
+
+```ts
+export const tracks = <P, A>(M: Monoid<P>, f: (a: A) => P) => (wa: Traced<P, A>): A => ...
+```
+
+Added in v1.16.0
---
+++ test/Traced.ts
@@ -0,0 +1,132 @@
+import * as assert from 'assert'
+import { getStructMonoid, monoidAny, Monoid } from '../src/Monoid'
+import { Traced, traced, getComonad, tracks, listen, listens, censor } from '../src/Traced'
+
+// Adapted from https://chshersh.github.io/posts/2019-03-25-comonadic-builders
+
+interface Settings {
+  settingsHasLibrary: boolean
+  settingsGitHub: boolean
+  settingsTravis: boolean
+}
+
+const M: Monoid<Settings> = getStructMonoid({
+  settingsHasLibrary: monoidAny,
+  settingsGitHub: monoidAny,
+  settingsTravis: monoidAny
+})
+
+const C = getComonad(M)
+
+interface Project {
+  projectName: string
+  projectHasLibrary: boolean
+  projectGitHub: boolean
+  projectTravis: boolean
+}
+
+interface ProjectBuilder extends Traced<Settings, Project> {}
+
+const buildProject = (projectName: string): ProjectBuilder =>
+  new Traced(settings => ({
+    projectName,
+    projectHasLibrary: settings.settingsHasLibrary,
+    projectGitHub: settings.settingsGitHub,
+    projectTravis: settings.settingsTravis
+  }))
+
+const hasLibraryB = (wa: ProjectBuilder): Project => {
+  const p = { ...M.empty, settingsHasLibrary: true }
+  return wa.run(p)
+}
+
+const gitHubB = (wa: ProjectBuilder): Project => {
+  const p = { ...M.empty, settingsGitHub: true }
+  return wa.run(p)
+}
+
+const getProjectName = (project: Project): string => project.projectName
+
+describe('Traced', () => {
+  it('map', () => {
+    const wa = buildProject('myproject')
+    assert.deepStrictEqual(traced.map(wa, getProjectName).run(M.empty), 'myproject')
+  })
+
+  it('getComonad', () => {
+    const wa = buildProject('myproject')
+    assert.deepStrictEqual(C.extract(wa), {
+      projectName: 'myproject',
+      projectHasLibrary: false,
+      projectGitHub: false,
+      projectTravis: false
+    })
+    assert.deepStrictEqual(C.extract(C.extend(wa, hasLibraryB)), {
+      projectName: 'myproject',
+      projectHasLibrary: true,
+      projectGitHub: false,
+      projectTravis: false
+    })
+  })
+
+  it('tracks', () => {
+    const travisB = tracks(M, (project: Project): Settings => ({ ...M.empty, settingsTravis: project.projectGitHub }))
+    assert.deepStrictEqual(C.extract(C.extend(buildProject('travis'), travisB)), {
+      projectName: 'travis',
+      projectHasLibrary: false,
+      projectGitHub: false,
+      projectTravis: false
+    })
+    assert.deepStrictEqual(C.extract(C.extend(C.extend(buildProject('github-travis'), gitHubB), travisB)), {
+      projectName: 'github-travis',
+      projectHasLibrary: false,
+      projectGitHub: true,
+      projectTravis: true
+    })
+  })
+
+  it('listen', () => {
+    assert.deepStrictEqual(C.extract(listen(buildProject('myproject'))), [
+      {
+        projectName: 'myproject',
+        projectHasLibrary: false,
+        projectGitHub: false,
+        projectTravis: false
+      },
+      {
+        settingsHasLibrary: false,
+        settingsGitHub: false,
+        settingsTravis: false
+      }
+    ])
+  })
+
+  it('listens', () => {
+    assert.deepStrictEqual(C.extract(listens(buildProject('myproject'), settings => settings.settingsTravis)), [
+      {
+        projectName: 'myproject',
+        projectHasLibrary: false,
+        projectGitHub: false,
+        projectTravis: false
+      },
+      false
+    ])
+  })
+
+  it('censor', () => {
+    assert.deepStrictEqual(
+      C.extract(
+        censor(buildProject('myproject'), settings => ({
+          ...settings,
+          settingsHasLibrary: !settings.settingsHasLibrary
+        }))
+      ),
+      {
+        projectName: 'myproject',
+        projectHasLibrary: true,
+        projectGitHub: false,
+        projectTravis: false
+      }
+    )
+  })
+})
