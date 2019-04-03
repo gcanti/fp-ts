9743f458662d6823af828b6b3788e5335bb1b721
gcanti <giulio.canti@gmail.com>
2019-04-03 05:20:13 +0200

    Console module: replace any with unknown

--- src/Console.ts
+++ src/Console.ts
@@ -6,27 +6,27 @@ import { IO } from './IO'
 /**
  * @since 1.0.0
  */
-export const log = (s: any): IO<void> => {
+export const log = (s: unknown): IO<void> => {
   return new IO(() => console.log(s)) // tslint:disable-line:no-console
 }
 
 /**
  * @since 1.0.0
  */
-export const warn = (s: any): IO<void> => {
+export const warn = (s: unknown): IO<void> => {
   return new IO(() => console.warn(s)) // tslint:disable-line:no-console
 }
 
 /**
  * @since 1.0.0
  */
-export const error = (s: any): IO<void> => {
+export const error = (s: unknown): IO<void> => {
   return new IO(() => console.error(s)) // tslint:disable-line:no-console
 }
 
 /**
  * @since 1.0.0
  */
-export const info = (s: any): IO<void> => {
+export const info = (s: unknown): IO<void> => {
   return new IO(() => console.info(s)) // tslint:disable-line:no-console
 }
