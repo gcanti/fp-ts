27a655b5dff4d839d999f37eb795e50168123b61
gcanti <giulio.canti@gmail.com>
2019-04-20 00:21:21 +0200

    Breaking Change: remove class from Writer

--- test/Writer.ts
+++ test/Writer.ts
@@ -1,56 +1,48 @@
 import * as assert from 'assert'
-import { Writer, censor, listen, listens, pass, tell, getMonad, writer } from '../src/Writer'
+import * as W from '../src/Writer'
 import { tuple } from '../src/function'
 import { monoidString } from '../src/Monoid'
 
 describe('Writer', () => {
   it('eval', () => {
-    const fa = new Writer(() => [1, 'a'])
-    assert.strictEqual(fa.eval(), 1)
+    assert.strictEqual(W.evalWriter(() => [1, 'a']), 1)
   })
 
   it('exec', () => {
-    const fa = new Writer(() => [1, 'a'])
-    assert.strictEqual(fa.exec(), 'a')
+    assert.strictEqual(W.execWriter(() => [1, 'a']), 'a')
   })
 
   it('map', () => {
-    const fa = new Writer(() => [1, 'a'])
     const double = (n: number): number => n * 2
-    assert.deepStrictEqual(fa.map(double).run(), [2, 'a'])
-    assert.deepStrictEqual(writer.map(fa, double).run(), [2, 'a'])
+    assert.deepStrictEqual(W.run(W.writer.map(() => [1, 'a'], double)), [2, 'a'])
   })
 
   it('tell', () => {
-    assert.deepStrictEqual(tell(1).run(), [undefined, 1])
+    assert.deepStrictEqual(W.run(W.tell(1)), [undefined, 1])
   })
 
   it('listen', () => {
-    const fa = new Writer(() => [1, 'a'])
-    assert.deepStrictEqual(listen(fa).run(), [[1, 'a'], 'a'])
+    assert.deepStrictEqual(W.run(W.listen(() => [1, 'a'])), [[1, 'a'], 'a'])
   })
 
   it('pass', () => {
-    const fa = new Writer(() => [tuple(1, (w: string) => w + 'b'), 'a'])
-    assert.deepStrictEqual(pass(fa).run(), [1, 'ab'])
+    assert.deepStrictEqual(W.run(W.pass(() => [tuple(1, (w: string) => w + 'b'), 'a'])), [1, 'ab'])
   })
 
   it('listens', () => {
-    const fa = new Writer(() => [1, 'a'])
-    assert.deepStrictEqual(listens(fa, w => w.length).run(), [[1, 1], 'a'])
+    assert.deepStrictEqual(W.run(W.listens(() => [1, 'a'], w => w.length)), [[1, 1], 'a'])
   })
 
   it('censor', () => {
-    const fa = new Writer(() => [1, ['a', 'b']])
-    assert.deepStrictEqual(censor(fa, w => w.filter(a => a !== 'a')).run(), [1, ['b']])
+    assert.deepStrictEqual(W.run(W.censor(() => [1, ['a', 'b']], w => w.filter(a => a !== 'a'))), [1, ['b']])
   })
 
   it('getMonad', () => {
-    const M = getMonad(monoidString)
-    assert.deepStrictEqual(M.of(1).run(), [1, ''])
+    const M = W.getMonad(monoidString)
+    assert.deepStrictEqual(W.run(M.of(1)), [1, ''])
     const double = (n: number): number => n * 2
-    assert.deepStrictEqual(M.ap(M.of(double), M.of(1)).run(), [2, ''])
+    assert.deepStrictEqual(W.run(M.ap(M.of(double), M.of(1))), [2, ''])
     const f = (n: number) => M.of(n * 2)
-    assert.deepStrictEqual(M.chain(M.of(1), f).run(), [2, ''])
+    assert.deepStrictEqual(W.run(M.chain(M.of(1), f)), [2, ''])
   })
 })
