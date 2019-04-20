8aafc875514a9004b8a72699add8ed38d7d24df4
gcanti <giulio.canti@gmail.com>
2019-04-20 09:46:26 +0200

    Breaking Change: remove class from Validation

--- src/Task.ts
+++ src/Task.ts
@@ -25,42 +25,6 @@ export interface Task<A> {
   (): Promise<A>
 }
 
-// /**
-//  * @since 1.0.0
-//  */
-// export class Task<A> {
-//   constructor(readonly run: Lazy<Promise<A>>) {}
-//   map<B>(f: (a: A) => B): Task<B> {
-//     return new Task(() => this.run().then(f))
-//   }
-//   ap<B>(fab: Task<(a: A) => B>): Task<B> {
-//     return new Task(() => Promise.all([fab.run(), this.run()]).then(([f, a]) => f(a)))
-//   }
-//   /**
-//    * Flipped version of `ap`
-//    */
-//   ap_<B, C>(this: Task<(b: B) => C>, fb: Task<B>): Task<C> {
-//     return fb.ap(this)
-//   }
-//   /**
-//    * Combine two effectful actions, keeping only the result of the first
-//    * @since 1.6.0
-//    */
-//   applyFirst<B>(fb: Task<B>): Task<A> {
-//     return fb.ap(this.map(constant))
-//   }
-//   /**
-//    * Combine two effectful actions, keeping only the result of the second
-//    * @since 1.5.0
-//    */
-//   applySecond<B>(fb: Task<B>): Task<B> {
-//     return fb.ap(this.map(constIdentity as () => (b: B) => B))
-//   }
-//   chain<B>(f: (a: A) => Task<B>): Task<B> {
-//     return new Task(() => this.run().then(a => f(a).run()))
-//   }
-// }
-
 const map = <A, B>(fa: Task<A>, f: (a: A) => B): Task<B> => {
   return () => fa().then(f)
 }
