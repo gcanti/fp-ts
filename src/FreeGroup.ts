0a9c804cda118521768f46c704e5893722b831f4
gcanti <giulio.canti@gmail.com>
2019-04-20 10:31:57 +0200

    Breaking Change: remove class from FreeGroup

--- src/FreeGroup.ts
+++ src/FreeGroup.ts
@@ -24,46 +24,22 @@ export type URI = typeof URI
 /**
  * @since 1.13.0
  */
-export class FreeGroup<A> {
-  constructor(readonly value: Array<Either<A, A>>) {}
-  map<B>(f: (a: A) => B): FreeGroup<B> {
-    return new FreeGroup(this.value.map(e => either.bimap(e, f, f)))
-  }
-  ap<B>(fab: FreeGroup<(a: A) => B>): FreeGroup<B> {
-    return fab.chain(f => this.map(f)) // <- derived
-  }
-  ap_<B, C>(this: FreeGroup<(b: B) => C>, fb: FreeGroup<B>): FreeGroup<C> {
-    return fb.ap(this)
-  }
-  chain<B>(f: (a: A) => FreeGroup<B>): FreeGroup<B> {
-    return new FreeGroup(array.chain(this.value, e => either.bimap(e, f, f).value.value))
-  }
-}
+export interface FreeGroup<A> extends Array<Either<A, A>> {}
 
 const of = <A>(a: A): FreeGroup<A> => {
-  return new FreeGroup([right(a)])
+  return [right(a)]
 }
 
 const map = <A, B>(fa: FreeGroup<A>, f: (a: A) => B): FreeGroup<B> => {
-  return fa.map(f)
+  return fa.map(e => either.bimap(e, f, f))
 }
 
 const ap = <A, B>(fab: FreeGroup<(a: A) => B>, fa: FreeGroup<A>): FreeGroup<B> => {
-  return fa.ap(fab)
+  return chain(fab, f => map(fa, f)) // <- derived
 }
 
 const chain = <A, B>(fa: FreeGroup<A>, f: (a: A) => FreeGroup<B>): FreeGroup<B> => {
-  return fa.chain(f)
-}
-
-/**
- * Smart constructor which normalizes an array
- *
- * @since 1.13.0
- */
-export const fromArray = <A>(S: Setoid<A>): ((as: Array<Either<A, A>>) => FreeGroup<A>) => {
-  const normalizeS = normalize(S)
-  return as => new FreeGroup(normalizeS(as))
+  return array.chain(fa, e => either.bimap(e, f, f).value)
 }
 
 /**
@@ -91,13 +67,13 @@ export const normalize = <A>(S: Setoid<A>) => (g: Array<Either<A, A>>): Array<Ei
 export const getSetoid = <A>(S: Setoid<A>): Setoid<FreeGroup<A>> => {
   const AS = getArraySetoid(getEitherSetoid(S, S))
   const normalizeS = normalize(S)
-  return fromEquals((x, y) => AS.equals(normalizeS(x.value), normalizeS(y.value)))
+  return fromEquals((x, y) => AS.equals(normalizeS(x), normalizeS(y)))
 }
 
 /**
  * @since 1.13.0
  */
-export const empty: FreeGroup<never> = new FreeGroup(emptyArray)
+export const empty: FreeGroup<never> = emptyArray
 
 /**
  * @since 1.13.0
@@ -106,9 +82,13 @@ export const getGroup = <A>(S: Setoid<A>): Group<FreeGroup<A>> => {
   const M = getArrayMonoid<Either<A, A>>()
   const normalizeS = normalize(S)
   return {
-    concat: (x, y) => new FreeGroup(normalizeS(M.concat(x.value, y.value))),
+    concat: (x, y) => normalizeS(M.concat(x, y)),
     empty,
-    inverse: x => new FreeGroup(x.value.reverse().map(s => (isLeft(s) ? right(s.value) : left(s.value))))
+    inverse: x =>
+      x
+        .slice()
+        .reverse()
+        .map(s => (isLeft(s) ? right(s.value) : left(s.value)))
   }
 }
 
