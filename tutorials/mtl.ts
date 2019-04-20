88bca0c7d228c5b1a64351b49bb6399a01b78d2e
gcanti <giulio.canti@gmail.com>
2019-04-20 10:50:19 +0200

    fix mtl example

--- tutorials/mtl.ts
+++ tutorials/mtl.ts
@@ -1,11 +1,11 @@
-import { liftA2 } from '../src/Apply'
+import { sequenceS } from '../src/Apply'
 import { flatten } from '../src/Chain'
 import { HKT, Type, Type3, URIS, URIS3 } from '../src/HKT'
-import { IO, io, URI as IOURI } from '../src/IO'
+import { io, URI as IOURI } from '../src/IO'
 import { Monad, Monad1, Monad3C } from '../src/Monad'
-import { readerTaskEither, right, URI as ReaderTaskEitherURI, ReaderTaskEither } from '../src/ReaderTaskEither'
-import { delay, task, URI as TaskURI } from '../src/Task'
-import { right as taskEitherRight, fromLeft } from '../src/TaskEither'
+import * as RTE from '../src/ReaderTaskEither'
+import * as T from '../src/Task'
+import * as TE from '../src/TaskEither'
 
 // Adapted from https://tech.iheart.com/why-fp-its-the-composition-f585d17b01d3
 
@@ -20,7 +20,7 @@ interface MonadUser<M> {
 
 interface MonadFB<M> {
   findPost: (url: string) => HKT<M, string>
-  sendLike: (fbToken: string) => (post: string) => HKT<M, boolean>
+  sendLike: (fbToken: string, post: string) => HKT<M, boolean>
 }
 
 interface MonadApp<M> extends MonadUser<M>, MonadFB<M>, Monad<M> {}
@@ -37,8 +37,7 @@ function likePost<M>(M: MonadApp<M>): (token: string) => (url: string) => HKT<M,
   return token => url => {
     const mToken = M.chain(M.validateUser(token), uid => M.facebookToken(uid))
     const mPost = M.findPost(url)
-    // tslint:disable-next-line: deprecation
-    const mmResult = liftA2(M)(M.sendLike)(mToken)(mPost)
+    const mmResult = M.map(sequenceS(M)({ token: mToken, post: mPost }), ({ token, post }) => M.sendLike(token, post))
     return flatten(M)(mmResult)
   }
 }
@@ -54,7 +53,7 @@ interface MonadUser1<M extends URIS> {
 
 interface MonadFB1<M extends URIS> {
   findPost: (url: string) => Type<M, string>
-  sendLike: (fbToken: string) => (post: string) => Type<M, boolean>
+  sendLike: (fbToken: string, post: string) => Type<M, boolean>
 }
 
 interface MonadApp1<M extends URIS> extends MonadUser1<M>, MonadFB1<M>, Monad1<M> {}
@@ -68,8 +67,8 @@ const monadUserIO: MonadUser1<IOURI> = {
 
 const monadFBIO: MonadFB1<IOURI> = {
   findPost: url => io.of(`FBPost(${url})`),
-  sendLike: token => (post: string) => {
-    return new IO(() => true)
+  sendLike: (token, post) => {
+    return () => true
   }
 }
 
@@ -80,30 +79,29 @@ const monadAppIO: MonadApp1<IOURI> = {
 }
 
 // tslint:disable-next-line:no-console
-console.log('IO', likePost(monadAppIO)('session123')('https://me.com/1').run())
+console.log('IO', likePost(monadAppIO)('session123')('https://me.com/1')())
 // => IO true
 
 // Task
 
-const monadUserTask: MonadUser1<TaskURI> = {
-  validateUser: token => delay(1000, `string(${token})`),
-  facebookToken: uid => delay(500, `FBToken(${uid})`)
+const monadUserTask: MonadUser1<T.URI> = {
+  validateUser: token => T.delay(1000, `string(${token})`),
+  facebookToken: uid => T.delay(500, `FBToken(${uid})`)
 }
 
-const monadFBTask: MonadFB1<TaskURI> = {
-  findPost: url => delay(2000, `FBPost(${url})`),
-  sendLike: token => (post: string) => delay(1000, true)
+const monadFBTask: MonadFB1<T.URI> = {
+  findPost: url => T.delay(2000, `FBPost(${url})`),
+  sendLike: () => T.delay(1000, true)
 }
 
-const monadAppTask: MonadApp1<TaskURI> = {
-  ...(task as Monad1<TaskURI>),
+const monadAppTask: MonadApp1<T.URI> = {
+  ...T.task,
   ...monadUserTask,
   ...monadFBTask
 }
 
 // tslint:disable-next-line: no-floating-promises
-likePost(monadAppTask)('session123')('https://me.com/1')
-  .run()
+likePost(monadAppTask)('session123')('https://me.com/1')()
   // tslint:disable-next-line:no-console
   .then(result => console.log('Task', result))
 // => Task true
@@ -117,28 +115,26 @@ interface MonadUser3C<M extends URIS3, U, L> {
 
 interface MonadFB3C<M extends URIS3, U, L> {
   findPost: (url: string) => Type3<M, U, L, string>
-  sendLike: (fbToken: string) => (post: string) => Type3<M, U, L, boolean>
+  sendLike: (fbToken: string, post: string) => Type3<M, U, L, boolean>
 }
 
 interface MonadApp3C<M extends URIS3, U, L> extends MonadUser3C<M, U, L>, MonadFB3C<M, U, L>, Monad3C<M, U, L> {}
 
 type Env = { error: boolean }
 
-const monadUserReaderTaskEither: MonadUser3C<ReaderTaskEitherURI, Env, Error> = {
-  validateUser: token =>
-    new ReaderTaskEither(
-      e => (e.error ? fromLeft(new Error('validateUser error')) : taskEitherRight(monadUserTask.validateUser(token)))
-    ),
-  facebookToken: uid => right(monadUserTask.facebookToken(uid))
+const monadUserReaderTaskEither: MonadUser3C<RTE.URI, Env, Error> = {
+  validateUser: token => e =>
+    e.error ? TE.fromLeft(new Error('validateUser error')) : TE.right(monadUserTask.validateUser(token)),
+  facebookToken: uid => RTE.right(monadUserTask.facebookToken(uid))
 }
 
-const monadFBReaderTaskEither: MonadFB3C<ReaderTaskEitherURI, Env, Error> = {
-  findPost: url => right(monadFBTask.findPost(url)),
-  sendLike: token => (post: string) => right(monadFBTask.sendLike(token)(post))
+const monadFBReaderTaskEither: MonadFB3C<RTE.URI, Env, Error> = {
+  findPost: url => RTE.right(monadFBTask.findPost(url)),
+  sendLike: (token, post) => RTE.right(monadFBTask.sendLike(token, post))
 }
 
-const monadAppReaderTaskEither: MonadApp3C<ReaderTaskEitherURI, Env, Error> = {
-  ...((readerTaskEither as any) as Monad3C<ReaderTaskEitherURI, Env, Error>),
+const monadAppReaderTaskEither: MonadApp3C<RTE.URI, Env, Error> = {
+  ...((RTE.readerTaskEither as any) as Monad3C<RTE.URI, Env, Error>),
   ...monadUserReaderTaskEither,
   ...monadFBReaderTaskEither
 }
@@ -146,13 +142,13 @@ const monadAppReaderTaskEither: MonadApp3C<ReaderTaskEitherURI, Env, Error> = {
 const ma = likePost(monadAppReaderTaskEither)('session123')('https://me.com/1')
 
 // tslint:disable-next-line: no-floating-promises
-ma.run({ error: true })
+ma({ error: true })()
   // tslint:disable-next-line:no-console
   .then(result => console.log('ReaderTaskEither 2', result))
 // => ReaderTaskEither 2 left(Error: validateUser error)
 
 // tslint:disable-next-line: no-floating-promises
-ma.run({ error: false })
+ma({ error: false })()
   // tslint:disable-next-line:no-console
   .then(result => console.log('ReaderTaskEither 1', result))
 // => ReaderTaskEither 1 right(true)
