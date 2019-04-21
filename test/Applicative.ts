47f7c5f464f951b7a8c6e057de719217716f5fc2
gcanti <giulio.canti@gmail.com>
2019-04-21 11:46:08 +0200

    Breaking Change: remove Apply.applyFirst, Apply.applySecond, Apply.getSemigroup, Applicative.getMonoid

--- test/Applicative.ts
+++ test/Applicative.ts
@@ -1,12 +1,8 @@
 import * as assert from 'assert'
-import { getApplicativeComposition, getMonoid, when } from '../src/Applicative'
+import { getApplicativeComposition, when } from '../src/Applicative'
 import { array } from '../src/Array'
-import { either, left, right } from '../src/Either'
 import { io } from '../src/IO'
-import { monoidSum } from '../src/Monoid'
 import { none, option, some } from '../src/Option'
-import { semigroupString } from '../src/Semigroup'
-import { getApplicative } from '../src/Validation'
 
 describe('Applicative', () => {
   it('getApplicativeComposition', () => {
@@ -31,26 +27,5 @@ describe('Applicative', () => {
     assert.deepStrictEqual(log, [])
     when(io)(true, action)()
     assert.deepStrictEqual(log, ['action called'])
-  })
-
-  it('getMonoid', () => {
-    const MOption = getMonoid(option, monoidSum)()
-    assert.deepStrictEqual(MOption.concat(none, none), none)
-    assert.deepStrictEqual(MOption.concat(some(1), none), none)
-    assert.deepStrictEqual(MOption.concat(none, some(2)), none)
-    assert.deepStrictEqual(MOption.concat(some(1), some(2)), some(3))
-
-    const MEither = getMonoid(either, monoidSum)<string>()
-    assert.deepStrictEqual(MEither.concat(left('a'), left('b')), left('a'))
-    assert.deepStrictEqual(MEither.concat(right(1), left('b')), left('b'))
-    assert.deepStrictEqual(MEither.concat(left('a'), right(2)), left('a'))
-    assert.deepStrictEqual(MEither.concat(right(1), right(2)), right(3))
-
-    const validation = getApplicative(semigroupString)
-    const MValidation = getMonoid(validation, monoidSum)()
-    assert.deepStrictEqual(MValidation.concat(left('a'), left('b')), left('ab'))
-    assert.deepStrictEqual(MValidation.concat(right(1), left('b')), left('b'))
-    assert.deepStrictEqual(MValidation.concat(left('a'), right(2)), left('a'))
-    assert.deepStrictEqual(MValidation.concat(right(1), right(2)), right(3))
   })
 })
