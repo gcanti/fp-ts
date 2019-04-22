5d8bfa26681b9a516350416575bac455964eb4fc
gcanti <giulio.canti@gmail.com>
2019-04-22 17:00:06 +0200

    Polish: remove useless functions from Validation module

--- docs/modules/Validation.ts.md
+++ docs/modules/Validation.ts.md
@@ -10,13 +10,10 @@ parent: Modules
 
 - [getAlt (function)](#getalt-function)
 - [getApplicative (function)](#getapplicative-function)
-- [getCompactable (function)](#getcompactable-function)
-- [getFilterable (function)](#getfilterable-function)
 - [getMonad (function)](#getmonad-function)
 - [getMonadThrow (function)](#getmonadthrow-function)
 - [getMonoid (function)](#getmonoid-function)
 - [getSemigroup (function)](#getsemigroup-function)
-- [getWitherable (function)](#getwitherable-function)
 
 ---
 
@@ -25,7 +22,7 @@ parent: Modules
 **Signature**
 
 ```ts
-export const getAlt = <L>(S: Semigroup<L>): Alt2C<URI, L> => ...
+export function getAlt<L>(S: Semigroup<L>): Alt2C<URI, L> { ... }
 ```
 
 Added in v2.0.0
@@ -35,32 +32,8 @@ Added in v2.0.0
 **Signature**
 
 ```ts
-export const getApplicative = <L>(S: Semigroup<L>): Applicative2C<URI, L> => ...
+export function getApplicative<L>(S: Semigroup<L>): Applicative2C<URI, L> { ... }
 ```
-
-# getCompactable (function)
-
-Builds `Compactable` instance for `Validation` given `Monoid` for the failure side
-
-**Signature**
-
-```ts
-export function getCompactable<L>(ML: Monoid<L>): Compactable2C<URI, L> { ... }
-```
-
-Added in v2.0.0
-
-# getFilterable (function)
-
-Builds `Filterable` instance for `Validation` given `Monoid` for the left side
-
-**Signature**
-
-```ts
-export function getFilterable<L>(ML: Monoid<L>): Filterable2C<URI, L> { ... }
-```
-
-Added in v2.0.0
 
 # getMonad (function)
 
@@ -69,7 +42,7 @@ Added in v2.0.0
 **Signature**
 
 ```ts
-export const getMonad = <L>(S: Semigroup<L>): Monad2C<URI, L> => ...
+export function getMonad<L>(S: Semigroup<L>): Monad2C<URI, L> { ... }
 ```
 
 Added in v2.0.0
@@ -79,7 +52,7 @@ Added in v2.0.0
 **Signature**
 
 ```ts
-export const getMonadThrow = <L>(S: Semigroup<L>): MonadThrow2C<URI, L> => ...
+export function getMonadThrow<L>(S: Semigroup<L>): MonadThrow2C<URI, L> { ... }
 ```
 
 Added in v2.0.0
@@ -89,7 +62,7 @@ Added in v2.0.0
 **Signature**
 
 ```ts
-export const getMonoid = <L, A>(SL: Semigroup<L>, SA: Monoid<A>): Monoid<Either<L, A>> => ...
+export function getMonoid<L, A>(SL: Semigroup<L>, SA: Monoid<A>): Monoid<Either<L, A>> { ... }
 ```
 
 Added in v2.0.0
@@ -99,19 +72,7 @@ Added in v2.0.0
 **Signature**
 
 ```ts
-export const getSemigroup = <L, A>(SL: Semigroup<L>, SA: Semigroup<A>): Semigroup<Either<L, A>> => ...
-```
-
-Added in v2.0.0
-
-# getWitherable (function)
-
-Builds `Witherable` instance for `Validation` given `Monoid` for the left side
-
-**Signature**
-
-```ts
-export function getWitherable<L>(ML: Monoid<L>): Witherable2C<URI, L> { ... }
+export function getSemigroup<L, A>(SL: Semigroup<L>, SA: Semigroup<A>): Semigroup<Either<L, A>> { ... }
 ```
 
 Added in v2.0.0
--- test/Validation.ts
+++ test/Validation.ts
@@ -1,21 +1,9 @@
 import * as assert from 'assert'
 import { left, right } from '../src/Either'
-import * as I from '../src/Identity'
 import { getArrayMonoid, monoidString, monoidSum } from '../src/Monoid'
 import { none, some } from '../src/Option'
 import { semigroupString } from '../src/Semigroup'
-import {
-  getAlt,
-  getCompactable,
-  getFilterable,
-  getMonad,
-  getMonadThrow,
-  getMonoid,
-  getSemigroup,
-  getWitherable
-} from '../src/Validation'
-
-const p = (n: number): boolean => n > 2
+import { getAlt, getMonad, getMonadThrow, getMonoid, getSemigroup } from '../src/Validation'
 
 describe('Validation', () => {
   it('getMonad', () => {
@@ -53,115 +41,6 @@ describe('Validation', () => {
     assert.deepStrictEqual(M.concat(right(1), left('foo')), left('foo'))
     assert.deepStrictEqual(M.concat(left('foo'), right(1)), left('foo'))
     assert.deepStrictEqual(M.concat(left('foo'), left('bar')), left('foobar'))
-  })
-
-  describe('getCompactable', () => {
-    const C = getCompactable(monoidString)
-
-    it('compact', () => {
-      assert.deepStrictEqual(C.compact(left('1')), left('1'))
-      assert.deepStrictEqual(C.compact(right(none)), left(monoidString.empty))
-      assert.deepStrictEqual(C.compact(right(some(123))), right(123))
-    })
-
-    it('separate', () => {
-      assert.deepStrictEqual(C.separate(left('123')), { left: left('123'), right: left('123') })
-      assert.deepStrictEqual(C.separate(right(left('123'))), {
-        left: right('123'),
-        right: left(monoidString.empty)
-      })
-      assert.deepStrictEqual(C.separate(right(right('123'))), {
-        left: left(monoidString.empty),
-        right: right('123')
-      })
-    })
-  })
-
-  describe('getFilterable', () => {
-    const F = getFilterable(monoidString)
-
-    it('partition', () => {
-      assert.deepStrictEqual(F.partition(left('123'), p), {
-        left: left('123'),
-        right: left('123')
-      })
-      assert.deepStrictEqual(F.partition(right(1), p), {
-        left: right(1),
-        right: left(monoidString.empty)
-      })
-      assert.deepStrictEqual(F.partition(right(3), p), {
-        left: left(monoidString.empty),
-        right: right(3)
-      })
-    })
-
-    it('partitionMap', () => {
-      const f = (n: number) => (p(n) ? right(n + 1) : left(n - 1))
-      assert.deepStrictEqual(F.partitionMap(left('123'), f), {
-        left: left('123'),
-        right: left('123')
-      })
-      assert.deepStrictEqual(F.partitionMap(right(1), f), {
-        left: right(0),
-        right: left(monoidString.empty)
-      })
-      assert.deepStrictEqual(F.partitionMap(right(3), f), {
-        left: left(monoidString.empty),
-        right: right(4)
-      })
-    })
-
-    it('filter', () => {
-      assert.deepStrictEqual(F.filter(left('123'), p), left('123'))
-      assert.deepStrictEqual(F.filter(right(1), p), left(monoidString.empty))
-      assert.deepStrictEqual(F.filter(right(3), p), right(3))
-    })
-
-    it('filterMap', () => {
-      const f = (n: number) => (p(n) ? some(n + 1) : none)
-      assert.deepStrictEqual(F.filterMap(left('123'), f), left('123'))
-      assert.deepStrictEqual(F.filterMap(right(1), f), left(monoidString.empty))
-      assert.deepStrictEqual(F.filterMap(right(3), f), right(4))
-    })
-  })
-
-  describe('getWitherable', () => {
-    const W = getWitherable(monoidString)
-    const p = (n: number) => n > 2
-
-    it('wither', () => {
-      const f = (n: number) => I.identity.of(p(n) ? some(n + 1) : none)
-      const witherIdentity = W.wither(I.identity)
-      assert.deepStrictEqual(witherIdentity(left('foo'), f), I.identity.of(left('foo')))
-      assert.deepStrictEqual(witherIdentity(right(1), f), I.identity.of(left(monoidString.empty)))
-      assert.deepStrictEqual(witherIdentity(right(3), f), I.identity.of(right(4)))
-    })
-
-    it('wilt', () => {
-      const wiltIdentity = W.wilt(I.identity)
-      const f = (n: number) => I.identity.of(p(n) ? right(n + 1) : left(n - 1))
-      assert.deepStrictEqual(
-        wiltIdentity(left('foo'), f),
-        I.identity.of({
-          left: left('foo'),
-          right: left('foo')
-        })
-      )
-      assert.deepStrictEqual(
-        wiltIdentity(right(1), f),
-        I.identity.of({
-          left: right(0),
-          right: left(monoidString.empty)
-        })
-      )
-      assert.deepStrictEqual(
-        wiltIdentity(right(3), f),
-        I.identity.of({
-          left: left(monoidString.empty),
-          right: right(4)
-        })
-      )
-    })
   })
 
   describe('getMonadThrow', () => {
