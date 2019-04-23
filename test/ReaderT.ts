3b16e1d4ddc50c57c4db18af3f0fc9c5526f833a
gcanti <giulio.canti@gmail.com>
2019-04-23 10:33:33 +0200

    Breaking Change: move fromReader to ReaderT

--- test/ReaderT.ts
+++ test/ReaderT.ts
@@ -5,21 +5,16 @@ import * as readerT from '../src/ReaderT'
 import { lookup } from '../src/Record'
 
 describe('ReaderT', () => {
-  it('fromReader', () => {
-    const f = readerT.fromReader(option)(reader.of<void, number>(1))
-    assert.deepStrictEqual(f(undefined), some(1))
-  })
-
   it('getReaderT', () => {
-    const readerOption = readerT.getReaderT(option)
+    const T = readerT.getReaderT(option)
 
     function configure(key: string) {
       return (e: Record<string, string>) => lookup(key, e)
     }
 
-    const setupConnection = readerOption.chain(configure('host'), host => {
-      return readerOption.chain(configure('user'), user => {
-        return readerOption.map(configure('password'), password => [host, user, password] as [string, string, string])
+    const setupConnection = T.chain(configure('host'), host => {
+      return T.chain(configure('user'), user => {
+        return T.map(configure('password'), password => [host, user, password] as [string, string, string])
       })
     })
 
@@ -37,5 +32,8 @@ describe('ReaderT', () => {
     }
 
     assert.deepStrictEqual(setupConnection(badConfig), none)
+
+    const f = T.fromReader(reader.of<void, number>(1))
+    assert.deepStrictEqual(f(undefined), some(1))
   })
 })
