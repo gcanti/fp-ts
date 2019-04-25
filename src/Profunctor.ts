7200c9932d58ae41e82bd5325a5dae464738c13e
gcanti <giulio.canti@gmail.com>
2019-04-25 10:46:35 +0200

    Breaking Change: remove Profunctor.lmap, Profunctor.rmap

--- src/Profunctor.ts
+++ src/Profunctor.ts
@@ -25,31 +25,3 @@ export interface Profunctor3<F extends URIS3> extends Functor3<F> {
 export interface Profunctor4<F extends URIS4> extends Functor4<F> {
   readonly promap: <X, U, A, B, C, D>(fbc: Type4<F, X, U, B, C>, f: (a: A) => B, g: (c: C) => D) => Type4<F, X, U, A, D>
 }
-
-/**
- * @since 2.0.0
- */
-export function lmap<F extends URIS3>(
-  profunctor: Profunctor3<F>
-): <U, A, B, C>(fbc: Type3<F, U, B, C>, f: (a: A) => B) => Type3<F, U, A, C>
-export function lmap<F extends URIS2>(
-  profunctor: Profunctor2<F>
-): <A, B, C>(fbc: Type2<F, B, C>, f: (a: A) => B) => Type2<F, A, C>
-export function lmap<F>(profunctor: Profunctor<F>): <A, B, C>(fbc: HKT2<F, B, C>, f: (a: A) => B) => HKT2<F, A, C>
-export function lmap<F>(profunctor: Profunctor<F>): <A, B, C>(fbc: HKT2<F, B, C>, f: (a: A) => B) => HKT2<F, A, C> {
-  return (fbc, f) => profunctor.promap(fbc, f, c => c)
-}
-
-/**
- * @since 2.0.0
- */
-export function rmap<F extends URIS3>(
-  profunctor: Profunctor3<F>
-): <U, B, C, D>(fbc: Type3<F, U, B, C>, g: (c: C) => D) => Type3<F, U, B, D>
-export function rmap<F extends URIS2>(
-  profunctor: Profunctor2<F>
-): <B, C, D>(fbc: Type2<F, B, C>, g: (c: C) => D) => Type2<F, B, D>
-export function rmap<F>(profunctor: Profunctor<F>): <B, C, D>(fbc: HKT2<F, B, C>, g: (c: C) => D) => HKT2<F, B, D>
-export function rmap<F>(profunctor: Profunctor<F>): <B, C, D>(fbc: HKT2<F, B, C>, g: (c: C) => D) => HKT2<F, B, D> {
-  return (fbc, g) => profunctor.promap(fbc, b => b, g)
-}
