388283af29233c192aacbe6cdab8b2fdbca03bd9
gcanti <giulio.canti@gmail.com>
2019-04-27 10:59:27 +0200

    clean up Identity

--- docs/modules/Identity.ts.md
+++ docs/modules/Identity.ts.md
@@ -11,9 +11,9 @@ parent: Modules
 - [Identity (type alias)](#identity-type-alias)
 - [URI (type alias)](#uri-type-alias)
 - [URI (constant)](#uri-constant)
+- [getEq (constant)](#geteq-constant)
+- [getShow (constant)](#getshow-constant)
 - [identity (constant)](#identity-constant)
-- [getEq (function)](#geteq-function)
-- [getShow (function)](#getshow-function)
 
 ---
 
@@ -41,32 +41,32 @@ export type URI = typeof URI
 export const URI = ...
 ```
 
+# getEq (constant)
+
+**Signature**
+
+```ts
+export const getEq: <A>(E: Eq<A>) => Eq<Identity<A>> = ...
+```
+
+Added in v2.0.0
+
+# getShow (constant)
+
+**Signature**
+
+```ts
+export const getShow: <A>(S: Show<A>) => Show<Identity<A>> = ...
+```
+
+Added in v2.0.0
+
 # identity (constant)
 
 **Signature**
 
 ```ts
 export const identity: Monad1<URI> & Foldable1<URI> & Traversable1<URI> & Alt1<URI> & Comonad1<URI> & ChainRec1<URI> = ...
-```
-
-Added in v2.0.0
-
-# getEq (function)
-
-**Signature**
-
-```ts
-export const getEq = <A>(E: Eq<A>): Eq<Identity<A>> => ...
-```
-
-Added in v2.0.0
-
-# getShow (function)
-
-**Signature**
-
-```ts
-export const getShow = <A>(S: Show<A>): Show<Identity<A>> => ...
 ```
 
 Added in v2.0.0
--- src/Identity.ts
+++ src/Identity.ts
@@ -1,14 +1,13 @@
 import { Alt1 } from './Alt'
 import { Applicative } from './Applicative'
-import { ChainRec1, tailRec } from './ChainRec'
+import { ChainRec1 } from './ChainRec'
 import { Comonad1 } from './Comonad'
 import { Either } from './Either'
 import { Foldable1 } from './Foldable'
 import { identity as id } from './function'
 import { HKT } from './HKT'
 import { Monad1 } from './Monad'
-import { Monoid } from './Monoid'
-import { fromEquals, Eq } from './Eq'
+import { Eq } from './Eq'
 import { Show } from './Show'
 import { Traversable1 } from './Traversable'
 
@@ -27,65 +26,27 @@ export type Identity<A> = A
 /**
  * @since 2.0.0
  */
-export const getShow = <A>(S: Show<A>): Show<Identity<A>> => {
-  return {
-    show: a => S.show(a)
-  }
-}
+export const getShow: <A>(S: Show<A>) => Show<Identity<A>> = id
 
 /**
  * @since 2.0.0
  */
-export const getEq = <A>(E: Eq<A>): Eq<Identity<A>> => {
-  return fromEquals((x, y) => E.equals(x, y))
-}
+export const getEq: <A>(E: Eq<A>) => Eq<Identity<A>> = id
 
-const map = <A, B>(fa: Identity<A>, f: (a: A) => B): Identity<B> => {
-  return f(fa)
-}
-
-const of = <A>(a: A): Identity<A> => {
-  return a
-}
-
-const ap = <A, B>(fab: Identity<(a: A) => B>, fa: Identity<A>): Identity<B> => {
-  return fab(fa)
-}
-
-const chain = <A, B>(fa: Identity<A>, f: (a: A) => Identity<B>): Identity<B> => {
-  return f(fa)
-}
-
-const reduce = <A, B>(fa: Identity<A>, b: B, f: (b: B, a: A) => B): B => {
-  return f(b, fa)
-}
-
-const foldMap = <M>(_: Monoid<M>) => <A>(fa: Identity<A>, f: (a: A) => M): M => {
-  return f(fa)
-}
-
-const reduceRight = <A, B>(fa: Identity<A>, b: B, f: (a: A, b: B) => B): B => {
-  return f(fa, b)
-}
-
-const extend = <A, B>(ea: Identity<A>, f: (ea: Identity<A>) => B): Identity<B> => {
-  return f(ea)
-}
-
-const extract = <A>(fa: Identity<A>): A => {
-  return fa
-}
-
-const chainRec = <A, B>(a: A, f: (a: A) => Identity<Either<A, B>>): Identity<B> => {
-  return tailRec(a, a => f(a))
+const chainRec = <A, B>(a: A, f: (a: A) => Either<A, B>): B => {
+  let v = f(a)
+  while (v._tag === 'Left') {
+    v = f(v.left)
+  }
+  return v.right
 }
 
 const traverse = <F>(F: Applicative<F>) => <A, B>(ta: Identity<A>, f: (a: A) => HKT<F, B>): HKT<F, Identity<B>> => {
-  return F.map(f(ta), of)
+  return F.map(f(ta), id)
 }
 
 const sequence = <F>(F: Applicative<F>) => <A>(ta: Identity<HKT<F, A>>): HKT<F, Identity<A>> => {
-  return F.map(ta, of)
+  return F.map(ta, id)
 }
 
 /**
@@ -93,17 +54,17 @@ const sequence = <F>(F: Applicative<F>) => <A>(ta: Identity<HKT<F, A>>): HKT<F, 
  */
 export const identity: Monad1<URI> & Foldable1<URI> & Traversable1<URI> & Alt1<URI> & Comonad1<URI> & ChainRec1<URI> = {
   URI,
-  map,
-  of,
-  ap,
-  chain,
-  reduce,
-  foldMap,
-  reduceRight,
+  map: (ma, f) => f(ma),
+  of: id,
+  ap: (mab, ma) => mab(ma),
+  chain: (ma, f) => f(ma),
+  reduce: (fa, b, f) => f(b, fa),
+  foldMap: _ => (fa, f) => f(fa),
+  reduceRight: (fa, b, f) => f(fa, b),
   traverse,
   sequence,
   alt: id,
-  extract,
-  extend,
+  extract: id,
+  extend: (wa, f) => f(wa),
   chainRec
 }
