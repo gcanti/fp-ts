520fcc22c857f651bcfb7da6ec0571ba12603f78
gcanti <giulio.canti@gmail.com>
2019-05-01 12:12:28 +0200

    Breaking Change: remove IxMonad.iapplyFirst, iapplySecond

--- docs/modules/IxMonad.ts.md
+++ docs/modules/IxMonad.ts.md
@@ -10,8 +10,6 @@ parent: Modules
 
 - [IxMonad (interface)](#ixmonad-interface)
 - [IxMonad3 (interface)](#ixmonad3-interface)
-- [iapplyFirst (function)](#iapplyfirst-function)
-- [iapplySecond (function)](#iapplysecond-function)
 
 ---
 
@@ -40,33 +38,3 @@ export interface IxMonad3<F extends URIS3> {
   readonly ichain: <I, O, Z, A, B>(fa: Type3<F, I, O, A>, f: (a: A) => Type3<F, O, Z, B>) => Type3<F, I, Z, B>
 }
 ```
-
-# iapplyFirst (function)
-
-**Signature**
-
-```ts
-export function iapplyFirst<F extends URIS3>(
-  ixmonad: IxMonad3<F>
-): <I, O, A, Z, B>(fa: Type3<F, I, O, A>, fb: Type3<F, O, Z, B>) => Type3<F, I, Z, A>
-export function iapplyFirst<F>(
-  ixmonad: IxMonad<F>
-): <I, O, A, Z, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, A> { ... }
-```
-
-Added in v2.0.0
-
-# iapplySecond (function)
-
-**Signature**
-
-```ts
-export function iapplySecond<F extends URIS3>(
-  ixmonad: IxMonad3<F>
-): <I, O, A, Z, B>(fa: Type3<F, I, O, A>, fb: Type3<F, O, Z, B>) => Type3<F, I, Z, B>
-export function iapplySecond<F>(
-  ixmonad: IxMonad<F>
-): <I, O, A, Z, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, B> { ... }
-```
-
-Added in v2.0.0
--- src/IxMonad.ts
+++ src/IxMonad.ts
@@ -1,5 +1,4 @@
 import { HKT3, Type3, URIS3 } from './HKT'
-import { constant } from './function'
 
 // Adapted from https://github.com/garyb/purescript-indexed-monad
 
@@ -17,34 +16,4 @@ export interface IxMonad3<F extends URIS3> {
   readonly URI: F
   readonly iof: <I, A>(a: A) => Type3<F, I, I, A>
   readonly ichain: <I, O, Z, A, B>(fa: Type3<F, I, O, A>, f: (a: A) => Type3<F, O, Z, B>) => Type3<F, I, Z, B>
-}
-
-/**
- * @since 2.0.0
- */
-export function iapplyFirst<F extends URIS3>(
-  ixmonad: IxMonad3<F>
-): <I, O, A, Z, B>(fa: Type3<F, I, O, A>, fb: Type3<F, O, Z, B>) => Type3<F, I, Z, A>
-export function iapplyFirst<F>(
-  ixmonad: IxMonad<F>
-): <I, O, A, Z, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, A>
-export function iapplyFirst<F>(
-  ixmonad: IxMonad<F>
-): <I, O, A, Z, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, A> {
-  return (fa, fb) => ixmonad.ichain(fa, a => ixmonad.ichain(fb, () => ixmonad.iof(a)))
-}
-
-/**
- * @since 2.0.0
- */
-export function iapplySecond<F extends URIS3>(
-  ixmonad: IxMonad3<F>
-): <I, O, A, Z, B>(fa: Type3<F, I, O, A>, fb: Type3<F, O, Z, B>) => Type3<F, I, Z, B>
-export function iapplySecond<F>(
-  ixmonad: IxMonad<F>
-): <I, O, A, Z, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, B>
-export function iapplySecond<F>(
-  ixmonad: IxMonad<F>
-): <I, O, A, Z, B>(fa: HKT3<F, I, O, A>, fb: HKT3<F, O, Z, B>) => HKT3<F, I, Z, B> {
-  return (fa, fb) => ixmonad.ichain(fa, constant(fb))
 }
