b9c11e2df725d69213e8afb68d49d0c3cfba0519
gcanti <giulio.canti@gmail.com>
2019-06-02 10:58:27 +0200

    backport Store top level data-last functions

---
+++ test/Store.ts
@@ -0,0 +1,66 @@
+import * as assert from 'assert'
+import { Store, store, seek, seeks, peeks, experiment } from '../src/Store'
+import { array } from '../src/Array'
+import { pipeOp as pipe } from '../src/function'
+
+const len = (s: string): number => s.length
+
+describe('Store', () => {
+  it('map', () => {
+    const wa = new Store<string, number>(len, 'a')
+    assert.strictEqual(store.extract(store.map(wa, n => n + 1)), 2)
+  })
+
+  it('extends', () => {
+    const wa = new Store<string, number>(len, 'a')
+    assert.strictEqual(store.extract(store.extend(wa, wa => store.extract(store.map(wa, n => n + 1)))), 2)
+  })
+
+  it('seek', () => {
+    const wa = new Store<string, number>(len, 'a')
+    assert.strictEqual(
+      store.extract(
+        pipe(
+          wa,
+          seek('aa')
+        )
+      ),
+      2
+    )
+  })
+
+  it('seeks', () => {
+    const wa = new Store<string, number>(len, 'a')
+    assert.strictEqual(
+      store.extract(
+        pipe(
+          wa,
+          seeks(s => s + 'a')
+        )
+      ),
+      2
+    )
+  })
+
+  it('peeks', () => {
+    const wa = new Store<string, number>(len, 'a')
+    assert.strictEqual(
+      pipe(
+        wa,
+        peeks(s => s + 'a')
+      ),
+      2
+    )
+  })
+
+  it('experiment', () => {
+    const wa = new Store<string, number>(len, 'a')
+    assert.deepStrictEqual(
+      pipe(
+        wa,
+        experiment(array)(s => [s, s + 'a'])
+      ),
+      [1, 2]
+    )
+  })
+})
