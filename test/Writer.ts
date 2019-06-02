41d600579209f484feca1ddc4194c3ab8244444a
gcanti <giulio.canti@gmail.com>
2019-06-02 10:47:05 +0200

    backport Writer top level data-last functions

--- test/Writer.ts
+++ test/Writer.ts
@@ -1,17 +1,28 @@
 import * as assert from 'assert'
-import { Writer, censor, listen, listens, pass, tell, getMonad, writer } from '../src/Writer'
-import { tuple } from '../src/function'
+import {
+  Writer,
+  listen,
+  pass,
+  tell,
+  getMonad,
+  writer,
+  listens2v,
+  censor2v,
+  evalWriter,
+  execWriter
+} from '../src/Writer'
+import { tuple, pipeOp } from '../src/function'
 import { monoidString } from '../src/Monoid'
 
 describe('Writer', () => {
   it('eval', () => {
     const fa = new Writer(() => [1, 'a'])
-    assert.strictEqual(fa.eval(), 1)
+    assert.strictEqual(pipeOp(fa, evalWriter), 1)
   })
 
   it('exec', () => {
     const fa = new Writer(() => [1, 'a'])
-    assert.strictEqual(fa.exec(), 'a')
+    assert.strictEqual(pipeOp(fa, execWriter), 'a')
   })
 
   it('map', () => {
@@ -37,12 +48,12 @@ describe('Writer', () => {
 
   it('listens', () => {
     const fa = new Writer(() => [1, 'a'])
-    assert.deepStrictEqual(listens(fa, w => w.length).run(), [[1, 1], 'a'])
+    assert.deepStrictEqual(pipeOp(fa, listens2v(w => w.length)).run(), [[1, 1], 'a'])
   })
 
   it('censor', () => {
     const fa = new Writer(() => [1, ['a', 'b']])
-    assert.deepStrictEqual(censor(fa, w => w.filter(a => a !== 'a')).run(), [1, ['b']])
+    assert.deepStrictEqual(pipeOp(fa, censor2v(w => w.filter(a => a !== 'a'))).run(), [1, ['b']])
   })
 
   it('getMonad', () => {
