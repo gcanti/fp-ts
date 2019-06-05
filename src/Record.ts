89107bcb8cdafc79a6538114e9d24eeec6550300
gcanti <giulio.canti@gmail.com>
2019-06-05 10:48:45 +0200

    simplify signatures

--- src/Record.ts
+++ src/Record.ts
@@ -167,8 +167,7 @@ export function deleteAt(k: string): <A>(r: Record<string, A>) => Record<string,
 /**
  * @since 2.0.0
  */
-export function updateAt<K extends string, A>(k: K, a: A): (r: Record<K, A>) => Option<Record<K, A>>
-export function updateAt<A>(k: string, a: A): (r: Record<string, A>) => Option<Record<string, A>> {
+export function updateAt<K extends string, A>(k: K, a: A): (r: Record<K, A>) => Option<Record<K, A>> {
   return r => {
     if (!hasOwnProperty(k, r)) {
       return none
@@ -185,8 +184,7 @@ export function updateAt<A>(k: string, a: A): (r: Record<string, A>) => Option<R
 /**
  * @since 2.0.0
  */
-export function modifyAt<K extends string, A>(k: K, f: (a: A) => A): (r: Record<K, A>) => Option<Record<K, A>>
-export function modifyAt<A>(k: string, f: (a: A) => A): (r: Record<string, A>) => Option<Record<string, A>> {
+export function modifyAt<K extends string, A>(k: K, f: (a: A) => A): (r: Record<K, A>) => Option<Record<K, A>> {
   return r => {
     if (!hasOwnProperty(k, r)) {
       return none
