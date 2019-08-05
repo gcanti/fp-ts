ec8be9747c5222c23b8b978ccb04ad45611364a8
Malte Legenhausen <mlegenhausen@gmail.com>
2019-08-05 12:39:56 +0200

    ReaderTaskEither bracket function added

--- test/ReaderTaskEither.ts
+++ test/ReaderTaskEither.ts
@@ -292,4 +292,57 @@ describe('ReaderTaskEither', () => {
     const e2 = await _.run(_.fromReaderEither(RE.right(1)), {})
     assert.deepStrictEqual(e2, E.right(1))
   })
+
+  describe('bracket', () => {
+    let log: Array<string> = []
+
+    const acquireFailure = _.left('acquire failure')
+    const acquireSuccess = _.right({ res: 'acquire success' })
+    const useSuccess = () => _.right('use success')
+    const useFailure = () => _.left('use failure')
+    const releaseSuccess = () =>
+      _.readerTaskEither.fromIO(() => {
+        log.push('release success')
+      })
+    const releaseFailure = () => _.left('release failure')
+
+    beforeEach(() => {
+      log = []
+    })
+
+    it('should return the acquire error if acquire fails', async () => {
+      const e = await _.bracket(acquireFailure, useSuccess, releaseSuccess)(undefined)()
+      assert.deepStrictEqual(e, E.left('acquire failure'))
+    })
+
+    it('body and release must not be called if acquire fails', async () => {
+      await _.bracket(acquireFailure, useSuccess, releaseSuccess)(undefined)()
+      assert.deepStrictEqual(log, [])
+    })
+
+    it('should return the use error if use fails and release does not', async () => {
+      const e = await _.bracket(acquireSuccess, useFailure, releaseSuccess)(undefined)()
+      assert.deepStrictEqual(e, E.left('use failure'))
+    })
+
+    it('should return the release error if both use and release fail', async () => {
+      const e = await _.bracket(acquireSuccess, useFailure, releaseFailure)(undefined)()
+      assert.deepStrictEqual(e, E.left('release failure'))
+    })
+
+    it('release must be called if the body returns', async () => {
+      await _.bracket(acquireSuccess, useSuccess, releaseSuccess)(undefined)()
+      assert.deepStrictEqual(log, ['release success'])
+    })
+
+    it('release must be called if the body throws', async () => {
+      await _.bracket(acquireSuccess, useFailure, releaseSuccess)(undefined)()
+      assert.deepStrictEqual(log, ['release success'])
+    })
+
+    it('should return the release error if release fails', async () => {
+      const e = await _.bracket(acquireSuccess, useSuccess, releaseFailure)(undefined)()
+      assert.deepStrictEqual(e, E.left('release failure'))
+    })
+  })
 })
