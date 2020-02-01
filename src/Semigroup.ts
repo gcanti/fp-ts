f6661cca6a8789137e145ac89f84d01662051019
gcanti <giulio.canti@gmail.com>
2020-02-01 19:20:13 +0100

    add getIntercalateSemigroup

--- src/Semigroup.ts
+++ src/Semigroup.ts
@@ -177,3 +177,22 @@ export const semigroupString: Semigroup<string> = {
 export const semigroupVoid: Semigroup<void> = {
   concat: () => undefined
 }
+
+/**
+ * You can glue items between and stay associative
+ *
+ * @example
+ * import { getIntercalateSemigroup, semigroupString } from 'fp-ts/lib/Semigroup'
+ *
+ * const S = getIntercalateSemigroup(' ')(semigroupString)
+ *
+ * assert.strictEqual(S.concat('a', 'b'), 'a b')
+ * assert.strictEqual(S.concat(S.concat('a', 'b'), 'c'), S.concat('a', S.concat('b', 'c')))
+ *
+ * @since 2.5.0
+ */
+export function getIntercalateSemigroup<A>(a: A): (S: Semigroup<A>) => Semigroup<A> {
+  return S => ({
+    concat: (x, y) => S.concat(x, S.concat(a, y))
+  })
+}
