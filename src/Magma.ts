09aa68971947366b79a22e6e5779a3b1ad0abf14
gcanti <giulio.canti@gmail.com>
2020-02-04 18:40:15 +0100

    Magma: add link to Semigroup, closes #1114

--- src/Magma.ts
+++ src/Magma.ts
@@ -1,6 +1,8 @@
 /**
  * A `Magma` is a pair `(A, concat)` in which `A` is a non-empty set and `concat` is a binary operation on `A`
  *
+ * See [Semigroup](https://gcanti.github.io/fp-ts/modules/Semigroup.ts.html) for some instances.
+ *
  * @since 2.0.0
  */
 export interface Magma<A> {
