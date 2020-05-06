44c30a9beef8c958e4b88042a79353c20a8e2653
gcanti <giulio.canti@gmail.com>
2020-05-06 15:35:48 +0200

    add eqDate, eqMonth, eqYear

--- src/Date.ts
+++ src/Date.ts
@@ -2,6 +2,7 @@
  * @since 2.0.0
  */
 import { IO } from './IO'
+import { eqNumber, contramap, Eq } from './Eq'
 
 /**
  * Returns the current `Date`
@@ -16,3 +17,18 @@ export const create: IO<Date> = () => new Date()
  * @since 2.0.0
  */
 export const now: IO<number> = () => new Date().getTime()
+
+/**
+ * @since 2.6.0
+ */
+export const eqDate: Eq<Date> = contramap((x: Date) => x.getDate())(eqNumber)
+
+/**
+ * @since 2.6.0
+ */
+export const eqMonth: Eq<Date> = contramap((x: Date) => x.getMonth())(eqNumber)
+
+/**
+ * @since 2.6.0
+ */
+export const eqYear: Eq<Date> = contramap((x: Date) => x.getFullYear())(eqNumber)
--- test/Date.ts
+++ test/Date.ts
@@ -1,17 +1,32 @@
 import * as assert from 'assert'
-import { create, now } from '../src/Date'
+import * as _ from '../src/Date'
 
 describe('Date', () => {
   it('create', () => {
-    const d1 = create()
+    const d1 = _.create()
     const m2 = new Date().getTime()
     assert.deepStrictEqual(d1 instanceof Date, true)
     assert.deepStrictEqual(d1.getTime(), m2)
   })
 
   it('now', () => {
-    const m1 = now()
+    const m1 = _.now()
     const m2 = new Date().getTime()
     assert.deepStrictEqual(m1, m2)
+  })
+
+  it('eqDate', () => {
+    assert.deepStrictEqual(_.eqDate.equals(new Date(2000, 10, 1), new Date(2000, 11, 1)), true)
+    assert.deepStrictEqual(_.eqDate.equals(new Date(2000, 10, 1), new Date(2000, 11, 2)), false)
+  })
+
+  it('eqMonth', () => {
+    assert.deepStrictEqual(_.eqMonth.equals(new Date(2000, 10, 1), new Date(2000, 10, 7)), true)
+    assert.deepStrictEqual(_.eqMonth.equals(new Date(2000, 10, 1), new Date(2000, 11, 7)), false)
+  })
+
+  it('eqYear', () => {
+    assert.deepStrictEqual(_.eqYear.equals(new Date(2000, 10, 1), new Date(2000, 11, 7)), true)
+    assert.deepStrictEqual(_.eqYear.equals(new Date(2000, 10, 1), new Date(2001, 11, 7)), false)
   })
 })
