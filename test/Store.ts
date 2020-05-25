98a33652f22063211c8ede01509760caf066d0e1
gcanti <giulio.canti@gmail.com>
2020-05-25 13:05:39 +0200

    remove pipeable from Store

--- test/Store.ts
+++ test/Store.ts
@@ -1,33 +1,63 @@
 import * as assert from 'assert'
-import { Store, store, seek, seeks, peeks, experiment } from '../src/Store'
+import * as _ from '../src/Store'
 import { array } from '../src/Array'
 import { pipe } from '../src/pipeable'
 
 const len = (s: string): number => s.length
 
 describe('Store', () => {
-  it('map', () => {
-    const wa: Store<string, number> = { peek: len, pos: 'a' }
-    assert.deepStrictEqual(store.extract(store.map(wa, (n) => n + 1)), 2)
-  })
+  describe('pipeables', () => {
+    it('map', () => {
+      const wa: _.Store<string, number> = { peek: len, pos: 'a' }
+      assert.deepStrictEqual(
+        _.extract(
+          pipe(
+            wa,
+            _.map((n) => n + 1)
+          )
+        ),
+        2
+      )
+    })
 
-  it('extends', () => {
-    const wa: Store<string, number> = { peek: len, pos: 'a' }
-    assert.deepStrictEqual(store.extract(store.extend(wa, (wa) => store.extract(store.map(wa, (n) => n + 1)))), 2)
+    it('extend', () => {
+      const wa: _.Store<string, number> = { peek: len, pos: 'a' }
+      assert.deepStrictEqual(
+        _.extract(
+          pipe(
+            wa,
+            _.extend((wa) =>
+              _.extract(
+                pipe(
+                  wa,
+                  _.map((n) => n + 1)
+                )
+              )
+            )
+          )
+        ),
+        2
+      )
+    })
+
+    it('duplicate', () => {
+      const wa: _.Store<string, number> = { peek: len, pos: 'a' }
+      assert.deepStrictEqual(_.extract(_.extract(pipe(wa, _.duplicate))), 1)
+    })
   })
 
   it('seek', () => {
-    const wa: Store<string, number> = { peek: len, pos: 'a' }
-    assert.deepStrictEqual(store.extract(pipe(wa, seek('aa'))), 2)
+    const wa: _.Store<string, number> = { peek: len, pos: 'a' }
+    assert.deepStrictEqual(_.extract(pipe(wa, _.seek('aa'))), 2)
   })
 
   it('seeks', () => {
-    const wa: Store<string, number> = { peek: len, pos: 'a' }
+    const wa: _.Store<string, number> = { peek: len, pos: 'a' }
     assert.deepStrictEqual(
-      store.extract(
+      _.store.extract(
         pipe(
           wa,
-          seeks((s) => s + 'a')
+          _.seeks((s) => s + 'a')
         )
       ),
       2
@@ -35,22 +65,22 @@ describe('Store', () => {
   })
 
   it('peeks', () => {
-    const wa: Store<string, number> = { peek: len, pos: 'a' }
+    const wa: _.Store<string, number> = { peek: len, pos: 'a' }
     assert.deepStrictEqual(
       pipe(
         wa,
-        peeks((s) => s + 'a')
+        _.peeks((s) => s + 'a')
       ),
       2
     )
   })
 
   it('experiment', () => {
-    const wa: Store<string, number> = { peek: len, pos: 'a' }
+    const wa: _.Store<string, number> = { peek: len, pos: 'a' }
     assert.deepStrictEqual(
       pipe(
         wa,
-        experiment(array)((s) => [s, s + 'a'])
+        _.experiment(array)((s) => [s, s + 'a'])
       ),
       [1, 2]
     )
