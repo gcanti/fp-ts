e69ff16c7ce35d3d21bb90f05f9d31477ed8fd21
gcanti <giulio.canti@gmail.com>
2020-06-05 12:16:59 +0200

    Perf: pipe implemented in terms of flow

--- perf/pipe.ts
+++ perf/pipe.ts
@@ -1,34 +1,40 @@
 import * as Benchmark from 'benchmark'
 import * as O from '../src/Option'
 import { pipe } from '../src/pipeable'
+import { flow } from '../src/function'
 
 const suite = new Benchmark.Suite()
 
 /*
-pipe x 21,640,747 ops/sec ±0.48% (85 runs sampled)
-static dictionary x 57,118,380 ops/sec ±0.35% (88 runs sampled)
+pipe x 13,635,124 ops/sec ±0.61% (90 runs sampled)
+pipe implemented in terms of flow x 3,163,124 ops/sec ±0.35% (87 runs sampled)
 */
 
+// pipe implemented in terms of  flow
+const pipe2: typeof pipe = (a: unknown, ...fns: ReadonlyArray<any>): Function => (flow as any)(...fns)(a)
+
 suite
-  .add('pipe', function() {
+  .add('pipe', function () {
     pipe(
       O.some(1),
-      O.map(n => n * 2),
-      O.chain(n => (n > 2 ? O.some(n) : O.none)),
-      O.map(n => n + 1)
+      O.map((n) => n * 2),
+      O.chain((n) => (n > 2 ? O.some(n) : O.none)),
+      O.map((n) => n + 1)
     )
   })
-  .add('static dictionary', function() {
-    const x = O.some(1)
-    const y = O.option.map(x, n => n * 2)
-    const z = O.option.chain(y, n => (n > 2 ? O.some(n) : O.none))
-    O.option.map(z, n => n + 1)
+  .add('pipe implemented in terms of flow', function () {
+    pipe2(
+      O.some(1),
+      O.map((n) => n * 2),
+      O.chain((n) => (n > 2 ? O.some(n) : O.none)),
+      O.map((n) => n + 1)
+    )
   })
-  .on('cycle', function(event: any) {
+  .on('cycle', function (event: any) {
     // tslint:disable-next-line: no-console
     console.log(String(event.target))
   })
-  .on('complete', function(this: any) {
+  .on('complete', function (this: any) {
     // tslint:disable-next-line: no-console
     console.log('Fastest is ' + this.filter('fastest').map('name'))
   })
