3cdbbbd94d9266c29dd0ea74b23c7643c6243e79
gcanti <giulio.canti@gmail.com>
2020-06-07 22:53:50 +0200

    remove EitherT from IOEither

--- src/IOEither.ts
+++ src/IOEither.ts
@@ -7,9 +7,8 @@
 import { Alt2, Alt2C } from './Alt'
 import { Bifunctor2 } from './Bifunctor'
 import * as E from './Either'
-import { getEitherM } from './EitherT'
 import { Filterable2C, getFilterableComposition } from './Filterable'
-import { identity, Lazy, Predicate, Refinement, pipe } from './function'
+import { identity, Lazy, Predicate, Refinement, pipe, flow } from './function'
 import * as I from './IO'
 import { Monad2, Monad2C } from './Monad'
 import { MonadIO2, MonadIO2C } from './MonadIO'
@@ -18,17 +17,14 @@ import { Monoid } from './Monoid'
 import { Option } from './Option'
 import { Semigroup } from './Semigroup'
 import { getValidationM } from './ValidationT'
+import { apComposition } from './Apply'
+
+// -------------------------------------------------------------------------------------
+// model
+// -------------------------------------------------------------------------------------
 
 import Either = E.Either
 import IO = I.IO
-
-const T = /*#__PURE__*/ getEitherM(I.monadIO)
-
-declare module './HKT' {
-  interface URItoKind2<E, A> {
-    readonly IOEither: IOEither<E, A>
-  }
-}
 
 /**
  * @category model
@@ -42,51 +38,82 @@ export const URI = 'IOEither'
  */
 export type URI = typeof URI
 
+declare module './HKT' {
+  interface URItoKind2<E, A> {
+    readonly [URI]: IOEither<E, A>
+  }
+}
+
 /**
  * @category model
  * @since 2.0.0
  */
 export interface IOEither<E, A> extends IO<Either<E, A>> {}
 
-/**
- * @category constructors
- * @since 2.0.0
- */
-export const left: <E = never, A = never>(l: E) => IOEither<E, A> = T.left
+// -------------------------------------------------------------------------------------
+// constructors
+// -------------------------------------------------------------------------------------
 
 /**
  * @category constructors
  * @since 2.0.0
  */
-export const right: <E = never, A = never>(a: A) => IOEither<E, A> = T.of
+export const left: <E = never, A = never>(l: E) => IOEither<E, A> =
+  /*#__PURE__*/
+  flow(E.left, I.of)
 
 /**
  * @category constructors
  * @since 2.0.0
  */
-export const rightIO: <E = never, A = never>(ma: IO<A>) => IOEither<E, A> = T.rightM
+export const right: <E = never, A = never>(a: A) => IOEither<E, A> =
+  /*#__PURE__*/
+  flow(E.right, I.of)
 
 /**
  * @category constructors
  * @since 2.0.0
  */
-export const leftIO: <E = never, A = never>(me: IO<E>) => IOEither<E, A> = T.leftM
+export const rightIO: <E = never, A = never>(ma: IO<A>) => IOEither<E, A> =
+  /*#__PURE__*/
+  I.map(E.right)
+
+/**
+ * @category constructors
+ * @since 2.0.0
+ */
+export const leftIO: <E = never, A = never>(me: IO<E>) => IOEither<E, A> =
+  /*#__PURE__*/
+  I.map(E.left)
+
+/**
+ * Constructs a new `IOEither` from a function that performs a side effect and might throw
+ *
+ * @category constructors
+ * @since 2.0.0
+ */
+export function tryCatch<E, A>(f: Lazy<A>, onError: (reason: unknown) => E): IOEither<E, A> {
+  return () => E.tryCatch(f, onError)
+}
+
+// -------------------------------------------------------------------------------------
+// destructors
+// -------------------------------------------------------------------------------------
 
 /**
  * @category destructors
  * @since 2.0.0
  */
-export function fold<E, A, B>(onLeft: (e: E) => IO<B>, onRight: (a: A) => IO<B>): (ma: IOEither<E, A>) => IO<B> {
-  return (ma) => T.fold(ma, onLeft, onRight)
-}
+export const fold: <E, A, B>(onLeft: (e: E) => IO<B>, onRight: (a: A) => IO<B>) => (ma: IOEither<E, A>) => IO<B> =
+  /*#__PURE__*/
+  flow(E.fold, I.chain)
 
 /**
  * @category destructors
  * @since 2.0.0
  */
-export function getOrElse<E, A>(onLeft: (e: E) => IO<A>): (ma: IOEither<E, A>) => IO<A> {
-  return (ma) => T.getOrElse(ma, onLeft)
-}
+export const getOrElse: <E, A>(onLeft: (e: E) => IO<A>) => (ma: IOEither<E, A>) => IO<A> = (onLeft) =>
+  I.chain(E.fold(onLeft, I.of))
 
 /**
  * @category destructors
@@ -94,19 +121,224 @@ export function getOrElse<E, A>(onLeft: (e: E) => IO<A>): (ma: IOEither<E, A>) =
  */
 export const getOrElseW: <E, B>(onLeft: (e: E) => IO<B>) => <A>(ma: IOEither<E, A>) => IO<A | B> = getOrElse as any
 
-/**
- * @category combinators
- * @since 2.0.0
- */
-export function orElse<E, A, M>(onLeft: (e: E) => IOEither<M, A>): (ma: IOEither<E, A>) => IOEither<M, A> {
-  return (ma) => T.orElse(ma, onLeft)
-}
+// -------------------------------------------------------------------------------------
+// combinators
+// -------------------------------------------------------------------------------------
 
 /**
  * @category combinators
  * @since 2.0.0
  */
-export const swap: <E, A>(ma: IOEither<E, A>) => IOEither<A, E> = T.swap
+export const orElse: <E, A, M>(onLeft: (e: E) => IOEither<M, A>) => (ma: IOEither<E, A>) => IOEither<M, A> = (f) =>
+  I.chain(E.fold(f, right))
+
+/**
+ * @category combinators
+ * @since 2.0.0
+ */
+export const swap: <E, A>(ma: IOEither<E, A>) => IOEither<A, E> =
+  /*#__PURE__*/
+  I.map(E.swap)
+
+/**
+ * @category combinators
+ * @since 2.0.0
+ */
+export const filterOrElse: {
+  <E, A, B extends A>(refinement: Refinement<A, B>, onFalse: (a: A) => E): (ma: IOEither<E, A>) => IOEither<E, B>
+  <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E): (ma: IOEither<E, A>) => IOEither<E, A>
+} = <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E) => (ma: IOEither<E, A>) =>
+  pipe(
+    ma,
+    chain((a) => (predicate(a) ? right(a) : left(onFalse(a))))
+  )
+
+/**
+ * @category combinators
+ * @since 2.4.0
+ */
+export function fromEitherK<E, A extends ReadonlyArray<unknown>, B>(
+  f: (...a: A) => Either<E, B>
+): (...a: A) => IOEither<E, B> {
+  return (...a) => fromEither(f(...a))
+}
+
+/**
+ * @category combinators
+ * @since 2.4.0
+ */
+export function chainEitherK<E, A, B>(f: (a: A) => Either<E, B>): (ma: IOEither<E, A>) => IOEither<E, B> {
+  return chain(fromEitherK(f))
+}
+
+/**
+ * @category combinators
+ * @since 2.6.1
+ */
+export const chainEitherKW: <D, A, B>(
+  f: (a: A) => Either<D, B>
+) => <E>(ma: IOEither<E, A>) => IOEither<E | D, B> = chainEitherK as any
+
+// -------------------------------------------------------------------------------------
+// pipeables
+// -------------------------------------------------------------------------------------
+
+/**
+ * @category Functor
+ * @since 2.0.0
+ */
+export const map: <A, B>(f: (a: A) => B) => <E>(fa: IOEither<E, A>) => IOEither<E, B> = (f) => I.map(E.map(f))
+
+/**
+ * @category Bifunctor
+ * @since 2.0.0
+ */
+export const bimap: <E, G, A, B>(f: (e: E) => G, g: (a: A) => B) => (fa: IOEither<E, A>) => IOEither<G, B> = flow(
+  E.bimap,
+  I.map
+)
+
+/**
+ * @category Bifunctor
+ * @since 2.0.0
+ */
+export const mapLeft: <E, G>(f: (e: E) => G) => <A>(fa: IOEither<E, A>) => IOEither<G, A> = (f) => I.map(E.mapLeft(f))
+
+/**
+ * @category Apply
+ * @since 2.0.0
+ */
+export const ap: <E, A>(fa: IOEither<E, A>) => <B>(fab: IOEither<E, (a: A) => B>) => IOEither<E, B> =
+  /*#__PURE__*/
+  apComposition(I.io, E.either)
+
+/**
+ * @category Apply
+ * @since 2.0.0
+ */
+export const apFirst: <E, B>(fb: IOEither<E, B>) => <A>(fa: IOEither<E, A>) => IOEither<E, A> = (fb) => (fa) =>
+  pipe(
+    fa,
+    map((a) => () => a),
+    ap(fb)
+  )
+
+/**
+ * @category Apply
+ * @since 2.0.0
+ */
+export const apSecond = <E, B>(fb: IOEither<E, B>) => <A>(fa: IOEither<E, A>): IOEither<E, B> =>
+  pipe(
+    fa,
+    map(() => (b: B) => b),
+    ap(fb)
+  )
+
+/**
+ * @category Monad
+ * @since 2.0.0
+ */
+export const chain: <E, A, B>(f: (a: A) => IOEither<E, B>) => (ma: IOEither<E, A>) => IOEither<E, B> = (f) =>
+  I.chain(E.fold(left, f))
+
+/**
+ * @category Monad
+ * @since 2.0.0
+ */
+export const chainFirst: <E, A, B>(f: (a: A) => IOEither<E, B>) => (ma: IOEither<E, A>) => IOEither<E, A> = (f) =>
+  chain((a) =>
+    pipe(
+      f(a),
+      map(() => a)
+    )
+  )
+
+/**
+ * @category Monad
+ * @since 2.6.0
+ */
+export const chainW: <D, A, B>(
+  f: (a: A) => IOEither<D, B>
+) => <E>(ma: IOEither<E, A>) => IOEither<E | D, B> = chain as any
+
+/**
+ * @category Monad
+ * @since 2.0.0
+ */
+export const flatten: <E, A>(mma: IOEither<E, IOEither<E, A>>) => IOEither<E, A> = chain(identity)
+
+/**
+ * @category Alt
+ * @since 2.0.0
+ */
+export const alt: <E, A>(that: () => IOEither<E, A>) => (fa: IOEither<E, A>) => IOEither<E, A> = (that) =>
+  I.chain(E.fold(that, right))
+
+/**
+ * Make sure that a resource is cleaned up in the event of an exception (*). The release action is called regardless of
+ * whether the body action throws (*) or returns.
+ *
+ * (*) i.e. returns a `Left`
+ *
+ * @category MonadThrow
+ * @since 2.0.0
+ */
+export const bracket = <E, A, B>(
+  acquire: IOEither<E, A>,
+  use: (a: A) => IOEither<E, B>,
+  release: (a: A, e: Either<E, B>) => IOEither<E, void>
+): IOEither<E, B> =>
+  pipe(
+    acquire,
+    chain((a) =>
+      pipe(
+        pipe(use(a), I.map(E.right)),
+        chain((e) =>
+          pipe(
+            release(a, e),
+            chain(() => (E.isLeft(e) ? left(e.left) : of(e.right)))
+          )
+        )
+      )
+    )
+  )
+
+/**
+ * @category constructors
+ * @since 2.0.0
+ */
+export const fromOption: <E>(onNone: () => E) => <A>(ma: Option<A>) => IOEither<E, A> = (onNone) => (ma) =>
+  ma._tag === 'None' ? left(onNone()) : right(ma.value)
+
+/**
+ * @category constructors
+ * @since 2.0.0
+ */
+export const fromPredicate: {
+  <E, A, B extends A>(refinement: Refinement<A, B>, onFalse: (a: A) => E): (a: A) => IOEither<E, B>
+  <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E): (a: A) => IOEither<E, A>
+} = <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E) => (a: A) => (predicate(a) ? right(a) : left(onFalse(a)))
+
+/**
+ * @category constructors
+ * @since 2.0.0
+ */
+export const fromEither: <E, A>(ma: E.Either<E, A>) => IOEither<E, A> = (ma) =>
+  E.isLeft(ma) ? left(ma.left) : right(ma.right)
+
+// -------------------------------------------------------------------------------------
+// instances
+// -------------------------------------------------------------------------------------
+
+const map_: Monad2<URI>['map'] = (fa, f) => pipe(fa, map(f))
+const ap_: Monad2<URI>['ap'] = (fab, fa) => pipe(fab, ap(fa))
+const of = right
+const chain_: Monad2<URI>['chain'] = (ma, f) => pipe(ma, chain(f))
+const bimap_: Bifunctor2<URI>['bimap'] = (fa, f, g) => pipe(fa, bimap(f, g))
+const mapLeft_: Bifunctor2<URI>['mapLeft'] = (fa, f) => pipe(fa, mapLeft(f))
+const alt_: Alt2<URI>['alt'] = (fa, that) => pipe(fa, alt(that))
+const fromIO_ = rightIO
+const throwError_ = left
 
 /**
  * Semigroup returning the left-most non-`Left` value. If both operands are `Right`s then the inner values are
@@ -142,36 +374,6 @@ export function getApplyMonoid<E, A>(M: Monoid<A>): Monoid<IOEither<E, A>> {
 }
 
 /**
- * Constructs a new `IOEither` from a function that performs a side effect and might throw
- *
- * @category constructors
- * @since 2.0.0
- */
-export function tryCatch<E, A>(f: Lazy<A>, onError: (reason: unknown) => E): IOEither<E, A> {
-  return () => E.tryCatch(f, onError)
-}
-
-/**
- * Make sure that a resource is cleaned up in the event of an exception (*). The release action is called regardless of
- * whether the body action throws (*) or returns.
- *
- * (*) i.e. returns a `Left`
- *
- * @since 2.0.0
- */
-export function bracket<E, A, B>(
-  acquire: IOEither<E, A>,
-  use: (a: A) => IOEither<E, B>,
-  release: (a: A, e: Either<E, B>) => IOEither<E, void>
-): IOEither<E, B> {
-  return T.chain(acquire, (a) =>
-    T.chain(pipe(use(a), I.map(E.right)), (e) =>
-      T.chain(release(a, e), () => (E.isLeft(e) ? T.left(e.left) : T.of(e.right)))
-    )
-  )
-}
-
-/**
  * @category instances
  * @since 2.0.0
  */
@@ -180,9 +382,17 @@ export function getIOValidation<E>(
 ): Monad2C<URI, E> & Bifunctor2<URI> & Alt2C<URI, E> & MonadIO2C<URI, E> & MonadThrow2C<URI, E> {
   const V = getValidationM(S, I.monadIO)
   return {
+    URI,
     _E: undefined as any,
-    ...ioEither,
-    ...V
+    map: map_,
+    ap: V.ap,
+    of,
+    chain: chain_,
+    bimap: bimap_,
+    mapLeft: mapLeft_,
+    alt: V.alt,
+    fromIO: fromIO_,
+    throwError: throwError_
   }
 }
 
@@ -191,163 +401,21 @@ export function getIOValidation<E>(
  * @since 2.1.0
  */
 export function getFilterable<E>(M: Monoid<E>): Filterable2C<URI, E> {
-  const F = E.getWitherable(M)
+  const W = E.getWitherable(M)
+  const F = getFilterableComposition(I.monadIO, W)
 
   return {
     URI,
     _E: undefined as any,
-    ...getFilterableComposition(I.monadIO, F)
+    map: map_,
+    compact: F.compact,
+    separate: F.separate,
+    filter: F.filter,
+    filterMap: F.filterMap,
+    partition: F.partition,
+    partitionMap: F.partitionMap
   }
 }
-
-/**
- * @since 2.4.0
- */
-export function fromEitherK<E, A extends ReadonlyArray<unknown>, B>(
-  f: (...a: A) => Either<E, B>
-): (...a: A) => IOEither<E, B> {
-  return (...a) => fromEither(f(...a))
-}
-
-/**
- * @category Monad
- * @since 2.4.0
- */
-export function chainEitherK<E, A, B>(f: (a: A) => Either<E, B>): (ma: IOEither<E, A>) => IOEither<E, B> {
-  return chain(fromEitherK(f))
-}
-
-// -------------------------------------------------------------------------------------
-// pipeables
-// -------------------------------------------------------------------------------------
-
-/**
- * @category Alt
- * @since 2.0.0
- */
-export const alt: <E, A>(that: () => IOEither<E, A>) => (fa: IOEither<E, A>) => IOEither<E, A> = (that) => (fa) =>
-  T.alt(fa, that)
-
-/**
- * @category Apply
- * @since 2.0.0
- */
-export const ap: <E, A>(fa: IOEither<E, A>) => <B>(fab: IOEither<E, (a: A) => B>) => IOEither<E, B> = (fa) => (fab) =>
-  T.ap(fab, fa)
-
-/**
- * @category Apply
- * @since 2.0.0
- */
-export const apFirst: <E, B>(fb: IOEither<E, B>) => <A>(fa: IOEither<E, A>) => IOEither<E, A> = (fb) => (fa) =>
-  T.ap(
-    T.map(fa, (a) => () => a),
-    fb
-  )
-
-/**
- * @category Apply
- * @since 2.0.0
- */
-export const apSecond = <E, B>(fb: IOEither<E, B>) => <A>(fa: IOEither<E, A>): IOEither<E, B> =>
-  T.ap(
-    T.map(fa, () => (b: B) => b),
-    fb
-  )
-
-/**
- * @category Monad
- * @since 2.0.0
- */
-export const chain: <E, A, B>(f: (a: A) => IOEither<E, B>) => (ma: IOEither<E, A>) => IOEither<E, B> = (f) => (ma) =>
-  T.chain(ma, f)
-
-/**
- * @category Monad
- * @since 2.0.0
- */
-export const chainFirst: <E, A, B>(f: (a: A) => IOEither<E, B>) => (ma: IOEither<E, A>) => IOEither<E, A> = (f) => (
-  ma
-) => T.chain(ma, (a) => T.map(f(a), () => a))
-
-/**
- * @category Monad
- * @since 2.6.0
- */
-export const chainW: <D, A, B>(
-  f: (a: A) => IOEither<D, B>
-) => <E>(ma: IOEither<E, A>) => IOEither<E | D, B> = chain as any
-
-/**
- * @since 2.6.1
- */
-export const chainEitherKW: <D, A, B>(
-  f: (a: A) => Either<D, B>
-) => <E>(ma: IOEither<E, A>) => IOEither<E | D, B> = chainEitherK as any
-
-/**
- * @category Monad
- * @since 2.0.0
- */
-export const flatten: <E, A>(mma: IOEither<E, IOEither<E, A>>) => IOEither<E, A> = (mma) => T.chain(mma, identity)
-
-/**
- * @category Functor
- * @since 2.0.0
- */
-export const map: <A, B>(f: (a: A) => B) => <E>(fa: IOEither<E, A>) => IOEither<E, B> = (f) => (fa) => T.map(fa, f)
-
-/**
- * @category Bifunctor
- * @since 2.0.0
- */
-export const bimap: <E, G, A, B>(f: (e: E) => G, g: (a: A) => B) => (fa: IOEither<E, A>) => IOEither<G, B> = (f, g) => (
-  fa
-) => T.bimap(fa, f, g)
-
-/**
- * @category Bifunctor
- * @since 2.0.0
- */
-export const mapLeft: <E, G>(f: (e: E) => G) => <A>(fa: IOEither<E, A>) => IOEither<G, A> = (f) => (fa) =>
-  T.mapLeft(fa, f)
-
-/**
- * @category constructors
- * @since 2.0.0
- */
-export const fromOption: <E>(onNone: () => E) => <A>(ma: Option<A>) => IOEither<E, A> = (onNone) => (ma) =>
-  ma._tag === 'None' ? left(onNone()) : right(ma.value)
-
-/**
- * @category constructors
- * @since 2.0.0
- */
-export const fromPredicate: {
-  <E, A, B extends A>(refinement: Refinement<A, B>, onFalse: (a: A) => E): (a: A) => IOEither<E, B>
-  <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E): (a: A) => IOEither<E, A>
-} = <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E) => (a: A) => (predicate(a) ? right(a) : left(onFalse(a)))
-
-/**
- * @category combinators
- * @since 2.0.0
- */
-export const filterOrElse: {
-  <E, A, B extends A>(refinement: Refinement<A, B>, onFalse: (a: A) => E): (ma: IOEither<E, A>) => IOEither<E, B>
-  <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E): (ma: IOEither<E, A>) => IOEither<E, A>
-} = <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E) => (ma: IOEither<E, A>) =>
-  T.chain(ma, (a) => (predicate(a) ? right(a) : left(onFalse(a))))
-
-/**
- * @category constructors
- * @since 2.0.0
- */
-export const fromEither: <E, A>(ma: E.Either<E, A>) => IOEither<E, A> = (ma) =>
-  ma._tag === 'Left' ? left(ma.left) : right(ma.right)
-
-// -------------------------------------------------------------------------------------
-// instances
-// -------------------------------------------------------------------------------------
 
 /**
  * @category instances
@@ -355,13 +423,13 @@ export const fromEither: <E, A>(ma: E.Either<E, A>) => IOEither<E, A> = (ma) =>
  */
 export const ioEither: Monad2<URI> & Bifunctor2<URI> & Alt2<URI> & MonadIO2<URI> & MonadThrow2<URI> = {
   URI,
-  bimap: T.bimap,
-  mapLeft: T.mapLeft,
-  map: T.map,
-  of: right,
-  ap: T.ap,
-  chain: T.chain,
-  alt: T.alt,
-  fromIO: rightIO,
-  throwError: left
+  bimap: bimap_,
+  mapLeft: mapLeft_,
+  map: map_,
+  of,
+  ap: ap_,
+  chain: chain_,
+  alt: alt_,
+  fromIO: fromIO_,
+  throwError: throwError_
 }
