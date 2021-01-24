ac1ff05da806a6e89ed6077b3e9d4c49786f7836
gcanti <giulio.canti@gmail.com>
2021-01-24 11:22:57 +0100

    test ValidationT

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts', '!src/pipeable.ts', '!src/StateT.ts', '!src/WriterT.ts', '!src/ValidationT.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/pipeable.ts', '!src/StateT.ts', '!src/WriterT.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
