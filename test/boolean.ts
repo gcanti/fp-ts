ff0a20ac5df4f90fd93f46efd4b25ecc4019f078
gcanti <giulio.canti@gmail.com>
2021-01-31 07:58:26 +0100

    move boolean BooleanAlgebra to boolean module

--- test/boolean.ts
+++ test/boolean.ts
@@ -18,4 +18,26 @@ describe('boolean', () => {
       'false'
     )
   })
+
+  it('BooleanAlgebra', () => {
+    const BA = B.BooleanAlgebra
+    assert.deepStrictEqual(BA.implies(true, true), true)
+    assert.deepStrictEqual(BA.implies(true, false), false)
+    assert.deepStrictEqual(BA.implies(false, true), true)
+    assert.deepStrictEqual(BA.implies(false, false), true)
+
+    assert.deepStrictEqual(BA.join(true, true), true)
+    assert.deepStrictEqual(BA.join(true, false), true)
+    assert.deepStrictEqual(BA.join(false, true), true)
+    assert.deepStrictEqual(BA.join(false, false), false)
+
+    assert.deepStrictEqual(BA.meet(true, true), true)
+    assert.deepStrictEqual(BA.meet(true, false), false)
+
+    assert.deepStrictEqual(BA.not(true), false)
+    assert.deepStrictEqual(BA.not(false), true)
+
+    assert.deepStrictEqual(BA.one, true)
+    assert.deepStrictEqual(BA.zero, false)
+  })
 })
