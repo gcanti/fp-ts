16a384eff0b4d6627b8a713c325b5a2fda5be724
gcanti <giulio.canti@gmail.com>
2021-01-31 08:52:10 +0100

    move function BooleanAlgebra to function module

--- test/BooleanAlgebra.ts
+++ test/BooleanAlgebra.ts
@@ -17,28 +17,6 @@ describe('BooleanAlgebra', () => {
     assert.deepStrictEqual(BA.zero, undefined)
   })
 
-  it('getFunctionBooleanAlgebra', () => {
-    const BA = _.getFunctionBooleanAlgebra(B.BooleanAlgebra)<number>()
-    const f = (n: number) => n >= 0
-    const g = (n: number) => n < 2
-    assert.deepStrictEqual(BA.implies(f, g)(1), true)
-    assert.deepStrictEqual(BA.implies(f, g)(-1), true)
-
-    assert.deepStrictEqual(BA.join(f, g)(1), true)
-    assert.deepStrictEqual(BA.join(f, g)(-1), true)
-
-    assert.deepStrictEqual(BA.meet(f, g)(1), true)
-    assert.deepStrictEqual(BA.meet(f, g)(-1), false)
-
-    assert.deepStrictEqual(BA.not(f)(1), false)
-    assert.deepStrictEqual(BA.not(f)(-1), true)
-
-    assert.deepStrictEqual(BA.one(1), true)
-    assert.deepStrictEqual(BA.one(-1), true)
-    assert.deepStrictEqual(BA.zero(1), false)
-    assert.deepStrictEqual(BA.zero(-1), false)
-  })
-
   it('getDualBooleanAlgebra', () => {
     const BA = _.getDualBooleanAlgebra(B.BooleanAlgebra)
     assert.deepStrictEqual(BA.implies(true, true), true)
