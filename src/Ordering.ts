89f5912cf832623b914990ee11fe1a987caa3f46
gcanti <giulio.canti@gmail.com>
2021-02-01 10:40:43 +0100

    Ordering: rename instances

--- src/Ordering.ts
+++ src/Ordering.ts
@@ -1,9 +1,9 @@
 /**
  * @since 2.0.0
  */
-import { Semigroup } from './Semigroup'
-import { Eq } from './Eq'
-import { Monoid } from './Monoid'
+import * as S from './Semigroup'
+import * as E from './Eq'
+import * as M from './Monoid'
 
 // -------------------------------------------------------------------------------------
 // model
@@ -21,18 +21,26 @@ export type Ordering = -1 | 0 | 1
 
 /**
  * @category instances
- * @since 2.0.0
+ * @since 2.10.0
  */
-export const eqOrdering: Eq<Ordering> = {
+export const Eq: E.Eq<Ordering> = {
   equals: (x, y) => x === y
 }
 
 /**
  * @category instances
- * @since 2.4.0
+ * @since 2.10.0
  */
-export const monoidOrdering: Monoid<Ordering> = {
-  concat: (x, y) => (x !== 0 ? x : y),
+export const Semigroup: S.Semigroup<Ordering> = {
+  concat: (x, y) => (x !== 0 ? x : y)
+}
+
+/**
+ * @category instances
+ * @since 2.10.0
+ */
+export const Monoid: M.Monoid<Ordering> = {
+  concat: Semigroup.concat,
   empty: 0
 }
 
@@ -66,12 +74,28 @@ export function invert(O: Ordering): Ordering {
 // -------------------------------------------------------------------------------------
 
 /**
- * Use `monoidOrdering` instead
+ * Use `Semigroup` instead
  *
  * @category instances
  * @since 2.0.0
  * @deprecated
  */
-export const semigroupOrdering: Semigroup<Ordering> = {
-  concat: monoidOrdering.concat
-}
+export const semigroupOrdering: S.Semigroup<Ordering> = Semigroup
+
+/**
+ * Use `Eq` instead
+ *
+ * @category instances
+ * @since 2.0.0
+ * @deprecated
+ */
+export const eqOrdering: E.Eq<Ordering> = Eq
+
+/**
+ * Use `Monoid` instead
+ *
+ * @category instances
+ * @since 2.4.0
+ * @deprecated
+ */
+export const monoidOrdering: M.Monoid<Ordering> = Monoid
