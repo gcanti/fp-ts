738c8d625e987c4525955039772a9dd4da7e6274
gcanti <giulio.canti@gmail.com>
2021-02-03 07:52:55 +0100

    build2

--- scripts/build2.ts
+++ scripts/build2.ts
@@ -5,6 +5,7 @@ import * as RA from '../src/ReadonlyArray'
 import * as TE from '../src/TaskEither'
 import * as Tr from '../src/Tree'
 import * as T from '../src/Task'
+import * as E from '../src/Either'
 import * as fs from 'fs'
 
 // -------------------------------------------------------------------------------------
@@ -49,13 +50,6 @@ const toTree: (item: Item) => Tr.Tree<string> = Tr.unfoldTree(
   )
 )
 
-const getPackageJson = (name: string) => `{
-  "main": "./${name}.js",
-  "module": "./${name}.es6.js",
-  "typings": "./${name}.d.ts",
-  "sideEffects": false
-}`
-
 interface Module {
   readonly name: string
   readonly es5: string
@@ -70,9 +64,16 @@ const module = (name: string, es5: string, es6: string, typings: string): Module
   typings
 })
 
+const getModulePackageJson = (module: Module): string => `{
+  "main": "./${module.name}.js",
+  "module": "./${module.name}.es6.js",
+  "typings": "./${module.name}.d.ts",
+  "sideEffects": false
+}`
+
 const toFolder = (module: Module): Folder =>
   folder(module.name, [
-    file('package.json', getPackageJson(module.name)),
+    file('package.json', getModulePackageJson(module)),
     file(`${module.name}.js`, module.es5),
     file(`${module.name}.es6.js`, module.es6),
     file(`${module.name}.d.ts`, module.typings)
@@ -87,9 +88,25 @@ const readFile = TE.taskify<fs.PathLike, string, NodeJS.ErrnoException, string>(
 const writeFile = TE.taskify<fs.PathLike, string, NodeJS.ErrnoException, void>(fs.writeFile)
 const mkdir = TE.taskify(fs.mkdir)
 
-const toNames = (paths: ReadonlyArray<string>): ReadonlyArray<string> => paths.map((p) => path.basename(p, '.ts')) // .filter((name) => name !== 'index')
+const getBasename = (p: string): string => path.basename(p, '.ts')
 
-const names = pipe(glob(`./src/*.ts`), TE.map(toNames))
+const getNames: TE.TaskEither<Error, ReadonlyArray<string>> = pipe(glob(`./src/*.ts`), TE.map(RA.map(getBasename)))
+
+const es5Import = /require\(".\//g
+
+const replaceES5LocalImports = (s: string): string => s.replace(es5Import, `require("../`)
+
+const es6Import = /from '.\//g
+
+const replaceES6LocalImports = (s: string): string => s.replace(es6Import, `from '../`)
+
+const declareModule = /declare module '.\//g
+
+const replaceDeclareModule = (s: string): string => s.replace(declareModule, `declare module '../`)
+
+const typeImport = /import\(".\//g
+
+const replaceTypeImport = (s: string): string => s.replace(typeImport, `import("../`)
 
 const getModule = (name: string): TE.TaskEither<Error, Module> =>
   pipe(
@@ -97,12 +114,17 @@ const getModule = (name: string): TE.TaskEither<Error, Module> =>
     TE.bind('es5', () => readFile(`./dist/lib/${name}.js`, 'utf8')),
     TE.bind('es6', () => readFile(`./dist/es6/${name}.js`, 'utf8')),
     TE.bind('typings', () => readFile(`./dist/lib/${name}.d.ts`, 'utf8')),
-    TE.map(({ es5, es6, typings }) => module(name, es5, es6, typings))
+    TE.map(({ es5, es6, typings }) =>
+      module(
+        name,
+        replaceES5LocalImports(es5),
+        replaceES6LocalImports(es6),
+        replaceTypeImport(replaceDeclareModule(replaceES6LocalImports(typings)))
+      )
+    )
   )
 
-const traverse = RA.traverse(TE.ApplicativePar)
-
-const modules = pipe(names, TE.chain(traverse(getModule)))
+const getModules = pipe(getNames, TE.chain(TE.traverseReadonlyArray(getModule)))
 
 const writeModule = (module: Module): TE.TaskEither<Error, void> =>
   pipe(
@@ -110,16 +132,63 @@ const writeModule = (module: Module): TE.TaskEither<Error, void> =>
     TE.chain(() => writeFile(`./dist/${module.name}/${module.name}.js`, module.es5)),
     TE.chain(() => writeFile(`./dist/${module.name}/${module.name}.es6.js`, module.es6)),
     TE.chain(() => writeFile(`./dist/${module.name}/${module.name}.d.ts`, module.typings)),
-    TE.chain(() => writeFile(`./dist/${module.name}/package.json`, getPackageJson(module.name)))
+    TE.chain(() => writeFile(`./dist/${module.name}/package.json`, getModulePackageJson(module)))
   )
+
+const writeModules = pipe(getModules, TE.chainFirst(TE.traverseReadonlyArray(writeModule)))
+
+const moveFile = (from: string, to: string) =>
+  pipe(
+    readFile(from, 'utf8'),
+    TE.chain((contents) => writeFile(to, contents))
+  )
+
+const copyProjectFiles = pipe(
+  moveFile('./CHANGELOG.md', './dist/CHANGELOG.md'),
+  TE.chain(() => moveFile('./LICENSE', './dist/LICENSE')),
+  TE.chain(() => moveFile('./README.md', './dist/README.md'))
+)
+
+const writeProjectPackageJson = pipe(
+  readFile('./package.json', 'utf8'),
+  TE.chain((s) =>
+    TE.fromEither(
+      pipe(
+        E.parseJSON(s),
+        E.bimap(
+          () => new Error('invalid JSON'),
+          (json): E.Json => {
+            const clone = Object.assign(
+              {
+                main: 'index/index.js',
+                module: 'index/index.es6.js',
+                typings: 'index/index.d.ts',
+                sideEffects: false
+              },
+              json as any
+            )
+
+            delete clone.scripts
+            delete clone.files
+            delete clone.devDependencies
+
+            return clone
+          }
+        )
+      )
+    )
+  ),
+  TE.chain((contents) => writeFile('./dist/package.json', JSON.stringify(contents, null, 2)))
+)
 
 const draw = flow(toTree, Tr.drawTree)
 
 import * as Console from '../src/Console'
 
 const tree = pipe(
-  modules,
-  TE.chainFirst(traverse(writeModule)),
+  writeModules,
+  TE.chainFirst(() => copyProjectFiles),
+  TE.chainFirst(() => writeProjectPackageJson),
   TE.map(RA.map(toFolder)),
   TE.fold(
     (e) => T.fromIO(Console.error(e)),
