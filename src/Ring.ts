edfcd3e3524fe85f66d5aae78f951764ad313076
gcanti <giulio.canti@gmail.com>
2021-02-10 01:52:56 +0100

    Ring: rename `getTupleRing` to `tuple`

--- src/Ring.ts
+++ src/Ring.ts
@@ -32,10 +32,10 @@ export interface Ring<A> extends Semiring<A> {
  * Given a tuple of `Ring`s returns a `Ring` for the tuple
  *
  * @example
- * import { getTupleRing } from 'fp-ts/Ring'
+ * import { tuple } from 'fp-ts/Ring'
  * import * as N from 'fp-ts/number'
  *
- * const R = getTupleRing(N.Field, N.Field, N.Field)
+ * const R = tuple(N.Field, N.Field, N.Field)
  * assert.deepStrictEqual(R.add([1, 2, 3], [4, 5, 6]), [5, 7, 9])
  * assert.deepStrictEqual(R.mul([1, 2, 3], [4, 5, 6]), [4, 10, 18])
  * assert.deepStrictEqual(R.one, [1, 1, 1])
@@ -43,19 +43,16 @@ export interface Ring<A> extends Semiring<A> {
  * assert.deepStrictEqual(R.zero, [0, 0, 0])
  *
  * @category combinators
- * @since 2.0.0
+ * @since 2.10.0
  */
-export function getTupleRing<T extends ReadonlyArray<Ring<any>>>(
-  ...rings: T
-): Ring<{ [K in keyof T]: T[K] extends Ring<infer A> ? A : never }> {
-  return {
+export const tuple = <A extends ReadonlyArray<unknown>>(...rings: { [K in keyof A]: Ring<A[K]> }): Ring<A> =>
+  ({
     add: (x: any, y: any) => rings.map((R, i) => R.add(x[i], y[i])),
     zero: rings.map((R) => R.zero),
     mul: (x: any, y: any) => rings.map((R, i) => R.mul(x[i], y[i])),
     one: rings.map((R) => R.one),
     sub: (x: any, y: any) => rings.map((R, i) => R.sub(x[i], y[i]))
-  } as any
-}
+  } as any)
 
 // -------------------------------------------------------------------------------------
 // utils
@@ -71,6 +68,17 @@ export const negate = <A>(R: Ring<A>) => (a: A) => R.sub(R.zero, a)
 // -------------------------------------------------------------------------------------
 // deprecated
 // -------------------------------------------------------------------------------------
+
+/**
+ * Use `tuple` instead.
+ *
+ * @category combinators
+ * @since 2.0.0
+ * @deprecated
+ */
+export const getTupleRing: <T extends ReadonlyArray<Ring<any>>>(
+  ...rings: T
+) => Ring<{ [K in keyof T]: T[K] extends Ring<infer A> ? A : never }> = tuple as any
 
 /**
  * Use `function.getRing` instead.
