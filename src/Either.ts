aa36082483fe67c5e4aacbc7e06f0f419a08eb1b
gcanti <giulio.canti@gmail.com>
2021-03-20 12:19:55 +0100

    disable strict-type-predicates

--- src/Either.ts
+++ src/Either.ts
@@ -1404,7 +1404,6 @@ export function parseJSON<E>(s: string, onError: (reason: unknown) => E): Either
 export const stringifyJSON = <E>(u: unknown, onError: (reason: unknown) => E): Either<E, string> =>
   tryCatch(() => {
     const s = JSON.stringify(u)
-    // tslint:disable-next-line: strict-type-predicates
     if (typeof s !== 'string') {
       throw new Error('Converting unsupported structure to JSON')
     }
