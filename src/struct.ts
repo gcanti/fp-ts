2fed4b64ef490223bff3bde3c95be7a07948cb5e
gcanti <giulio.canti@gmail.com>
2021-03-31 11:50:26 +0200

    struct: add `evolve`

--- src/struct.ts
+++ src/struct.ts
@@ -1,6 +1,7 @@
 /**
  * @since 2.10.0
  */
+import * as _ from './internal'
 import { getObjectSemigroup, Semigroup } from './Semigroup'
 
 // -------------------------------------------------------------------------------------
@@ -26,3 +27,39 @@ import { getObjectSemigroup, Semigroup } from './Semigroup'
  */
 // tslint:disable-next-line: deprecation
 export const getAssignSemigroup: <A extends object = never>() => Semigroup<A> = getObjectSemigroup
+
+// -------------------------------------------------------------------------------------
+// utils
+// -------------------------------------------------------------------------------------
+
+/**
+ * Creates a new object by recursively evolving a shallow copy of `a`, according to the `transformation` functions.
+ *
+ * @example
+ * import { pipe } from 'fp-ts/function'
+ * import { evolve } from 'fp-ts/struct'
+ *
+ * assert.deepStrictEqual(
+ *   pipe(
+ *     { a: 'a', b: 1 },
+ *     evolve({
+ *       a: (a) => a.length,
+ *       b: (b) => b * 2
+ *     })
+ *   ),
+ *   { a: 1, b: 2 }
+ * )
+ *
+ * @since 3.0.0
+ */
+export const evolve = <A, F extends { [K in keyof A]: (a: A[K]) => unknown }>(transformations: F) => (
+  a: A
+): { [K in keyof F]: ReturnType<F[K]> } => {
+  const out: Record<string, unknown> = {}
+  for (const k in a) {
+    if (_.hasOwnProperty.call(a, k)) {
+      out[k] = transformations[k](a[k])
+    }
+  }
+  return out as any
+}
--- test/struct.ts
+++ test/struct.ts
@@ -1,3 +1,4 @@
+import { pipe } from '../src/function'
 import * as _ from '../src/struct'
 import * as U from './util'
 
@@ -16,5 +17,23 @@ describe('struct', () => {
     }
     const S = _.getAssignSemigroup<T>()
     U.deepStrictEqual(S.concat(foo, bar), Object.assign({}, foo, bar))
+  })
+
+  it('evolve', () => {
+    U.deepStrictEqual(
+      pipe(
+        { a: 'a', b: 1, c: true },
+        _.evolve({
+          a: (s) => s.length,
+          b: (b) => b > 0,
+          c: (c) => !c
+        })
+      ),
+      { a: 1, b: true, c: false }
+    )
+    // should ignore non own properties
+    const x: Record<'b', number> = Object.create({ a: 1 })
+    x.b = 1
+    U.deepStrictEqual(pipe(x, _.evolve({ b: (b) => b > 0 })), { b: true })
   })
 })
