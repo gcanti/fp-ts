32384f240318d54b1560d94c7557a919480519a9
gcanti <giulio.canti@gmail.com>
2021-04-22 16:50:10 +0200

    - struct: remove evolve - tuple: remove evolve

--- src/struct.ts
+++ src/struct.ts
@@ -1,8 +1,8 @@
 /**
  * @since 3.0.0
  */
-import type { Semigroup } from './Semigroup'
 import * as _ from './internal'
+import type { Semigroup } from './Semigroup'
 
 // -------------------------------------------------------------------------------------
 // instances
@@ -29,39 +29,3 @@ import * as _ from './internal'
 export const getAssignSemigroup = <A = never>(): Semigroup<A> => ({
   concat: (second) => (first) => Object.assign({}, first, second)
 })
-
-// -------------------------------------------------------------------------------------
-// utils
-// -------------------------------------------------------------------------------------
-
-/**
- * Creates a new object by recursively evolving a shallow copy of `a`, according to the `transformation` functions.
- *
- * @example
- * import { pipe } from 'fp-ts/function'
- * import { evolve } from 'fp-ts/struct'
- *
- * assert.deepStrictEqual(
- *   pipe(
- *     { a: 'a', b: 1 },
- *     evolve({
- *       a: (a) => a.length,
- *       b: (b) => b * 2
- *     })
- *   ),
- *   { a: 1, b: 2 }
- * )
- *
- * @since 3.0.0
- */
-export const evolve = <A, F extends { [K in keyof A]: (a: A[K]) => unknown }>(transformations: F) => (
-  a: A
-): { [K in keyof F]: ReturnType<F[K]> } => {
-  const out: Record<string, unknown> = {}
-  for (const k in a) {
-    if (_.has.call(a, k)) {
-      out[k] = transformations[k](a[k])
-    }
-  }
-  return out as any
-}
--- src/tuple.ts
+++ src/tuple.ts
@@ -10,33 +10,3 @@
  * @since 3.0.0
  */
 export const tuple = <T extends ReadonlyArray<unknown>>(...t: T): T => t
-
-/**
- * Creates a new tuple by recursively evolving a shallow copy of `as`, according to the `transformation` functions.
- *
- * @example
- * import { pipe } from 'fp-ts/function'
- * import { evolve } from 'fp-ts/tuple'
- *
- * assert.deepStrictEqual(
- *   pipe(
- *     ['a', 1] as const,
- *     evolve(
- *       (s) => s.length,
- *       (n) => n * 2
- *     )
- *   ),
- *   [1, 2]
- * )
- *
- * @since 3.0.0
- */
-export const evolve = <A extends ReadonlyArray<unknown>, F extends { [K in keyof A]: (a: A[K]) => unknown }>(
-  ...transformations: F
-) => (as: A): { [K in keyof F]: ReturnType<F[K]> } => {
-  const out: Array<unknown> = [...as]
-  for (let i = 0; i < transformations.length; i++) {
-    out[i] = transformations[i](as[i])
-  }
-  return out as any
-}
