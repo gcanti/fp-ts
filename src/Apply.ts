9edada8239cf665a7b71be76a78fceba98e5f773
Wayne Van Son <waynevanson@gmail.com>
2022-06-12 10:32:25 +1000

    feat(Apply/ap): add `Apply3<F>` to `Apply1<G>` overload

--- src/Apply.ts
+++ src/Apply.ts
@@ -110,6 +110,12 @@ export interface Apply4<F extends URIS4> extends Functor4<F> {
  * @category combinators
  * @since 2.10.0
  */
+export function ap<F extends URIS3, G extends URIS>(
+  F: Apply3<F>,
+  G: Apply1<G>
+): <R, E, A1>(
+  fa: Kind3<F, R, E, Kind<G, A1>>
+) => <A2>(fab: Kind3<F, R, E, Kind<G, (a: A1) => A2>>) => Kind3<F, R, E, Kind<G, A2>>
 export function ap<F extends URIS2, G extends URIS4, E1>(
   F: Apply2C<F, E1>,
   G: Apply4<G>
