e3300fe30867d5480b12317a646a600d47470aed
gcanti <giulio.canti@gmail.com>
2022-09-24 10:06:54 +0200

    jest -> vitest

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+    include: ['./test/*.ts'],
+    exclude: ['./test/util.ts']
+  }
+})
